2787de63ac6cd2f04f132d6a8328c595162c2183
Mike Sall <mikesall@Mikes-MacBook-Pro.local>
2020-08-03 12:56:42 -0700

    make tabs 2 spaces

--- contracts/GoldfinchPool.sol
+++ contracts/GoldfinchPool.sol
@@ -6,20 +6,20 @@ import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GoldfinchPool is Ownable {
-    using SafeMath for uint256;
-    uint public sharePrice = 1e18;
-    uint mantissa = 1e18;
-    uint public totalShares;
-    mapping(address => uint) public capitalProviders;
+  using SafeMath for uint256;
+  uint public sharePrice = 1e18;
+  uint mantissa = 1e18;
+  uint public totalShares;
+  mapping(address => uint) public capitalProviders;
 
-    function deposit() external payable {
-      uint shares = capitalProviders[msg.sender];
-      uint newShares = getNumShares(msg.value, mantissa, sharePrice);
-      totalShares = totalShares.add(newShares);
-      capitalProviders[msg.sender] = shares.add(newShares);
-    }
+  function deposit() external payable {
+    uint shares = capitalProviders[msg.sender];
+    uint newShares = getNumShares(msg.value, mantissa, sharePrice);
+    totalShares = totalShares.add(newShares);
+    capitalProviders[msg.sender] = shares.add(newShares);
+  }
 
-    function getNumShares(uint amount, uint multiplier, uint price) pure internal returns (uint) {
-      return amount.mul(multiplier).div(price);
-    }
+  function getNumShares(uint amount, uint multiplier, uint price) pure internal returns (uint) {
+    return amount.mul(multiplier).div(price);
+  }
 }
