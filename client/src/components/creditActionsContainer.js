1981455bf8d96280cddb190db5d2440aa12e8daa
sanjayprabhu <sanjayprabhu@gmail.com>
2020-09-29 22:13:08 +0000

    Switch creditActionsContainer to useState

--- client/src/components/creditActionsContainer.js
+++ client/src/components/creditActionsContainer.js
@@ -1,89 +1,78 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PaymentForm from './paymentForm.js';
 import DrawdownForm from './drawdownForm.js';
 import iconDown from '../images/down-purp.svg';
 import iconUp from '../images/up-purp.svg';
 
-class CreditActionsContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showAction: null,
-    };
-  }
+function CreditActionsContainer(props) {
+  const [showAction, setShowAction] = useState(null);
 
-  openAction = (e, action) => {
+  function openAction(e, action) {
     e.preventDefault();
-    this.setState({
-      showAction: action,
-    });
-  };
-
-  closeForm = e => {
-    this.setState({
-      showAction: null,
-    });
-  };
-
-  render() {
-    let formBody;
-    if (!this.props.creditLine.balance) {
-      formBody = (
-        <div className="form-start">
-          <button className="button non-functioning">
-            <img className="button-icon" src={iconDown} alt="down-arrow" />
-            Drawdown
-          </button>
-          <button className="button non-functioning">
-            <img className="button-icon" src={iconUp} alt="up-arrow" />
-            Payment
-          </button>
-        </div>
-      );
-    } else if (this.state.showAction === null) {
-      formBody = (
-        <div className="form-start">
-          <button
-            onClick={e => {
-              this.openAction(e, 'drawdown');
-            }}
-            className="button"
-          >
-            <img className="button-icon" src={iconDown} alt="down-arrow" />
-            Drawdown
-          </button>
-          <button
-            onClick={e => {
-              this.openAction(e, 'payment');
-            }}
-            className="button"
-          >
-            <img className="button-icon" src={iconUp} alt="up-arrow" />
-            Payment
-          </button>
-        </div>
-      );
-    } else if (this.state.showAction === 'payment') {
-      formBody = (
-        <PaymentForm
-          closeForm={this.closeForm}
-          actionComplete={this.props.actionComplete}
-          borrower={this.props.borrower}
-          creditLine={this.props.creditLine}
-        />
-      );
-    } else if (this.state.showAction === 'drawdown') {
-      formBody = (
-        <DrawdownForm
-          closeForm={this.closeForm}
-          actionComplete={this.props.actionComplete}
-          borrower={this.props.borrower}
-          creditLine={this.props.creditLine}
-        />
-      );
-    }
-    return <div className="form-section">{formBody}</div>;
+    setShowAction(action);
   }
+
+  function closeForm() {
+    setShowAction(null);
+  }
+
+  let formBody;
+  if (!props.creditLine.balance) {
+    formBody = (
+      <div className="form-start">
+        <button className="button non-functioning">
+          <img className="button-icon" src={iconDown} alt="down-arrow" />
+          Drawdown
+        </button>
+        <button className="button non-functioning">
+          <img className="button-icon" src={iconUp} alt="up-arrow" />
+          Payment
+        </button>
+      </div>
+    );
+  } else if (showAction === null) {
+    formBody = (
+      <div className="form-start">
+        <button
+          onClick={e => {
+            openAction(e, 'drawdown');
+          }}
+          className="button"
+        >
+          <img className="button-icon" src={iconDown} alt="down-arrow" />
+          Drawdown
+        </button>
+        <button
+          onClick={e => {
+            openAction(e, 'payment');
+          }}
+          className="button"
+        >
+          <img className="button-icon" src={iconUp} alt="up-arrow" />
+          Payment
+        </button>
+      </div>
+    );
+  } else if (showAction === 'payment') {
+    formBody = (
+      <PaymentForm
+        closeForm={closeForm}
+        actionComplete={props.actionComplete}
+        borrower={props.borrower}
+        creditLine={props.creditLine}
+      />
+    );
+  } else if (showAction === 'drawdown') {
+    formBody = (
+      <DrawdownForm
+        closeForm={closeForm}
+        actionComplete={props.actionComplete}
+        borrower={props.borrower}
+        creditLine={props.creditLine}
+      />
+    );
+  }
+  return <div className="form-section">{formBody}</div>;
 }
 
 export default CreditActionsContainer;
