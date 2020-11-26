f454e6e9ddd74bf47eedf19b61cc0b5f82ede82e
Sanjay Raveendran <sanjayprabhu@gmail.com>
2020-11-25 16:16:51 -0800

    Skip calling the contract proxy kit automatically since it doesn't work well for multisig

--- blockchain_scripts/upgrade.js
+++ blockchain_scripts/upgrade.js
@@ -1,6 +1,5 @@
 const { getDeployedContract } = require("./deployHelpers.js")
 const hre = require("hardhat")
-const cpkSDK = require("contract-proxy-kit")
 
 /*
 This script deploys the latest implementations of upgradeable contracts and requests an upgrade via the
@@ -8,40 +7,47 @@ gnosis multisig
 */
 let logger
 
-async function main() {
-  await multisig(hre, cpkSDK)
+const SAFE_CONFIG = {
+  1: { safeAddress: "TODO" }, // Mainnet
+  4: { safeAddress: "0xAA96CA940736e937A8571b132992418c7d220976" }, // Rinkeby
 }
 
-async function multisig(hre, cpkSDK) {
-  const { deployments, getNamedAccounts, ethers } = hre
+async function main() {
+  await multisig(hre)
+}
+
+async function multisig(hre) {
+  const { getNamedAccounts, getChainId } = hre
   const { proxy_owner } = await getNamedAccounts()
-  const CPK = cpkSDK.default
-  const { EthersAdapter } = cpkSDK
 
   // Since this is not a "real" deployment (just a script),
   //the deployments.log is not enabled. So, just use console.log instead
   logger = console.log
 
-  let contractsToUpgrade = ["GoldfinchConfig", "CreditLineFactory", "CreditDesk", "Pool"]
-  let safeSDK = await getCPK(CPK, EthersAdapter, ethers)
-  const contracts = await getContracts(contractsToUpgrade, safeSDK.address, proxy_owner, deployments, ethers)
+  const chainId = await getChainId()
 
-  let upgradeTransactions = []
-  for (let i = 0; i < contractsToUpgrade.length; i++) {
-    let contractInfo = contracts[contractsToUpgrade[i]]
-    let proxy = contractInfo.proxy
-    upgradeTransactions.push({
-      operation: CPK.Call,
-      data: proxy.interface.encodeFunctionData("changeImplementation", [contractInfo.newImplementation, "0x"]),
-      to: proxy.address,
-    })
+  if (!SAFE_CONFIG[chainId]) {
+    throw new Error(`Unsupported chain id: ${chainId}`)
   }
-  const result = await safeSDK.execTransactions(upgradeTransactions)
-  logger(`Done. Upgrade transaction: ${result.hash}`)
+
+  let contractsToUpgrade = process.env.CONTRACTS || "GoldfinchConfig, CreditLineFactory, CreditDesk, Pool"
+  contractsToUpgrade = contractsToUpgrade.split(/[ ,]+/)
+  const contracts = await deployUpgrades(contractsToUpgrade, proxy_owner, hre)
+
+  logger(`Safe address: ${SAFE_CONFIG[chainId].safeAddress}`)
+  for (let i = 0; i < contractsToUpgrade.length; i++) {
+    let contract = contracts[contractsToUpgrade[i]]
+    logger(`${contract.name}. Proxy: ${contract.proxy.address} New Implementation: ${contract.newImplementation}`)
+  }
+
+  logger("Done.")
 }
 
-async function getContracts(contractNames, safeAddress, proxy_owner, deployments, ethers) {
+async function deployUpgrades(contractNames, proxy_owner, hre) {
+  const { deployments, ethers, getChainId } = hre
   const { deploy } = deployments
+  const safeAddress = SAFE_CONFIG[await getChainId()].safeAddress
+
   const result = {}
   const dependencies = {
     GoldfinchConfig: { ["ConfigOptions"]: (await deployments.getOrNull("ConfigOptions")).address },
@@ -52,11 +58,11 @@ async function getContracts(contractNames, safeAddress, proxy_owner, deployments
     let contractName = contractNames[i]
     let contract = await getDeployedContract(deployments, contractName)
     let contractProxy = await getDeployedContract(deployments, `${contractName}_Proxy`)
-    // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.2.0/contracts/proxy/TransparentUpgradeableProxy.sol#L68
-    const adminStorageLocation = "0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103"
-    let admin = await ethers.provider.getStorageAt(contractProxy.address, adminStorageLocation)
-    admin = ethers.utils.hexStripZeros(admin)
-    admin = await contractProxy.owner()
+    // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.2.0/contracts/proxy/TransparentUpgradeableProxy.sol#L81
+    const implStorageLocation = "0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc"
+    let currentImpl = await ethers.provider.getStorageAt(contractProxy.address, implStorageLocation)
+    currentImpl = ethers.utils.hexStripZeros(currentImpl)
+    const admin = await contractProxy.owner()
     if (admin.toLowerCase() !== safeAddress.toLowerCase()) {
       logger(`Converting safe ${safeAddress} as the proxy owner for ${contractName}`)
       const contractAsAdmin = await getDeployedContract(deployments, `${contractName}_Proxy`, proxy_owner)
@@ -70,41 +76,22 @@ async function getContracts(contractNames, safeAddress, proxy_owner, deployments
       args: [],
       libraries: dependencies[contractName],
     })
-    let contractInfo = { contract: contract, proxy: contractProxy, newImplementation: deployResult.address }
+    let contractInfo = {
+      name: contractName,
+      contract: contract,
+      proxy: contractProxy,
+      newImplementation: deployResult.address,
+    }
+
+    if (currentImpl.toLowerCase() === contractInfo.newImplementation.toLowerCase()) {
+      logger(`${contractName} did not change, skipping`)
+      continue
+    }
     result[contractName] = contractInfo
-    logger(`${contractName}: Proxy: ${contractProxy.address}, newImpl: ${contractInfo.newImplementation}`)
+
+    logger(`Deployed ${contractName} to ${contractInfo.newImplementation} (current ${currentImpl})`)
   }
   return result
-}
-
-async function getCPK(CPK, EthersAdapter, ethers) {
-  // Emulate browser for CPK
-  const [protocol_owner] = await ethers.getSigners()
-  const window = { addEventListener: () => {}, parent: { postMessage: () => {} }, performance: { now: () => {} } }
-  // noinspection JSConstantReassignment
-  global.window = window
-
-  const ethLibAdapter = new EthersAdapter({ ethers, signer: protocol_owner })
-
-  const cpk = await CPK.create({ ethLibAdapter })
-  const proxyDeployed = await cpk.isProxyDeployed()
-  const owner = await cpk.getOwnerAccount()
-  const address = cpk.address
-  logger(`Safe Deployed? ${proxyDeployed}, Safe address: ${address}, owner: ${owner}`)
-
-  if (!proxyDeployed) {
-    // Create the safe and seed it with some ETH for gas.
-    const result = await cpk.execTransactions([
-      {
-        operation: CPK.Call,
-        data: "0x",
-        to: cpk.address,
-        value: `${1e17}`,
-      },
-    ])
-    logger(`Created safe at ${result.hash}`)
-  }
-  return cpk
 }
 
 if (require.main === module) {
