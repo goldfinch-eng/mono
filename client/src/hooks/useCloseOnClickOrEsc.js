726d3d099495c20655b4ec4c52907b2d56e38585
Sanjay Raveendran <sanjayprabhu@gmail.com>
2020-12-10 18:11:21 -0800

    Fix Esc closing the form even when an input has focus

--- client/src/hooks/useCloseOnClickOrEsc.js
+++ client/src/hooks/useCloseOnClickOrEsc.js
@@ -2,33 +2,44 @@ import { useState, useRef, useEffect, useCallback } from 'react';
 
 function useCloseOnClickOrEsc(opts = {}) {
   const node = useRef();
-  const handleEscFunction = useCallback(event => {
-    if (opts.closeOnEsc === false) {
-      return;
-    }
-    if (event.keyCode === 27) {
+  const handleEscFunction = useCallback(
+    event => {
+      if (opts.closeOnEsc === false) {
+        return;
+      }
+      if (event.keyCode === 27) {
+        if (opts.closeFormFn) {
+          // If an input has focus, then don't close the form, unfocus it first
+          if (document.activeElement && document.activeElement.tagName == 'INPUT') {
+            document.activeElement.blur();
+          } else {
+            opts.closeFormFn();
+          }
+        } else {
+          setOpen('');
+        }
+      }
+    },
+    [opts],
+  );
+
+  const [open, setOpen] = useState('');
+
+  const handleClickOutside = useCallback(
+    e => {
+      if (node.current.contains(e.target) || opts.closeOnClick === false) {
+        // inside click
+        return;
+      }
+      // outside click
       if (opts.closeFormFn) {
         opts.closeFormFn();
       } else {
         setOpen('');
       }
-    }
-  }, [opts]);
-
-  const [open, setOpen] = useState('');
-
-  const handleClickOutside = useCallback(e => {
-    if (node.current.contains(e.target) || opts.closeOnClick === false) {
-      // inside click
-      return;
-    }
-    // outside click
-    if (opts.closeFormFn) {
-      opts.closeFormFn();
-    } else {
-      setOpen('');
-    }
-  }, [opts]);
+    },
+    [opts],
+  );
 
   useEffect(() => {
     document.addEventListener('mousedown', handleClickOutside);
