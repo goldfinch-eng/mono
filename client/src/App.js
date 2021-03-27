396c0c016ea1f4896d0147480d33dac84216a018
Mark Hudnall <me@markhudnall.com>
2021-03-27 15:24:38 -0700

    Fix setUserAddress (#224)

--- client/src/App.js
+++ client/src/App.js
@@ -88,7 +88,8 @@ function App() {
   async function refreshUserData(overrideAddress) {
     let data = defaultUser();
     const accounts = await web3.eth.getAccounts();
-    let userAddress = (gnosisSafeInfo && gnosisSafeInfo.safeAddress) || (accounts && accounts[0]) || user.address;
+    let userAddress =
+      overrideAddress || (gnosisSafeInfo && gnosisSafeInfo.safeAddress) || (accounts && accounts[0]) || user.address;
     // Set this to the borrower contract address to test gasless transactions
     // let userAddress = '0xd3D57673BAE28880376cDF89aeFe4653A5C84A08';
     if (userAddress && usdc && creditDesk.loaded && pool.loaded) {
