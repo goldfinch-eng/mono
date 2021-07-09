8dacce90a36d33da250fed47d463a6c734cf6e93
ihinsdale <ian@ianhinsdale.com>
2021-07-09 11:06:01 -0400

    Configure client to use Sentry SDK.

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,15 @@ import "./layout/index.scss"
 import { App } from "./App"
 import * as serviceWorker from "./serviceWorker"
 import "focus-within-polyfill"
+import * as Sentry from "@sentry/react"
+import { Integrations } from "@sentry/tracing"
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: process.env.NODE_ENV === "production" ? 0.25 : 1.0,
+})
 
 ReactDOM.render(<App />, document.getElementById("root"))
 
