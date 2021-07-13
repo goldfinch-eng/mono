c79da390b61b7af88f83a34516485c40ea4f2b79
ihinsdale <ian@ianhinsdale.com>
2021-07-12 21:56:33 -0400

    Don't enable Sentry by default in development, for client. Use Sentry's CaptureConsole integration, for client.

--- client/src/index.js
+++ client/src/index.js
@@ -5,30 +5,33 @@ import {App} from "./App"
 import * as serviceWorker from "./serviceWorker"
 import "focus-within-polyfill"
 import * as Sentry from "@sentry/react"
-import {Integrations} from "@sentry/tracing"
+import {BrowserTracing as BrowserTracingIntegration} from "@sentry/tracing"
+import {CaptureConsole as CaptureConsoleIntegration} from "@sentry/integrations"
 
 function configureSentry() {
   const dsn =
     process.env.REACT_APP_DEPLOY_CONTEXT === "deploy-preview"
       ? process.env.REACT_APP_SENTRY_DSN_DEPLOY_PREVIEW
       : process.env.REACT_APP_SENTRY_DSN
-  if (!dsn) {
-    console.error("Failed to obtain Sentry `dsn`.")
-  }
+  if (dsn) {
+    const release = process.env.REACT_APP_COMMIT_ID
 
-  const release = process.env.REACT_APP_COMMIT_ID
+    Sentry.init({
+      dsn,
+      integrations: [new BrowserTracingIntegration(), new CaptureConsoleIntegration()],
+      release,
+      environment: process.env.NODE_ENV,
+      tracesSampleRate: process.env.NODE_ENV === "production" ? 0.25 : 1.0,
+    })
 
-  Sentry.init({
-    dsn,
-    integrations: [new Integrations.BrowserTracing()],
-    release,
-    environment: process.env.NODE_ENV,
-    tracesSampleRate: process.env.NODE_ENV === "production" ? 0.25 : 1.0,
-  })
-
-  if (!release) {
+    if (!release) {
+      if (process.env.NODE_ENV !== "development") {
+        console.error("Failed to obtain Sentry `release`.")
+      }
+    }
+  } else {
     if (process.env.NODE_ENV !== "development") {
-      console.error("Failed to obtain Sentry `release`.")
+      console.error("Failed to obtain Sentry `dsn`.")
     }
   }
 }
