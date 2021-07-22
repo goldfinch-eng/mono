cce5a096aa18c2b0892fabec3c6ee51878eea4a2
ihinsdale <ian@ianhinsdale.com>
2021-07-22 18:34:20 -0400

    Fix GFN-01: validate that addresses are not zero address.

--- contracts/protocol/core/GoldfinchFactory.sol
+++ contracts/protocol/core/GoldfinchFactory.sol
@@ -24,6 +24,7 @@ contract GoldfinchFactory is BaseUpgradeablePausable {
   event PoolCreated(address indexed pool, address indexed borrower);
 
   function initialize(address owner, GoldfinchConfig _config) public initializer {
+    require(owner != address(0) && address(_config) != address(0), "Owner and config addresses cannot be empty");
     __BaseUpgradeablePausable__init(owner);
     config = _config;
   }
