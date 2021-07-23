9c1526aff840a76657c7a15f5ac2ddfdfeff1d46
ihinsdale <ian@ianhinsdale.com>
2021-07-23 17:59:40 -0400

    Validate that arguments are not zero addresses. Missed by audit.

--- contracts/protocol/core/CreditDesk.sol
+++ contracts/protocol/core/CreditDesk.sol
@@ -54,6 +54,7 @@ contract CreditDesk is BaseUpgradeablePausable, ICreditDesk {
    * @param _config The address of the GoldfinchConfig contract
    */
   function initialize(address owner, GoldfinchConfig _config) public initializer {
+    require(owner != address(0) && address(_config) != address(0), "Owner and config addresses cannot be empty");
     __BaseUpgradeablePausable__init(owner);
     config = _config;
   }
