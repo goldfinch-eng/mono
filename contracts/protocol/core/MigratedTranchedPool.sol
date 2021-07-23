12b15ba1705c31c94e59329ff1bb8a3206d8b271
ihinsdale <ian@ianhinsdale.com>
2021-07-23 15:30:20 -0400

    Fix MIG-02: use local variable to reduce storage reads.

--- contracts/protocol/core/MigratedTranchedPool.sol
+++ contracts/protocol/core/MigratedTranchedPool.sol
@@ -21,21 +21,23 @@ contract MigratedTranchedPool is TranchedPool, IMigratedTranchedPool {
   ) external override returns (IV2CreditLine) {
     require(msg.sender == config.creditDeskAddress(), "Only credit desk can call this");
     require(!migrated, "Already migrated");
+
     // Set accounting state vars.
-    creditLine.setBalance(clToMigrate.balance());
-    creditLine.setInterestOwed(clToMigrate.interestOwed());
-    creditLine.setPrincipalOwed(clToMigrate.principalOwed());
-    creditLine.setTermEndTime(termEndTime);
-    creditLine.setNextDueTime(nextDueTime);
-    creditLine.setInterestAccruedAsOf(interestAccruedAsOf);
-    creditLine.setLastFullPaymentTime(lastFullPaymentTime);
-    creditLine.setTotalInterestAccrued(totalInterestPaid.add(clToMigrate.interestOwed()));
+    IV2CreditLine newCl = creditLine;
+    newCl.setBalance(clToMigrate.balance());
+    newCl.setInterestOwed(clToMigrate.interestOwed());
+    newCl.setPrincipalOwed(clToMigrate.principalOwed());
+    newCl.setTermEndTime(termEndTime);
+    newCl.setNextDueTime(nextDueTime);
+    newCl.setInterestAccruedAsOf(interestAccruedAsOf);
+    newCl.setLastFullPaymentTime(lastFullPaymentTime);
+    newCl.setTotalInterestAccrued(totalInterestPaid.add(clToMigrate.interestOwed()));
 
     migrateDeposits(clToMigrate, totalInterestPaid, totalPrincipalPaid);
 
     migrated = true;
 
-    return creditLine;
+    return newCl;
   }
 
   function migrateDeposits(
