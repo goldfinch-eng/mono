78da7277b4bdd15b786da0b042af9e003ef485fb
Sanjay Raveendran <sanjayprabhu@gmail.com>
2021-08-18 17:06:55 -0700

    Update tranchedPool admin script

--- blockchain_scripts/tranchedPoolAdmin.ts
+++ blockchain_scripts/tranchedPoolAdmin.ts
@@ -16,16 +16,17 @@ async function main() {
     throw new Error("Must specify an POOL and an ACTION")
   }
 
-  const pool = await getPool(poolAddress, protocolOwner)
-
+  const tranchedPool = await getPool(poolAddress, protocolOwner)
 
   if (action === "lockJunior") {
-    await lockJunior(pool)
+    await lockJunior(tranchedPool)
   } else if (action === "investSenior") {
-    await investSeniorAndLock(pool)
+    await investSeniorAndLock(tranchedPool)
   } else if (action === "lockAndInvest") {
-    await lockJunior(pool)
-    await investSeniorAndLock(pool)
+    await lockJunior(tranchedPool)
+    await investSeniorAndLock(tranchedPool)
+  } else if (action === "assess") {
+    await tranchedPool.assess()
   }
   console.log("Done")
 }
@@ -41,9 +42,9 @@ async function lockJunior(pool) {
   console.log(`Locked the junior tranche for ${pool.address}`)
 }
 
-async function investSeniorAndLock(pool) {
-  const juniorTrancheLockedUntil = parseInt((await pool.getTranche(TRANCHES.Junior))[4])
-  const seniorTranchedLockedUntil = parseInt((await pool.getTranche(TRANCHES.Senior))[4])
+async function investSeniorAndLock(tranchedPool) {
+  const juniorTrancheLockedUntil = parseInt((await tranchedPool.getTranche(TRANCHES.Junior))[4])
+  const seniorTranchedLockedUntil = parseInt((await tranchedPool.getTranche(TRANCHES.Senior))[4])
 
   if (juniorTrancheLockedUntil === 0) {
     throw new Error("Junior tranche must be locked first")
@@ -52,14 +53,22 @@ async function investSeniorAndLock(pool) {
     throw new Error("Senior tranche already locked")
   }
 
-  const seniorFund = await getFund(pool, pool.signer)
-  let txn = await seniorFund.invest(pool.address)
-  await txn.wait()
-  console.log(`Senior fund invested into ${pool.address}`)
+  const seniorPool = await getSeniorPool(tranchedPool, tranchedPool.signer)
 
-  txn = await pool.lockPool()
+  const estimatedInvestment = await seniorPool.estimateInvestment(tranchedPool.address)
+  let txn
+
+  if (estimatedInvestment.isZero()) {
+    console.log(`Senior pool investment not required`)
+  } else {
+    txn = await seniorPool.invest(tranchedPool.address)
+    await txn.wait()
+    console.log(`Senior pool invested into ${tranchedPool.address}`)
+  }
+
+  txn = await tranchedPool.lockPool()
   await txn.wait()
-  console.log(`Pool locked`)
+  console.log(`TranchedPool locked`)
 }
 
 async function getPool(contractAddress, signerAddress) {
@@ -68,22 +77,21 @@ async function getPool(contractAddress, signerAddress) {
   return await ethers.getContractAt(abi, contractAddress, signer)
 }
 
-async function getFund(pool, signerAddress) {
+async function getSeniorPool(pool, signerAddress) {
   const signer = await getSignerForAddress(signerAddress)
   const configAddress = await pool.config()
   const configABI = await getAbi("GoldfinchConfig")
   const config = await ethers.getContractAt(configABI, configAddress, signer)
 
-  const fundAddress = await config.getAddress(CONFIG_KEYS.SeniorFund)
-  const fundABI = await getAbi("SeniorFund")
-  return await ethers.getContractAt(fundABI, fundAddress, signer)
+  const poolAddress = await config.getAddress(CONFIG_KEYS.SeniorPool)
+  const poolABI = await getAbi("SeniorPool")
+  return await ethers.getContractAt(poolABI, poolAddress, signer)
 }
 
 async function getAbi(contractName) {
   const chainId = await hre.getChainId()
   const networkName = chainId === "31337" ? "localhost" : process.env.HARDHAT_NETWORK
-  const abi = deployedABIs[chainId][networkName!].contracts[contractName].abi
-  return abi
+  return deployedABIs[chainId][networkName!].contracts[contractName].abi
 }
 
 
