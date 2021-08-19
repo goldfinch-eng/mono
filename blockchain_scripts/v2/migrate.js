e5900b978cfa68c6deb78020d09b56fdab7c5e32
Ian Hinsdale <ian@goldfinch.finance>
2021-08-19 13:32:00 -0400

    Re-arrange order of statements to match tests.

--- blockchain_scripts/v2/migrate.js
+++ blockchain_scripts/v2/migrate.js
@@ -231,17 +231,20 @@ async function handleNewDeployments(migrator) {
 async function givePermsToMigrator({pool, creditDesk, goldfinchFactory, fidu, migrator, oldConfig}) {
   // Give owner roles to the migrator
   if (isMainnetForking()) {
-    await pool.grantRole(OWNER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
-    await creditDesk.grantRole(OWNER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
-    await goldfinchFactory.grantRole(OWNER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
     await fidu.grantRole(MINTER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
     await fidu.grantRole(OWNER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
-    await oldConfig.grantRole(OWNER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
-
-    await pool.grantRole(PAUSER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
-    await creditDesk.grantRole(PAUSER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
-    await goldfinchFactory.grantRole(PAUSER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
     await fidu.grantRole(PAUSER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
+
+    await creditDesk.grantRole(OWNER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
+    await creditDesk.grantRole(PAUSER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
+
+    await pool.grantRole(OWNER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
+    await pool.grantRole(PAUSER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
+
+    await goldfinchFactory.grantRole(OWNER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
+    await goldfinchFactory.grantRole(PAUSER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
+
+    await oldConfig.grantRole(OWNER_ROLE, migrator.address, {from: MAINNET_MULTISIG})
   } else {
     console.log("--------Do a multisend tx----------")
     console.log("-----------------------------------")
