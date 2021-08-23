180b35094b58b3e52e092032748afd4b68f273c1
Ian Hinsdale <ian@goldfinch.finance>
2021-08-23 17:46:08 -0400

    WIP Implement tests of server's signature timestamp logic.

--- server/functions/test/index.test.ts
+++ server/functions/test/index.test.ts
@@ -3,6 +3,7 @@ import chaiSubset from "chai-subset"
 import * as firebaseTesting from "@firebase/rules-unit-testing"
 import * as admin from "firebase-admin"
 import crypto from "crypto"
+import {SinonFakeTimers, useFakeTimers} from "sinon"
 
 import {FirebaseConfig, getUsers, setEnvForTest} from "../src/db"
 import {kycStatus, personaCallback} from "../src"
@@ -11,16 +12,26 @@ chai.use(chaiSubset as any)
 const expect = chai.expect
 import firestore = admin.firestore
 import Firestore = firestore.Firestore
+import {Request} from "express"
 
 describe("functions", () => {
   let testFirestore: Firestore
   let testApp: admin.app.App
   let config: Omit<FirebaseConfig, "sentry">
   const projectId = "goldfinch-frontend-test"
-  const address = "0xE7f9ED35DA54b2e4A1857487dBf42A32C4DBD4a0"
+  const address = "0xb5c52599dFc7F9858F948f003362A7f4B5E678A5"
   const validSignature =
-    "0xaf75579e99f8810b5c009041961852a4872d3b19031a283ff8ea451854ac072331610c5edaf6ec7430a11cea0f19a2a177ce3b5c52ee93b933fd91e2f9336ad71c"
+    "0xd5c801fb4f8e37b41dfcf5b69a63b62563dcd171f18425517aed1e0172a0ebc118ef362f66b2a7f95d406e81e55830f48aa872c1fffdb0fa55d0789df7d9e0651c"
   let users: firestore.CollectionReference<firestore.DocumentData>
+
+  const now = 1629746103600
+  let clock: SinonFakeTimers
+  const ONE_HOUR_MILLIS = 1000 * 60 * 60
+  const ONE_MINUTE_MILLIS = 1000 * 60
+
+  before(async () => {
+    clock = useFakeTimers(now)
+  })
 
   beforeEach(() => {
     testApp = firebaseTesting.initializeAdminApp({projectId: projectId})
@@ -31,6 +42,10 @@ describe("functions", () => {
     }
     setEnvForTest(testFirestore, config)
     users = getUsers(testFirestore)
+  })
+
+  after(async () => {
+    clock.restore()
   })
 
   afterEach(async () => {
@@ -52,39 +67,75 @@ describe("functions", () => {
   }
 
   describe("kycStatus", async () => {
-    const generateKycRequest = (address: string, signature: string) => {
-      return {headers: {"x-goldfinch-signature": signature}, query: {address}} as any
+    const generateKycRequest = (
+      address: string,
+      signature: string,
+      timestamp: number | string | undefined,
+    ): Request => {
+      return {
+        headers: {"x-goldfinch-signature": signature, "x-goldfinch-signature-timestamp": timestamp},
+        query: {address},
+      } as unknown as Request
     }
+
     describe("validation", async () => {
       it("checks if address is present", async () => {
-        await kycStatus(generateKycRequest("", ""), expectResponse(400, {error: "Address or signature not provided"}))
+        await kycStatus(
+          generateKycRequest("", validSignature, now),
+          expectResponse(400, {error: "Address not provided."}),
+        )
       })
 
       it("checks if the signature is present", async () => {
+        await kycStatus(generateKycRequest(address, "", now), expectResponse(400, {error: "Signature not provided."}))
+      })
+
+      it("checks if the signature timestamp is present", async () => {
         await kycStatus(
-          generateKycRequest(address, ""),
-          expectResponse(400, {error: "Address or signature not provided"}),
+          generateKycRequest(address, validSignature, undefined),
+          expectResponse(400, {error: "Signature timestamp not provided."}),
         )
       })
 
       it("returns an error if the signature is incorrect", async () => {
         const invalidSignature =
           "0xaf75579e99f8810b5c009041961852a4872d3b19031a283ff8ea451854ac072331610c5edaf6ec7430a11cea0f19a2a111ce3b5c52ee93b933fd91e2f9336ad71c"
-        const req = generateKycRequest(address, invalidSignature)
-        await kycStatus(req, expectResponse(403, {error: "Invalid address or signature"}))
+        const req = generateKycRequest(address, invalidSignature, now)
+        await kycStatus(req, expectResponse(403, {error: "Invalid signature."}))
+      })
+
+      it("returns an error if the signature timestamp is invalid", async () => {
+        const validSignatureInvalidTimestamp =
+          "0xf55449d7cea45a1537616da2ca9300623ec8c74888868209a4b02c19990e7d884f5835a4bc56218cf6e2d72d6d2351b2f0c52717159643025a70e2d3d25a7ac21c"
+        const req = generateKycRequest(address, validSignatureInvalidTimestamp, "foo")
+        await kycStatus(req, expectResponse(400, {error: "Invalid signature timestamp."}))
+      })
+
+      it("returns an error if the signature timestamp has expired", async () => {
+        const validSignatureExpiredTimestamp =
+          "0xf55449d7cea45a1537616da2ca9300623ec8c74888868209a4b02c19990e7d884f5835a4bc56218cf6e2d72d6d2351b2f0c52717159643025a70e2d3d25a7ac21c"
+        const req = generateKycRequest(address, validSignatureExpiredTimestamp, now - ONE_HOUR_MILLIS - 1)
+        await kycStatus(req, expectResponse(403, {error: "Signature expired."}))
+      })
+
+      it("returns an error if the signature timestamp is in the future", async () => {
+        const validSignatureTooFutureTimestamp =
+          "0xf55449d7cea45a1537616da2ca9300623ec8c74888868209a4b02c19990e7d884f5835a4bc56218cf6e2d72d6d2351b2f0c52717159643025a70e2d3d25a7ac21c"
+        const req = generateKycRequest(address, validSignatureTooFutureTimestamp, now + ONE_MINUTE_MILLIS + 1)
+        await kycStatus(req, expectResponse(403, {error: "Unexpected signature timestamp."}))
       })
     })
 
-    describe("with valid address and signature", async () => {
+    describe("with valid address and signature and timestamp", async () => {
       describe("when the user doesn't exist", async () => {
         it("returns status unknown", async () => {
-          const req = generateKycRequest(address, validSignature)
+          const req = generateKycRequest(address, validSignature, now)
           await kycStatus(req, expectResponse(200, {address, status: "unknown"}))
         })
       })
       describe("when the user exists", async () => {
         it("returns the status and countrycode based on status in the db", async () => {
-          const req = generateKycRequest(address, validSignature)
+          const req = generateKycRequest(address, validSignature, now)
 
           await users.doc(address.toLowerCase()).set({
             persona: {},
