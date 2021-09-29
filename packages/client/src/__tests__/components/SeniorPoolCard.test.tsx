35a9ece0f0cef0f3832a26ca69cc888ca5849b54
Jaênia Sousa <jaenia@vinta.com.br>
2021-09-29 10:45:18 -0300

    [feat] Show open pools on Earn page (#560)
    * Creates badge component
    
    * Adds capacity badge to tranched pool card
    
    * Makes possible to have a fixed width of 48px in the badge
    
    * Adds tests for open/full pools
    
    * Uses Typescript on Badge component
    
    * Shows open/full info to the Senior Pool on Earn page
    
    * Uses isZero function when checking the remaining capacity on senior pool card
    
    * Refactors senior pool card tests to use big number for remaining capacity
    
    * Checks if remaining capacity is defined when checking if is equal to zero
    
    * Fixes css of pool row with the badge in mobile
    
    * Uses const instead of let on test stores
    
    The change was made to SeniorPoolCard and TranchedPoolCard tests

---
+++ packages/client/src/__tests__/components/SeniorPoolCard.test.tsx
@@ -0,0 +1,44 @@
+import "@testing-library/jest-dom"
+import {AppContext} from "../../App"
+import {render, screen, waitFor} from "@testing-library/react"
+import {defaultUser} from "../../ethereum/user"
+import {SeniorPoolCard} from "../../components/earn"
+import BigNumber from "bignumber.js"
+
+function renderSeniorPoolCard(remainingCapacity) {
+  const props = {
+    balance: 100,
+    userBalance: 200,
+    apy: 10,
+    limit: 300,
+    remainingCapacity: remainingCapacity,
+  }
+
+  const store = {
+    user: defaultUser(),
+  }
+
+  return render(
+    <AppContext.Provider value={store}>
+      <SeniorPoolCard {...props} />
+    </AppContext.Provider>,
+  )
+}
+
+describe("Senior pool card", () => {
+  it("should show open badge", async () => {
+    renderSeniorPoolCard(new BigNumber(100))
+
+    await waitFor(() => {
+      expect(screen.getByText("Open")).toBeInTheDocument()
+    })
+  })
+
+  it("should show full badge", async () => {
+    renderSeniorPoolCard(new BigNumber(0))
+
+    await waitFor(() => {
+      expect(screen.getByText("Full")).toBeInTheDocument()
+    })
+  })
+})
