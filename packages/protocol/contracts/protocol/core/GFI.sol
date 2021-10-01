7e68d06ea5609339e4cfb9a1faaa10785527bd3f
wbj-goldfinch <90154962+wbj-goldfinch@users.noreply.github.com>
2021-10-01 22:03:22 +0000

    [feature] cap the max amount of GFI

--- packages/protocol/contracts/protocol/core/GFI.sol
+++ packages/protocol/contracts/protocol/core/GFI.sol
@@ -2,42 +2,35 @@
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
+<<<<<<< HEAD:packages/protocol/contracts/protocol/core/GFI.sol
 import "@openzeppelin/contracts-ethereum-package/contracts/presets/ERC20PresetMinterPauser.sol";
 import "./ConfigHelper.sol";
+=======
+import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+>>>>>>> [feature] cap the max amount of GFI:packages/protocol/contracts/staking/GFI.sol
 
 /**
  * @title GFI
  * @notice GFI is Goldfinch's governance token.
  * @author Goldfinch
  */
-contract GFI is ERC20PresetMinterPauserUpgradeSafe {
+contract GFI is ERC20PresetMinterPauser {
+  using SafeMath for uint256;
+
   bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
-  GoldfinchConfig public config;
-  using ConfigHelper for GoldfinchConfig;
 
-  event GoldfinchConfigUpdated(address indexed who, address configAddress);
+  /// The maximum number of tokens that can be minted
+  uint256 public cap;
 
-  /*
-    We are using our own initializer function so we can set the owner by passing it in.
-    I would override the regular "initializer" function, but I can't because it's not marked
-    as "virtual" in the parent contract
-  */
-  // solhint-disable-next-line func-name-mixedcase
-  function __initialize__(
+  event CapUpdated(address indexed who, uint256 cap);
+
+  constructor(
     address owner,
-    string calldata name,
-    string calldata symbol,
-    GoldfinchConfig _config
-  ) external initializer {
-    __Context_init_unchained();
-    __AccessControl_init_unchained();
-    __ERC20_init_unchained(name, symbol);
-
-    __ERC20Burnable_init_unchained();
-    __Pausable_init_unchained();
-    __ERC20Pausable_init_unchained();
-
-    config = _config;
+    string memory name,
+    string memory symbol,
+    uint256 initialCap
+  ) public ERC20PresetMinterPauser(name, symbol) {
+    cap = initialCap;
 
     _setupRole(MINTER_ROLE, owner);
     _setupRole(PAUSER_ROLE, owner);
@@ -48,9 +41,42 @@ contract GFI is ERC20PresetMinterPauserUpgradeSafe {
     _setRoleAdmin(OWNER_ROLE, OWNER_ROLE);
   }
 
-  function updateGoldfinchConfig() external {
-    require(hasRole(OWNER_ROLE, _msgSender()), "ERC20PresetMinterPauser: Must have owner role to change config");
-    config = GoldfinchConfig(config.configAddress());
-    emit GoldfinchConfigUpdated(msg.sender, address(config));
+  /**
+   * @notice create and send tokens to a specified address
+   * @dev this function will fail if the caller attempts to mint over the current cap
+   */
+  // solhint-disable-next-line modifiers/ensure-modifiers
+  function mintTo(address account, uint256 amount) external onlyMinter {
+    require(mintingAmountIsWithinCap(amount, totalSupply(), cap), "Cannot mint more than cap");
+    _mint(account, amount);
+  }
+
+  /**
+   * @notice sets the maximum number of tokens that can be minted
+   * @dev the cap must be greater than the current total supply
+   */
+  // solhint-disable-next-line modifiers/ensure-modifiers
+  function setCap(uint256 cap_) external onlyOwner {
+    require(cap_ >= totalSupply(), "Cannot decrease the cap below existing supply");
+    cap = cap_;
+    emit CapUpdated(_msgSender(), cap);
+  }
+
+  function mintingAmountIsWithinCap(
+    uint256 amount,
+    uint256 totalSupply,
+    uint256 cap_
+  ) internal pure returns (bool) {
+    return totalSupply.add(amount) <= cap_;
+  }
+
+  modifier onlyOwner() {
+    require(hasRole(OWNER_ROLE, _msgSender()), "Must be owner to modify the cap");
+    _;
+  }
+
+  modifier onlyMinter() {
+    require(hasRole(MINTER_ROLE, _msgSender()), "Must be owner to modify the cap");
+    _;
   }
 }
--- packages/protocol/test/GFI.test.ts
+++ packages/protocol/test/GFI.test.ts
@@ -1,39 +1,123 @@
 import {deployments} from "hardhat"
 import {getDeployedAsTruffleContract} from "./testHelpers"
+import BN from "bn.js"
 import {expectEvent} from "@openzeppelin/test-helpers"
+import {GFIInstance} from "../typechain/truffle"
 
-const BEFORE_EACH_TIMEOUT = 30000
+interface TestSetupReturn {
+  gfi: GFIInstance
+}
 
 describe("GFI", () => {
-  const testSetup = deployments.createFixture(async ({deployments, getNamedAccounts}) => {
-    await deployments.run("base_deploy")
-    const gfi = await getDeployedAsTruffleContract(deployments, "GFI")
-    const goldfinchConfig = await getDeployedAsTruffleContract(deployments, "GoldfinchConfig")
+  const testSetup: (options?: any) => Promise<TestSetupReturn> = deployments.createFixture(
+    async ({deployments, getNamedAccounts}) => {
+      const {protocol_owner: owner} = await getNamedAccounts()
+      await deployments.run("base_deploy")
+      const gfi = await getDeployedAsTruffleContract<GFIInstance>(deployments, "GFI")
 
-    return {gfi, goldfinchConfig}
-  })
+      return {gfi}
+    }
+  )
 
-  let owner, gfi, goldfinchConfig
-  beforeEach(async function () {
-    this.timeout(BEFORE_EACH_TIMEOUT)
-
+  let owner: string | undefined
+  let notOwner: string | undefined
+  let gfi: GFIInstance
+  beforeEach(async () => {
     // eslint-disable-next-line @typescript-eslint/no-extra-semi
-    ;[owner] = await web3.eth.getAccounts()
+    ;[owner, notOwner] = await web3.eth.getAccounts()
     const deployments = await testSetup()
     gfi = deployments.gfi
-    goldfinchConfig = deployments.goldfinchConfig
   })
 
-  describe("updateGoldfinchConfig", () => {
-    describe("setting it", () => {
+  describe("symbol", () => {
+    it("is GFI", async () => {
+      console.log(gfi.constructor.name)
+      expect(await gfi.symbol()).to.eq("GFI")
+    })
+  })
+
+  describe("name", () => {
+    it("is GFI", async () => {
+      expect(await gfi.name()).to.eq("GFI")
+    })
+  })
+
+  describe("setCap", () => {
+    describe("as owner", () => {
+      it("allows you to increase the cap", async () => {
+        const cap = await gfi.cap({from: owner})
+        const newCap = cap.add(new BN(1000000))
+
+        await gfi.setCap(newCap, {from: owner})
+        expect(await gfi.cap({from: owner})).to.bignumber.eq(newCap)
+      })
+
+      it("does not allow you to decrease the cap below the total supply", async () => {
+        const totalSupply = await gfi.totalSupply({from: owner})
+        const belowTotalSupply = totalSupply.sub(new BN(1))
+        expect(gfi.setCap(belowTotalSupply, {from: owner})).to.be.rejected
+      })
+
       it("emits an event", async () => {
-        const newConfig = await deployments.deploy("GoldfinchConfig", {from: owner})
-        await goldfinchConfig.setGoldfinchConfig(newConfig.address, {from: owner})
-        const tx = await gfi.updateGoldfinchConfig({from: owner})
-        expectEvent(tx, "GoldfinchConfigUpdated", {
-          who: owner,
-          configAddress: newConfig.address,
-        })
+        const cap = await gfi.cap({from: owner})
+        const newCap = cap.add(new BN(1000000))
+
+        const tx = await gfi.setCap(newCap, {from: owner})
+        expectEvent(tx, "CapUpdated", {cap: newCap})
+      })
+    })
+
+    describe("not as owner", () => {
+      it("forbids you from using the setCap function", async () => {
+        const cap = await gfi.cap({from: notOwner})
+        expect(gfi.setCap(new BN(100000000000), {from: notOwner})).to.be.rejected
+        expect(await gfi.cap({from: notOwner})).to.bignumber.eq(cap)
+      })
+    })
+  })
+
+  describe("mintTo", () => {
+    describe("as owner", () => {
+      it("minting over the cap fails", async () => {
+        const cap = await gfi.cap({from: owner})
+        const totalSupply = await gfi.totalSupply({from: owner})
+        const remainingUnderCap = cap.sub(totalSupply)
+        const overCap = remainingUnderCap.add(new BN(1))
+
+        expect(gfi.mintTo(owner!, overCap, {from: owner})).to.be.rejected
+      })
+
+      it("minting up to the cap succeeds", async () => {
+        const initialSupply = await gfi.totalSupply({from: owner})
+        const cap: BN = await gfi.cap({from: owner})
+        const remainingCap = cap.sub(initialSupply)
+
+        const initialBalance = await gfi.balanceOf(owner!)
+        const expectedFinalBalance = initialBalance.add(remainingCap)
+        await gfi.mintTo(owner!, remainingCap)
+        expect(await gfi.balanceOf(owner!)).to.bignumber.eq(expectedFinalBalance)
+      })
+    })
+
+    describe("not as owner", () => {
+      it("minting over the cap fails", async () => {
+        const totalSupply = await gfi.totalSupply({from: notOwner})
+        const cap = await gfi.cap({from: notOwner})
+        const remainingUnderCap = cap.sub(totalSupply)
+        const overCap = remainingUnderCap.add(new BN(1))
+
+        expect(gfi.setCap(overCap, {from: notOwner})).to.be.rejected
+        expect(await gfi.cap({from: notOwner})).to.bignumber.equal(cap)
+      })
+
+      it("minting under the cap fails", async () => {
+        const totalSupply = await gfi.totalSupply({from: notOwner})
+        const cap = await gfi.cap({from: notOwner})
+        const remainingUnderCap = cap.sub(totalSupply)
+        const underCap = remainingUnderCap.sub(new BN(1))
+
+        expect(gfi.setCap(underCap, {from: notOwner})).to.be.rejected
+        expect(await gfi.cap({from: notOwner})).to.bignumber.equal(cap)
       })
     })
   })
