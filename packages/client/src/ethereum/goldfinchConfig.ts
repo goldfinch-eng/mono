d57c4839732eae1038bfb480ac52089911c3b3c1
wbj-goldfinch <90154962+wbj-goldfinch@users.noreply.github.com>
2021-10-06 20:05:16 +0000

    remove other import errors

--- packages/client/src/ethereum/goldfinchConfig.ts
+++ packages/client/src/ethereum/goldfinchConfig.ts
@@ -1,5 +1,4 @@
 import {fetchDataFromAttributes} from "./utils"
-// @ts-expect-error ts-migrate(2307) FIXME: Cannot find module '@goldfinch-eng/protocol/blockc... Remove this comment to see the full error message
 import {CONFIG_KEYS} from "@goldfinch-eng/protocol/blockchain_scripts/configKeys"
 
 async function refreshGoldfinchConfigData(goldfinchConfigContract) {
