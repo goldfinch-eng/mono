8aeac21b9422eff5c72358370d5ae6c70bcf6eda
Jaênia <jaeniaps@gmail.com>
2021-10-11 17:11:06 -0300

    Refactors code to use props

--- packages/client/src/pages/rewards/Rewards.tsx
+++ packages/client/src/pages/rewards/Rewards.tsx
@@ -54,7 +54,7 @@ function RewardsSummary(props: RewardsSummaryProps) {
   )
 }
 
-function NoRewardsListItem(props) {
+function NoRewards(props) {
   return (
     <li className="table-row rewards-list-item no-rewards background-container">
       You have no rewards. You can earn rewards by supplying to&nbsp;
@@ -77,11 +77,16 @@ function ActionButton(props) {
   )
 }
 
-function RewardsListItem(props) {
-  // TODO: modify to get from prop
-  const communityRewards = true
-  const [accepted, setAccepted] = useState(communityRewards ? false : true)
-  const isTabletOrMobile = useMediaQuery({query: "(max-width: 900px)"})
+interface RewardsListItemProps {
+  isCommunityRewards: boolean
+  title: string
+  grantedGFI: string
+  claimableGFI: string
+}
+
+function RewardsListItem(props: RewardsListItemProps) {
+  const [accepted, setAccepted] = useState(props.isCommunityRewards ? false : true)
+  const isTabletOrMobile = useMediaQuery({query: "(max-width: 900px)"}) // equivalent to the sass variable $screen-l
 
   function handleAccept() {
     setAccepted(!accepted)
@@ -91,20 +96,34 @@ function RewardsListItem(props) {
 
   return (
     <>
+      {!isTabletOrMobile && (
+        <li className="rewards-list-item table-row background-container clickable">
+          <div className="table-cell col32">{props.title}</div>
+          <div className={`table-cell col20 numeric ${valueDisabledClass}`}>{props.grantedGFI}</div>
+          <div className={`table-cell col20 numeric ${valueDisabledClass}`}>{props.claimableGFI}</div>
+          {!accepted ? (
+            <ActionButton text="Accept" onClick={handleAccept} />
+          ) : (
+            <ActionButton text="Claim GFI" onClick={() => console.log("claim action")} />
+          )}
+          <button className="expand">{iconCarrotDown}</button>
+        </li>
+      )}
+
       {isTabletOrMobile && (
         <li className="mobile-ui rewards-list-item background-container clickable">
           <div className="rewards-list-item-header">
-            <div className="">Staked 16K FIDU on Nov 1</div>
+            <div>{props.title}</div>
             <button className="expand">{iconCarrotDown}</button>
           </div>
           <div className="item-details">
             <div className="detail-container">
               <span className="detail-label">Granted GFI</span>
-              <div className={`${valueDisabledClass} detail-value`}>50.34</div>
+              <div className={`${valueDisabledClass} detail-value`}>{props.grantedGFI}</div>
             </div>
             <div className="detail-container">
               <span className="detail-label">Claimable GFI</span>
-              <div className={`${valueDisabledClass} detail-value`}>4.03</div>
+              <div className={`${valueDisabledClass} detail-value`}>{props.claimableGFI}</div>
             </div>
           </div>
           {!accepted ? (
@@ -114,26 +133,34 @@ function RewardsListItem(props) {
           )}
         </li>
       )}
-
-      {!isTabletOrMobile && (
-        <li className="rewards-list-item table-row background-container clickable">
-          <div className="table-cell col32">Staked 16K FIDU on Nov 1</div>
-          <div className={`table-cell col20 numeric ${valueDisabledClass}`}>50.34</div>
-          <div className={`table-cell col20 numeric ${valueDisabledClass}`}>4.03</div>
-          {!accepted ? (
-            <ActionButton text="Accept" onClick={handleAccept} />
-          ) : (
-            <ActionButton text="Claim GFI" onClick={() => console.log("claim action")} disabled />
-          )}
-          <button className="expand">{iconCarrotDown}</button>
-        </li>
-      )}
     </>
   )
 }
 
 function Rewards(props) {
   const isTabletOrMobile = useMediaQuery({query: "(max-width: 900px)"})
+
+  // TODO: remove this variable when getting real data
+  const gfiRewards = [
+    {
+      isCommunityRewards: false,
+      title: "Staked 16K FIDU on Nov 1",
+      grantedGFI: "50.34",
+      claimableGFI: "4.03",
+    },
+    {
+      isCommunityRewards: true,
+      title: "Flight Academy Reward",
+      grantedGFI: "12,500.00",
+      claimableGFI: "0.00",
+    },
+    {
+      isCommunityRewards: true,
+      title: "Liquidity Provider Airdrop",
+      grantedGFI: "100,000.00",
+      claimableGFI: "50,000.00",
+    },
+  ]
 
   return (
     <div className="content-section">
@@ -154,10 +181,18 @@ function Rewards(props) {
           )}
         </div>
         <ul className="rewards-list">
-          <RewardsListItem />
-          <RewardsListItem />
-          <RewardsListItem />
-          {/* <NoRewardsListItem /> */}
+          {gfiRewards.length === 0 && <NoRewards />}
+
+          {gfiRewards.length > 0 &&
+            gfiRewards.map((item) => (
+              <RewardsListItem
+                key={item.title}
+                isCommunityRewards={item.isCommunityRewards}
+                title={item.title}
+                grantedGFI={item.grantedGFI}
+                claimableGFI={item.claimableGFI}
+              />
+            ))}
         </ul>
       </div>
     </div>
