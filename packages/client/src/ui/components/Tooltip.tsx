30e5168733dd9c07dab1c3bd19f825d7fcce173a
smakosh <ismai23l@hotmail.com>
2021-10-13 12:39:54 -0700

    styling: match the delay with the hover

--- packages/client/src/ui/components/Tooltip.tsx
+++ packages/client/src/ui/components/Tooltip.tsx
@@ -7,7 +7,7 @@ type TooltipProps = {
 }
 
 const Tooltip = ({children, className, id}: TooltipProps) => (
-  <ReactTooltip className={className} id={id} effect="solid" arrowColor="transparent" delayShow={200}>
+  <ReactTooltip className={className} id={id} effect="solid" arrowColor="transparent" delayShow={50}>
     {children}
   </ReactTooltip>
 )
