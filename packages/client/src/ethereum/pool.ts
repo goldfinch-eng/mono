0d091a39d9b8e0d9cff50da35b103432e9a65d35
Rebeca Sarai <rebecasaraiaguiar@gmail.com>
2021-10-19 18:11:47 -0300

    Fixes display of Fidu amount decimals

--- packages/client/src/ethereum/pool.ts
+++ packages/client/src/ethereum/pool.ts
@@ -2,7 +2,7 @@ import BigNumber from "bignumber.js"
 import {fetchDataFromAttributes, getPoolEvents, INTEREST_DECIMALS, USDC_DECIMALS} from "./utils"
 import {Tickers, usdcFromAtomic} from "./erc20"
 import {FIDU_DECIMALS, sharesToBalance, balanceInDollars} from "./fidu"
-import {getBlockInfo, getCurrentBlock, roundDownPenny, BlockInfo} from "../utils"
+import {getBlockInfo, getCurrentBlock, roundDownPenny, BlockInfo, displayNumber} from "../utils"
 import _ from "lodash"
 import {getBalanceAsOf, mapEventsToTx} from "./events"
 import {Contract, EventData} from "web3-eth-contract"
@@ -651,7 +651,7 @@ class StakedPosition {
   }
 
   get reason(): string {
-    const fiduAmount = this.amount.div(FIDU_DECIMALS.toString())
+    const fiduAmount = displayNumber(this.amount.div(FIDU_DECIMALS.toString()), 2)
     const date = new Date(this.rewards.startTime).toLocaleDateString(undefined, {
       month: "short",
       day: "numeric",
