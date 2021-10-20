8368bd4b9cccfafb5ea904a24930e1abeeb1a8b7
Jaênia <jaeniaps@gmail.com>
2021-10-20 11:32:16 -0300

    Improves structure of typing on theme

--- packages/client/src/styles/theme/styled.d.ts
+++ packages/client/src/styles/theme/styled.d.ts
@@ -1,68 +1,8 @@
 import "styled-components"
+import {defaultTheme} from "./theme"
 
-interface ColorsObject {
-  sandXxxDark: string
-  sandXxDark: string
-  sandXDark: string
-  sandDark: string
-  sand: string
-  sandLight: string
-  sandXLight: string
-
-  purpDark: string
-  purp: string
-  purpLight: string
-  purpXLight: string
-  purpXxLight: string
-
-  blueDark: string
-  blue: string
-  blueLight: string
-  blueXLight: string
-  blueXxLight: string
-
-  redXLight: string
-  red: string
-  redDark: string
-}
-
-interface FontSizesObject {
-  sansSizeXxs: string
-  sansSizeXs: string
-  sansSizeS: string
-  sansSizeM: string
-  sansSizeBase: string
-  sansSizeMl: string
-  sansSizeL: string
-  sansSizeXl: string
-  sansSizeXxl: string
-}
-
-interface TypographyObject {
-  fontSize: FontSizesObject
-}
-
-interface BreakPointsObject {
-  screenS: string
-  screenM: string
-  screenL: string
-}
-
-interface WidthsObject {
-  contentMaxWidth: string
-  navWidth: string
-}
-
-interface HeightsObject {
-  widgetButtonHeight: string
-}
+type ThemeInterface = typeof defaultTheme
 
 declare module "styled-components" {
-  export interface DefaultTheme {
-    colors: ColorsObject
-    typography: TypographyObject
-    breakpoints: BreakPointsObject
-    widths: WidthsObject
-    heights: HeightsObject
-  }
+  export interface DefaultTheme extends ThemeInterface {}
 }
--- packages/client/src/styles/theme/theme.ts
+++ packages/client/src/styles/theme/theme.ts
@@ -1,44 +1,10 @@
 import {DefaultTheme} from "styled-components"
+import colors from "./colors"
+import typography from "./typography"
 
 export const defaultTheme: DefaultTheme = {
-  colors: {
-    sandXxxDark: "#9e9994",
-    sandXxDark: "#b4ada7",
-    sandXDark: "#c3beb7",
-    sandDark: "#d6d1cc",
-    sand: "#e4e0dd",
-    sandLight: "#f1efed",
-    sandXLight: "#f8f6f4",
-
-    purpDark: "#483e5e",
-    purp: "#675b81",
-    purpLight: "#897da3",
-    purpXLight: "#ada5bf",
-    purpXxLight: "#d1ccdb",
-
-    blueDark: "#67afd7",
-    blue: "#75c1eb",
-    blueLight: "#96d3f5",
-    blueXLight: "#b6e4fe",
-    blueXxLight: "#ddeffa",
-
-    redXLight: "#fae6de",
-    red: "#e4af98",
-    redDark: "#cc937a",
-  },
-  typography: {
-    fontSize: {
-      sansSizeXxs: "12px",
-      sansSizeXs: "15px",
-      sansSizeS: "18px",
-      sansSizeM: "21px",
-      sansSizeBase: "24px",
-      sansSizeMl: "28px",
-      sansSizeL: "32px",
-      sansSizeXl: "36px",
-      sansSizeXxl: "42px",
-    },
-  },
+  colors,
+  typography,
   breakpoints: {
     screenS: "350px",
     screenM: "600px",
