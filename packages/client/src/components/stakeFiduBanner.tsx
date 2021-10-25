e04302e9a8d7eb8333a6c5e750e7af550591f2b9
Ian Hinsdale <ian@goldfinch.finance>
2021-10-25 10:24:14 -0400

    Edit comment.

--- packages/client/src/components/stakeFiduBanner.tsx
+++ packages/client/src/components/stakeFiduBanner.tsx
@@ -59,7 +59,7 @@ export default function StakeFiduBanner(props: StakeFiduBannerProps) {
   }
 
   // Being eligible for supplying into the senior pool is logically independent of, and therefore not
-  // necessary, for being able to stake any unstaked FIDU you may have. But for consistency of UX in
+  // necessary for, being able to stake any unstaked FIDU you may have. But for consistency of UX in
   // relation to the other actions on the senior pool page, we condition here on having satisfied the
   // same base requirement(s) that the other actions require.
   const userSatisfiesSeniorPoolRequirements = eligibleForSeniorPool(props.kyc, user)
