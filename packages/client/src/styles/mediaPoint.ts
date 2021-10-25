b7feffbb7aababe3d433fed77098ce7239409d4a
Jaênia <jaeniaps@gmail.com>
2021-10-25 09:40:56 -0300

    Adds typing to mediaPoint function

--- packages/client/src/styles/mediaPoint.ts
+++ packages/client/src/styles/mediaPoint.ts
@@ -1,4 +1,18 @@
-export function mediaPoint(theme) {
+interface ThemeBreakpoints {
+  breakpoints: {
+    screenS: string
+    screenM: string
+    screenL: string
+  }
+}
+
+interface MediaPoint {
+  screenS: string
+  screenM: string
+  screenL: string
+}
+
+export function mediaPoint(theme: ThemeBreakpoints): MediaPoint {
   const screenS = `@media (max-width: ${theme.breakpoints.screenS})`
   const screenM = `@media (max-width: ${theme.breakpoints.screenM})`
   const screenL = `@media (max-width: ${theme.breakpoints.screenL})`
