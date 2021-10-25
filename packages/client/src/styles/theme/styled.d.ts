f7956eded35166d1961892397523f35d08477e0f
Jaênia <jaeniaps@gmail.com>
2021-10-25 10:06:21 -0300

    Deletes styled.d.ts file

--- packages/client/src/styles/theme/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import "styled-components"
-import {defaultTheme} from "./theme"
-
-type ThemeInterface = typeof defaultTheme
-
-declare module "styled-components" {
-  export interface DefaultTheme extends ThemeInterface {}
-}
