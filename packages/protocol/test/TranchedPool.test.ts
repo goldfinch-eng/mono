a98c0b96de582d568262b8331afa0b8cfb357768
gregan <greg@goldfinch.finance>
2021-11-02 17:06:52 -0700

    Fix import conflict

--- packages/protocol/test/TranchedPool.test.ts
+++ packages/protocol/test/TranchedPool.test.ts
@@ -27,7 +27,6 @@ import {getApprovalDigest, getWallet} from "./permitHelpers"
 import {TranchedPoolInstance, PoolRewardsInstance} from "../typechain/truffle"
 import {JuniorTrancheLocked, DepositMade} from "../typechain/truffle/TranchedPool"
 import {CONFIG_KEYS} from "../blockchain_scripts/configKeys"
-import {JuniorTrancheLocked, DepositMade} from "../typechain/truffle/MigratedTranchedPool"
 import {assertNonNullable} from "@goldfinch-eng/utils"
 
 const RESERVE_FUNDS_COLLECTED_EVENT = "ReserveFundsCollected"
