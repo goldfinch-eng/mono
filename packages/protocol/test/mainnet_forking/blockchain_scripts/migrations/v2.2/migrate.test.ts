8327a0207674d49825b5f60299b3ea6828080de6
Mark Hudnall <me@markhudnall.com>
2021-11-19 17:00:56 -0800

    Change config on existing TranchedPools + more tests

--- packages/protocol/test/mainnet_forking/blockchain_scripts/migrations/v2.2/migrate.test.ts
+++ packages/protocol/test/mainnet_forking/blockchain_scripts/migrations/v2.2/migrate.test.ts
@@ -1,24 +1,56 @@
-import {getNamedAccounts, deployments} from "hardhat"
+import {ethers, getNamedAccounts, deployments} from "hardhat"
 import {fundWithWhales} from "@goldfinch-eng/protocol/blockchain_scripts/mainnetForkingHelpers"
 import {assertIsString} from "@goldfinch-eng/utils"
 import * as migrate from "@goldfinch-eng/protocol/blockchain_scripts/migrations/v2.2/migrate"
 import {
   ETHDecimals,
   getEthersContract,
+  getProtocolOwner,
   MINTER_ROLE,
   OWNER_ROLE,
   PAUSER_ROLE,
+  SIGNER_ROLE,
 } from "@goldfinch-eng/protocol/blockchain_scripts/deployHelpers"
 import {Awaited} from "@goldfinch-eng/protocol/blockchain_scripts/types"
 import {TEST_TIMEOUT} from "../../../MainnetForking.test"
 import {Deployment} from "hardhat-deploy/types"
 import {CONFIG_KEYS, CONFIG_KEYS_BY_TYPE} from "@goldfinch-eng/protocol/blockchain_scripts/configKeys"
-import {GoldfinchConfig} from "@goldfinch-eng/protocol/typechain/ethers"
+import {GoldfinchConfig, TranchedPool} from "@goldfinch-eng/protocol/typechain/ethers"
+import poolMetadata from "@goldfinch-eng/client/config/pool-metadata/mainnet.json"
 
 const performMigration = deployments.createFixture(async ({deployments}) => {
   await deployments.fixture("base_deploy", {keepExistingDeployments: true})
   return await migrate.main()
 })
+
+export function expectProxyOwner({toBe, forContracts}: {toBe: () => Promise<string>; forContracts: string[]}) {
+  describe("proxy owners", async () => {
+    forContracts.forEach((contractName) => {
+      it(`sets the correct proxy owner for ${contractName}`, async () => {
+        const proxyDeployment = await deployments.get(`${contractName}_Proxy`)
+        const proxyContract = await ethers.getContractAt(proxyDeployment.abi, proxyDeployment.address)
+        expect(await proxyContract.owner()).to.eq(await toBe())
+      })
+    })
+  })
+}
+
+export type RoleExpectation = {contractName: string; roles: string[]; address: () => Promise<string>}
+export function expectRoles(expectations: RoleExpectation[]) {
+  describe("roles", async () => {
+    for (const {contractName, roles, address} of expectations) {
+      for (const role of roles) {
+        it(`assigns the ${role} role`, async () => {
+          const addr = await address()
+          const deployment = await deployments.get(contractName)
+          const contract = await ethers.getContractAt(deployment.abi, deployment.address)
+
+          expect(await contract.hasRole(role, addr)).to.be.true
+        })
+      }
+    }
+  })
+}
 
 describe("v2.2 migration", async function () {
   this.timeout(TEST_TIMEOUT)
@@ -36,13 +68,15 @@ describe("v2.2 migration", async function () {
     const {gf_deployer} = await getNamedAccounts()
     assertIsString(gf_deployer)
     await fundWithWhales(["ETH"], [gf_deployer])
+    migration = await performMigration()
+  })
+
+  expectProxyOwner({
+    toBe: async () => getProtocolOwner(),
+    forContracts: ["StakingRewards", "CommunityRewards"],
   })
 
   context("GoldfinchConfig", async () => {
-    beforeEach(async () => {
-      migration = await performMigration()
-    })
-
     it("deploys a proxied GoldfinchConfig and initializes values", async () => {
       const newConfigDeployment = await deployments.get("GoldfinchConfig")
       const newConfig = migration.deployedContracts.config
@@ -87,18 +121,45 @@ describe("v2.2 migration", async function () {
         const contract = await getEthersContract(contractName)
         expect(await contract.config()).to.eq(newConfig.address)
       }
+
+      // Existing TranchedPool should also point to new config now
+      const tranchedPoolAddresses = Object.keys(poolMetadata)
+      const tranchedPoolContracts = await Promise.all(
+        tranchedPoolAddresses.map(async (address) => getEthersContract<TranchedPool>("TranchedPool", {at: address}))
+      )
+      for (const tranchedPool of tranchedPoolContracts) {
+        expect(await tranchedPool.config()).to.eq(newConfig.address)
+      }
     })
   })
 
-  // context("token launch", async () => {
-  //   it("deploys relevant contracts", async () => {})
+  context("token launch", async () => {
+    it("deploys staking / airdrop contracts", async () => {
+      for (const contractName of ["StakingRewards", "CommunityRewards"]) {
+        await expect(deployments.get(contractName)).to.not.be.rejected
+      }
+    })
 
-  //   it("assigns the DISTRIBUTOR_ROLE", async () => {})
+    // it("deploys merkle distributors", async () => {
+    //   for (const contractName of ["MerkleDistributor", "MerkleDirectDistributor"]) {
+    //     await expect(deployments.get(contractName)).to.not.be.rejected
+    //   }
+    // })
 
-  //   it("mints and distributes GFI to the correct addresses", async () => {})
-  // })
+    // expectRoles([
+    //   {
+    //     contractName: "CommunityRewards",
+    //     roles: [SIGNER_ROLE],
+    //     address: async () => (await deployments.get("MerkleDistributor")).address,
+    //   },
+    // ])
 
-  // context("other contracts", async () => {
-  //   it("deploys DynamicLeverageRatioStrategy", async () => {})
-  // })
+    // it("mints and distributes GFI to the correct addresses", async () => {})
+  })
+
+  context("other contracts", async () => {
+    it("deploys DynamicLeverageRatioStrategy", async () => {
+      await expect(deployments.get("DynamicLeverageRatioStrategy")).to.not.be.rejected
+    })
+  })
 })
