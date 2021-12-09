8227e933509405e4b7e76e9140722881889ff9d4
ihinsdale <ian@ianhinsdale.com>
2021-12-09 11:45:19 -0800

    Edit message.

--- packages/client/src/ethereum/currentBlock.ts
+++ packages/client/src/ethereum/currentBlock.ts
@@ -3,7 +3,7 @@ import {BlockInfo} from "../utils"
 export function checkSameBlock(currentBlock: BlockInfo, ...deps: Array<BlockInfo>): void {
   deps.forEach((dep: BlockInfo) => {
     if (currentBlock.number !== dep.number) {
-      const message = "Dependency is not based on current block number."
+      const message = `Dependency block number differs from current block number (${currentBlock.number}): ${dep.number}`
       if (process.env.NODE_ENV === "development" && process.env.REACT_APP_MURMURATION !== "yes") {
         throw new Error(message)
       } else {
