012541da94253208ebece3f6a4d6930875b39b72
Mariane Pastor <carolinep_16@hotmail.com>
2022-01-18 13:49:34 -0300

    Add regression test for withdrawal form (#232)
    * Add regression test for withdrawal form
    
    * Merge tests
    
    * remove unused method

--- packages/client/src/__tests__/components/withdrawalForm.test.tsx
+++ packages/client/src/__tests__/components/withdrawalForm.test.tsx
@@ -268,6 +268,26 @@ describe("withdrawal form", () => {
     })
   })
 
+  it("fills max amount and is not limited by the user balance", async () => {
+    const {user} = await setupClaimableStakingReward(goldfinchProtocol, seniorPool, currentBlock)
+    user.info.value.usdcBalanceInDollars = new BigNumber(0)
+
+    await mockCapitalProviderCalls()
+    const capitalProvider = await fetchCapitalProviderData(seniorPool, stakingRewards, gfi, user)
+    capitalProvider.value.availableToWithdrawInDollars = new BigNumber("10000")
+
+    const poolData = {
+      balance: new BigNumber(usdcToAtomic("50000000")),
+    }
+    renderWithdrawalForm(poolData, capitalProvider, undefined, undefined, currentBlock, undefined, undefined, user)
+
+    fireEvent.click(screen.getByText("Max", {selector: "button"}))
+
+    await waitFor(() => {
+      expect(screen.getByPlaceholderText("0")).toHaveProperty("value", "10,000")
+    })
+  })
+
   it("shows withdrawal form, to user with partially claimed staking reward", async () => {
     const {user} = await setupPartiallyClaimedStakingReward(goldfinchProtocol, seniorPool, undefined, currentBlock)
 
