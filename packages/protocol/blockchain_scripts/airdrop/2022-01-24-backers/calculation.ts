5f0e1592d9d501bbd8babc1c610b0791c1ef4c13
Mark Hudnall <me@markhudnall.com>
2022-01-26 16:53:56 -0800

    Fix lint errors

--- packages/protocol/blockchain_scripts/airdrop/2022-01-24-backers/calculation.ts
+++ packages/protocol/blockchain_scripts/airdrop/2022-01-24-backers/calculation.ts
@@ -3,11 +3,7 @@ import {BigNumber as BigNum} from "bignumber.js"
 import {parseCsv} from "../community/parseCsv"
 import {program} from "commander"
 import {decimals} from "@goldfinch-eng/protocol/test/testHelpers"
-import {
-  LIQUIDITY_PROVIDER_GRANT_REASON,
-} from "@goldfinch-eng/protocol/blockchain_scripts/merkle/merkleDistributor/types"
-
-async function getNotVestedContracts() {}
+import {LIQUIDITY_PROVIDER_GRANT_REASON} from "@goldfinch-eng/protocol/blockchain_scripts/merkle/merkleDistributor/types"
 
 interface RawCSV {
   poolName: string
@@ -26,55 +22,74 @@ interface RawCSV {
 
 interface PreparedRow {
   backerAddress: string
-  immediatelyAvailable: BigNum,
+  immediatelyAvailable: BigNum
   vesting: BigNum
 }
 
 function sanitizeData(data: RawCSV[]): PreparedRow[] {
-  const toBigInt = (s: string) => new BigNum(s.replace(/,/g, ""))
-    .multipliedBy(decimals.toString())
+  const toBigInt = (s: string) => new BigNum(s.replace(/,/g, "")).multipliedBy(decimals.toString())
 
-  return data.map(row => ({
-      backerAddress: row['Backer Address'],
-      immediatelyAvailable: toBigInt(row['Immeditely available']),
-      vesting: toBigInt(row['Unlocks over 12 months']),
+  return data.map((row) => ({
+    backerAddress: row["Backer Address"],
+    immediatelyAvailable: toBigInt(row["Immeditely available"]),
+    vesting: toBigInt(row["Unlocks over 12 months"]),
   }))
 }
 
 async function saveImmediateData(data: PreparedRow[], path: string) {
   console.log(`Saving ${data.length} rows for no-vesting data to ${path}`)
-  await fs.writeFile(path, JSON.stringify(data.map(row => ({
-    account: row.backerAddress,
-    reason: LIQUIDITY_PROVIDER_GRANT_REASON,
-    grant: {
-      amount: row.immediatelyAvailable.toFixed(),
-      cliffLength: "0",
-      vestingInterval: "0",
-      vestingLength: "0",
-    }
-  })), null, 2))
+  await fs.writeFile(
+    path,
+    JSON.stringify(
+      data.map((row) => ({
+        account: row.backerAddress,
+        reason: LIQUIDITY_PROVIDER_GRANT_REASON,
+        grant: {
+          amount: row.immediatelyAvailable.toFixed(),
+          cliffLength: "0",
+          vestingInterval: "0",
+          vestingLength: "0",
+        },
+      })),
+      null,
+      2
+    )
+  )
 }
 
 async function saveVestingData(data: PreparedRow[], path: string) {
   console.log(`Saving ${data.length} rows for no-vesting data to ${path}`)
-  await fs.writeFile(path, JSON.stringify(data.map(row => ({
-    account: row.backerAddress,
-    reason: LIQUIDITY_PROVIDER_GRANT_REASON,
-    grant: {
-      amount: row.vesting.toFixed(),
-      cliffLength: "0",
-      vestingInterval: "2628000",  //  1 month
-      vestingLength:  "31536000",  // 12 months
-    }
-  })), null, 2))
+  await fs.writeFile(
+    path,
+    JSON.stringify(
+      data.map((row) => ({
+        account: row.backerAddress,
+        reason: LIQUIDITY_PROVIDER_GRANT_REASON,
+        grant: {
+          amount: row.vesting.toFixed(),
+          cliffLength: "0",
+          vestingInterval: "2628000", //  1 month
+          vestingLength: "31536000", // 12 months
+        },
+      })),
+      null,
+      2
+    )
+  )
 }
 
 async function main(inputFile: string, immediatelyAvailableFile: string, vestingFile: string) {
   const data = await parseCsv<RawCSV>(inputFile)
   const sanitizedData = sanitizeData(data)
   await Promise.all([
-    saveImmediateData(sanitizedData.filter(row => !row.immediatelyAvailable.isZero()), immediatelyAvailableFile),
-    saveVestingData(sanitizedData.filter(row => !row.vesting.isZero()), vestingFile)
+    saveImmediateData(
+      sanitizedData.filter((row) => !row.immediatelyAvailable.isZero()),
+      immediatelyAvailableFile
+    ),
+    saveVestingData(
+      sanitizedData.filter((row) => !row.vesting.isZero()),
+      vestingFile
+    ),
   ])
 }
 
