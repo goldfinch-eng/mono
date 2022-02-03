f8ab0bad7b77cc655e8dcdc98a72d346bb76d042
Jaenia Sousa <jaeniaps@gmail.com>
2022-02-03 11:35:58 -0300

    Adds tooltip icon to Est. APY column name

--- packages/client/src/components/Earn/PoolList.tsx
+++ packages/client/src/components/Earn/PoolList.tsx
@@ -1,3 +1,5 @@
+import {InfoIcon} from "../../ui/icons"
+
 export default function PoolList({title, subtitle, children}) {
   return (
     <div className="pools-list table-spaced background-container">
@@ -5,7 +7,19 @@ export default function PoolList({title, subtitle, children}) {
       <div className="subtitle">{subtitle}</div>
       <div className="table-header background-container-inner">
         <div className="table-cell col40">Pool</div>
-        <div className="table-cell col16 numeric apy">Est. APY</div>
+        <div className="table-cell col16 numeric apy apy-column-name">
+          Est. APY
+          <span
+            className="tooltip-icon"
+            data-tip=""
+            data-for=""
+            data-offset="{'top': 0, 'left': 0}"
+            data-place="bottom"
+          >
+            <InfoIcon />
+          </span>
+        </div>
+
         <div className="table-cell col22 numeric limit">Pool Limit</div>
         <div className="table-cell col22 numeric balance">Your Balance</div>
       </div>
