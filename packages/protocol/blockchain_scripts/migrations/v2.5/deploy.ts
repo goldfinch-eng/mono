834770541161dae8220cbe03aab6eb0f11fd4ad3
gregan <greg@goldfinch.finance>
2022-03-01 13:58:35 -0800

    Write v2.5 migrations to setSupportedUIDs

--- packages/protocol/blockchain_scripts/migrations/v2.5/deploy.ts
+++ packages/protocol/blockchain_scripts/migrations/v2.5/deploy.ts
@@ -1,10 +1,16 @@
-import {ContractDeployer, ContractUpgrader} from "../../deployHelpers"
+import {ContractDeployer, ContractUpgrader, getEthersContract} from "../../deployHelpers"
 import hre from "hardhat"
-import {changeImplementations, DeployEffects} from "../deployEffects"
+import {changeImplementations, DeployEffects, getDeployEffects} from "../deployEffects"
+import {UniqueIdentity} from "@goldfinch-eng/protocol/typechain/ethers"
+import {deployments} from "hardhat"
 
 export async function deploy(deployEffects: DeployEffects) {
   const deployer = new ContractDeployer(console.log, hre)
   const upgrader = new ContractUpgrader(deployer)
+  const effects = await getDeployEffects({
+    title: "v2.5",
+    description: "Upgrade Go contract's goSeniorPool and update UniqueIdentity supportedUIDTypes",
+  })
 
   // 1.
   // Upgrade existing contracts
@@ -13,6 +19,19 @@ export async function deploy(deployEffects: DeployEffects) {
   })
 
   await deployEffects.add(await changeImplementations({contracts: upgradedContracts}))
+
+  // 2.
+  // Add support for US Entity and Non US Entity types to UID
+  const uniqueIdentity = await getEthersContract<UniqueIdentity>("UniqueIdentity", {
+    at: (await deployments.get("UniqueIdentity")).address,
+  })
+  await effects.add({
+    deferred: [
+      await uniqueIdentity.populateTransaction.setSupportedUIDTypes([0, 1, 2, 3, 4], [true, true, true, true, true]),
+    ],
+  })
+
+  await effects.executeDeferred()
 
   return {
     deployedContracts: {},
--- packages/protocol/test/mainnet_forking/blockchain_scripts/migrations/v2.5/migrate.test.ts
+++ packages/protocol/test/mainnet_forking/blockchain_scripts/migrations/v2.5/migrate.test.ts
@@ -7,12 +7,13 @@ import * as migrate2_5 from "@goldfinch-eng/protocol/blockchain_scripts/migratio
 import {expectOwnerRole, expectProxyOwner} from "@goldfinch-eng/protocol/test/testHelpers"
 import {TEST_TIMEOUT} from "../../../MainnetForking.test"
 import {impersonateAccount} from "@goldfinch-eng/protocol/blockchain_scripts/helpers/impersonateAccount"
-import {GoInstance} from "@goldfinch-eng/protocol/typechain/truffle"
+import {GoInstance, UniqueIdentityInstance} from "@goldfinch-eng/protocol/typechain/truffle"
 
 const setupTest = deployments.createFixture(async () => {
   await deployments.fixture("base_deploy", {keepExistingDeployments: true})
 
   const go = await getTruffleContract<GoInstance>("Go")
+  const uniqueIdentity = await getTruffleContract<UniqueIdentityInstance>("UniqueIdentity")
 
   const {gf_deployer} = await getNamedAccounts()
   assertIsString(gf_deployer)
@@ -20,27 +21,48 @@ const setupTest = deployments.createFixture(async () => {
   await impersonateAccount(hre, await getProtocolOwner())
   await fundWithWhales(["ETH"], [await getProtocolOwner()])
 
-  await migrate2_5.main()
-
   return {
     go,
+    uniqueIdentity,
   }
 })
 
 describe("v2.5", async function () {
+  let uniqueIdentity
   this.timeout(TEST_TIMEOUT)
 
   beforeEach(async () => {
-    await setupTest()
+    const setupTestResult = await setupTest()
+    uniqueIdentity = setupTestResult.uniqueIdentity
   })
 
-  expectProxyOwner({
-    toBe: getProtocolOwner,
-    forContracts: ["Go"],
+  it("v2.5 properly upgrades UniqueIdentity", async () => {
+    expect(await uniqueIdentity.supportedUIDTypes(0)).to.equal(true)
+    expect(await uniqueIdentity.supportedUIDTypes(1)).to.equal(true)
+    expect(await uniqueIdentity.supportedUIDTypes(2)).to.equal(true)
+    expect(await uniqueIdentity.supportedUIDTypes(3)).to.equal(false)
+    expect(await uniqueIdentity.supportedUIDTypes(4)).to.equal(false)
+    await migrate2_5.main()
+    expect(await uniqueIdentity.supportedUIDTypes(0)).to.equal(true)
+    expect(await uniqueIdentity.supportedUIDTypes(1)).to.equal(true)
+    expect(await uniqueIdentity.supportedUIDTypes(2)).to.equal(true)
+    expect(await uniqueIdentity.supportedUIDTypes(3)).to.equal(true)
+    expect(await uniqueIdentity.supportedUIDTypes(4)).to.equal(true)
   })
 
-  expectOwnerRole({
-    toBe: async () => getProtocolOwner(),
-    forContracts: ["Go"],
+  context("after deploy", async () => {
+    beforeEach(async () => {
+      await migrate2_5.main()
+    })
+
+    expectProxyOwner({
+      toBe: getProtocolOwner,
+      forContracts: ["Go"],
+    })
+
+    expectOwnerRole({
+      toBe: async () => getProtocolOwner(),
+      forContracts: ["Go"],
+    })
   })
 })
