ae2c085fd42f1263f36658c0645a8fafdb474314
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-03-09 17:53:36 -0500

    introduced nominal typing for useWallet()

--- packages/client2/lib/wallet/use-wallet.ts
+++ packages/client2/lib/wallet/use-wallet.ts
@@ -1,9 +1,25 @@
+import type { Web3Provider } from "@ethersproject/providers";
 import { getPriorityConnector } from "@web3-react/core";
+import type { Connector } from "@web3-react/types";
 
 import { metaMask, metaMaskHooks } from "./connectors/metamask";
 import { walletConnect, walletConnectHooks } from "./connectors/walletconnect";
 
-export function useWallet() {
+export function useWallet():
+  | {
+      isActive: false;
+      connector: Connector;
+      provider: undefined;
+      account: undefined;
+      error: Error | undefined;
+    }
+  | {
+      isActive: true;
+      connector: Connector;
+      provider: Web3Provider;
+      account: string;
+      error: Error | undefined;
+    } {
   // This hook basically runs through the given connectors and returns the first one that's actually connected
   // In this case we're telling it to prioritize MetaMask, then WalletConnect
   const {
@@ -17,11 +33,26 @@ export function useWallet() {
     [walletConnect, walletConnectHooks]
   );
 
+  const connector = usePriorityConnector();
+  const provider = usePriorityProvider();
+  const account = usePriorityAccount();
+  const isActive = usePriorityIsActive();
+  const error = usePriorityError();
+
+  if (isActive) {
+    return {
+      isActive: true,
+      connector,
+      provider: provider as Web3Provider,
+      account: account as string,
+      error,
+    };
+  }
   return {
-    connector: usePriorityConnector(),
-    provider: usePriorityProvider(),
-    account: usePriorityAccount(),
-    isActive: usePriorityIsActive(),
-    error: usePriorityError(),
+    isActive: false,
+    connector,
+    provider: undefined,
+    account: undefined,
+    error,
   };
 }
