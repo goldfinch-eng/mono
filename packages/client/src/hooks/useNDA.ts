9711f0eba384dd3372b876942b19f49d4d68ebf7
Rebeca Sarai <rebecasaraiaguiar@gmail.com>
2022-03-09 15:06:21 -0300

    Creates unauthenticated goldfinch client to fetch nda

--- packages/client/src/hooks/useNDA.ts
+++ packages/client/src/hooks/useNDA.ts
@@ -3,25 +3,20 @@ import {AppContext} from "../App"
 import {TranchedPool} from "../ethereum/tranchedPool"
 import {UserLoaded} from "../ethereum/user"
 import {useAsyncFn, useStaleWhileRevalidating} from "./useAsync"
-import DefaultGoldfinchClient from "./useGoldfinchClient"
+import {SimpleGoldfinchClient} from "./useGoldfinchClient"
 import useNonNullContext from "./useNonNullContext"
 import {useSignIn} from "./useSignIn"
 
 export function useFetchNDA({user, tranchedPool}: {user?: UserLoaded; tranchedPool?: TranchedPool}) {
-  const {network, setSessionData} = useNonNullContext(AppContext)
+  const {network} = useNonNullContext(AppContext)
   const [session] = useSignIn()
 
   const [result, refresh] = useAsyncFn(() => {
-    if (
-      session.status !== "authenticated" ||
-      !user ||
-      !tranchedPool?.address ||
-      !tranchedPool?.metadata?.NDAUrl ||
-      !tranchedPool?.metadata?.detailsUrl
-    ) {
+    if (!user || !tranchedPool?.address || !tranchedPool?.metadata?.NDAUrl || !tranchedPool?.metadata?.detailsUrl) {
       return
     }
-    const client = new DefaultGoldfinchClient(network.name!, session, setSessionData)
+
+    const client = new SimpleGoldfinchClient(network.name!)
     return client.fetchNDA(user.address, tranchedPool!.address).then((r) => r.json)
   }, [session.status, network, user?.address, tranchedPool?.address])
   const nda = useStaleWhileRevalidating(result)
