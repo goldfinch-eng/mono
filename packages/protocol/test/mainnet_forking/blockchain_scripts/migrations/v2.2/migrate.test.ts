6aa3f4b2f7a780f1bb24e97d13ea647deec53caa
Will Johnston <90154962+wbj-goldfinch@users.noreply.github.com>
2022-03-10 19:50:15 +0000

    skip

--- packages/protocol/test/mainnet_forking/blockchain_scripts/migrations/v2.2/migrate.test.ts
+++ packages/protocol/test/mainnet_forking/blockchain_scripts/migrations/v2.2/migrate.test.ts
@@ -23,7 +23,7 @@ const v231PerformMigration = deployments.createFixture(async () => {
   await migrate231.main()
 })
 
-xdescribe("V2.2 & v2.3 migration", async function () {
+describe.skip("V2.2 & v2.3 migration", async function () {
   this.timeout(TEST_TIMEOUT)
   let stakingRewards: StakingRewardsInstance
   let merkleDirectDistributor: MerkleDirectDistributorInstance
