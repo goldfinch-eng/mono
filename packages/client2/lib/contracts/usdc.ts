b8913999fc80f425304932d01b0289293f405abd
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-03-11 00:00:03 -0500

    useUsdcContract() result is now memoized

--- packages/client2/lib/contracts/usdc.ts
+++ packages/client2/lib/contracts/usdc.ts
@@ -1,12 +1,15 @@
+import { useMemo } from "react";
+
 import { CONTRACT_ADDRESSES } from "@/constants";
 import { useWallet } from "@/lib/wallet";
 import { Erc20__factory } from "@/types/ethers-contracts";
 
 export function useUsdcContract() {
   const { provider, chainId } = useWallet();
-  if (provider && chainId) {
-    const usdcAddress = CONTRACT_ADDRESSES[chainId].USDC;
-    const contract = Erc20__factory.connect(usdcAddress, provider.getSigner());
-    return contract;
-  }
+  return useMemo(() => {
+    if (provider && chainId) {
+      const usdcAddress = CONTRACT_ADDRESSES[chainId].USDC;
+      return Erc20__factory.connect(usdcAddress, provider.getSigner());
+    }
+  }, [provider, chainId]);
 }
