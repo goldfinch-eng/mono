b7384a3a7e7000ded95c4a83d7add44340274fc5
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-03-14 15:47:04 -0400

    added metadata for localhost pools

--- packages/client2/constants/index.ts
+++ packages/client2/constants/index.ts
@@ -1,4 +1,8 @@
+import localhostMetadata from "./metadata/localhost.json";
 import { mainnetMetadata } from "./metadata/mainnet";
 export * from "./contract-addresses";
 
-export const POOL_METADATA = mainnetMetadata;
+export const POOL_METADATA =
+  process.env.NEXT_PUBLIC_POOL_METADATA_NETWORK === "mainnet"
+    ? mainnetMetadata
+    : localhostMetadata;
