a258e3d53398c4aa14407e535a4f7269537b1edb
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-04-20 13:28:20 -0400

    removed some commented code

--- packages/client2/lib/state/app-wide-modals.tsx
+++ packages/client2/lib/state/app-wide-modals.tsx
@@ -2,13 +2,6 @@
  * The general rule of thumb for deciding whether or not to place a modal in here is simply that it should be in here if it serves a purpose that shows up in multiple places throughout the app.
  */
 import { useReactiveVar } from "@apollo/client";
-// import {
-//   createContext,
-//   useContext,
-//   useState,
-//   useCallback,
-//   ReactNode,
-// } from "react";
 
 import { WalletModal } from "@/components/wallet-modal";
 
