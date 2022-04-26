17bb5664494b1252f46bee9e3dc59bb201154792
Emily Hsia <emily.j.hsia@gmail.com>
2022-04-26 14:27:51 -0400

    [Staking] Disable legacy contract behavior on all environments except production

--- packages/client/src/ethereum/pool.ts
+++ packages/client/src/ethereum/pool.ts
@@ -907,8 +907,8 @@ class StakingRewards {
     this.legacyContract = goldfinchProtocol.getContract<Contract>(
       "StakingRewards",
       undefined,
-      // Disable this legacy contract behavior in the test and development environments, where it's not needed.
-      process.env.NODE_ENV !== "test" && process.env.NODE_ENV !== "development"
+      // Disable this legacy contract behavior in the test/development/murmuration environments, where it's not needed.
+      process.env.NODE_ENV === "production"
     )
     this.address = goldfinchProtocol.getAddress("StakingRewards")
     this.curvePool = getCurvePoolContract(goldfinchProtocol)
