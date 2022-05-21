78901adefeed99f4019c23e314c18fd590290023
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-05-21 12:13:55 -0400

    removed the pre-approved addresses from client2 repo

--- packages/client2/constants/approved-addresses/index.ts
+++
@@ -1,9 +0,0 @@
-import NonUSEntitiesJson from "./nonus-entities.json";
-import USEntitiesJson from "./us-accredited-entities.json";
-import USIndividualsJson from "./us-accredited-individuals.json";
-
-export const APPROVED_ADDRESSES = {
-  NonUSEntities: NonUSEntitiesJson.map((address) => address.toLowerCase()),
-  USEntities: USEntitiesJson.map((address) => address.toLowerCase()),
-  USIndividuals: USIndividualsJson.map((address) => address.toLowerCase()),
-};
