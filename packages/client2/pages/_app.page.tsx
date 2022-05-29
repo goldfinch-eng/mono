0b1f71a851a186df9d51949b8c98ed2b2fdfc3b7
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-05-29 12:42:37 -0400

    upgraded web3react packages, introduced Web3ReactProvider for a small performance boost

--- packages/client2/pages/_app.page.tsx
+++ packages/client2/pages/_app.page.tsx
@@ -10,24 +10,27 @@ import { Layout } from "@/components/layout";
 import { apolloClient } from "@/lib/graphql/apollo";
 import { useAppInitialization } from "@/lib/state/app-init";
 import { AppWideModals } from "@/lib/state/app-wide-modals";
+import { WalletProvider } from "@/lib/wallet";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   useAppInitialization();
   return (
-    <ApolloProvider client={apolloClient}>
-      <ToastContainer position="top-center" theme="colored" />
-      <Head>
-        <title>Goldfinch</title>
-        {/* remove this if we decide we want Google to index the app pages (unlikely) */}
-        <meta name="robots" content="noindex" />
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+    <WalletProvider>
+      <ApolloProvider client={apolloClient}>
+        <ToastContainer position="top-center" theme="colored" />
+        <Head>
+          <title>Goldfinch</title>
+          {/* remove this if we decide we want Google to index the app pages (unlikely) */}
+          <meta name="robots" content="noindex" />
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
 
-      <AppWideModals />
+        <AppWideModals />
 
-      {process.env.NEXT_PUBLIC_ENV === "local" && <DevToolsPanel />}
-    </ApolloProvider>
+        {process.env.NEXT_PUBLIC_ENV === "local" && <DevToolsPanel />}
+      </ApolloProvider>
+    </WalletProvider>
   );
 }
