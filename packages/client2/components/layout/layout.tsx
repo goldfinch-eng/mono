64fab0b162d9813961b0a0c3a98a60057a612e64
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-05-30 09:52:01 -0400

    fixed an issue where server-rendering broke because of portals

--- packages/client2/components/layout/layout.tsx
+++ packages/client2/components/layout/layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 
 import { Nav } from "../nav";
@@ -25,10 +25,33 @@ export function Layout({ children }: LayoutProps) {
   );
 }
 
+// Portals don't work properly during server-rendering, because `document` doesn't exist, so this workaround is necessary.
+function ClientOnlyPortal({
+  selector,
+  children,
+}: {
+  selector: string;
+  children: ReactNode;
+}) {
+  const [isMountedOnClient, setIsMountedOnClient] = useState(false);
+  useEffect(() => {
+    setIsMountedOnClient(true);
+  }, []);
+  return isMountedOnClient ? (
+    createPortal(children, document.querySelector(selector) as Element)
+  ) : (
+    <>{children}</>
+  );
+}
+
 export function SubnavPortal({ children }: { children: ReactNode }) {
-  return createPortal(children, document.getElementById(subnavId) as Element);
+  return (
+    <ClientOnlyPortal selector={`#${subnavId}`}>{children}</ClientOnlyPortal>
+  );
 }
 
 export function BannerPortal({ children }: { children: ReactNode }) {
-  return createPortal(children, document.getElementById(bannerId) as Element);
+  return (
+    <ClientOnlyPortal selector={`#${bannerId}`}>{children}</ClientOnlyPortal>
+  );
 }
