0f1e4138024bb64a3b1c6faae6a6743cd74984c7
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-05-31 15:04:23 -0400

    roughed out the layout for the senior pool page

--- packages/client2/pages/pools/senior/index.page.tsx
+++ packages/client2/pages/pools/senior/index.page.tsx
@@ -1,10 +1,16 @@
 import { gql } from "@apollo/client";
 
-import { Banner, Heading, HelperText } from "@/components/design-system";
+import {
+  Banner,
+  Breadcrumb,
+  Heading,
+  HelperText,
+} from "@/components/design-system";
 import { BannerPortal } from "@/components/layout";
 import { useSeniorPoolPageQuery } from "@/lib/graphql/generated";
 import { useWallet } from "@/lib/wallet";
 
+import goldfinchYellow from "./goldfinch-yellow.png";
 import {
   PortfolioSection,
   SENIOR_POOL_PORTFOLIO_USER_FIELDS,
@@ -52,16 +58,29 @@ export default function SeniorPoolPage() {
           expandedContent="This offering is only available to non-U.S. persons. This offering has not been registered under the U.S. Securities Act of 1933 (“Securities Act”), as amended, and may not be offered or sold in the United States or to a U.S. person (as defined in Regulation S promulgated under the Securities Act) absent registration or an applicable exemption from the registration requirements."
         />
       </BannerPortal>
-      <div className="space-y-6">
-        <Heading level={1}>Senior Pool</Heading>
-        {error ? (
-          <HelperText isError>
-            There was a problem fetching data on the senior pool. Shown data may
-            be outdated.
-          </HelperText>
-        ) : null}
-        <PortfolioSection user={data?.user} seniorPool={seniorPool} />
-        <StatusSection seniorPool={seniorPool} />
+      <div className="mb-6 flex justify-center lg:justify-start">
+        <Breadcrumb label="Goldfinch" image={goldfinchYellow.src} />
+      </div>
+      <Heading className="mb-12 text-center lg:text-left" level={1}>
+        Senior Pool
+      </Heading>
+      <div className="flex flex-col items-stretch gap-10 xl:grid xl:grid-cols-12">
+        <div className="xl:relative xl:col-start-9 xl:col-end-13">
+          <div className="flex flex-col items-stretch gap-8 xl:sticky xl:top-12">
+            <div>panel1</div>
+            <div>panel2</div>
+          </div>
+        </div>
+        <div className="xl:col-start-1 xl:col-end-9 xl:row-start-1">
+          {error ? (
+            <HelperText isError>
+              There was a problem fetching data on the senior pool. Shown data
+              may be outdated.
+            </HelperText>
+          ) : null}
+          <PortfolioSection user={data?.user} seniorPool={seniorPool} />
+          <StatusSection seniorPool={seniorPool} />
+        </div>
       </div>
     </>
   );
