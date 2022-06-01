7c1ff111963833ad79e0a5b7dfb37091b3b0acce
Vedran Sisic <vedran.sisic@gmail.com>
2022-06-01 18:17:12 -0400

    Removed comment

--- packages/client2/lib/state/mobile-nav.tsx
+++ packages/client2/lib/state/mobile-nav.tsx
@@ -1,6 +1,3 @@
-/**
- * The general rule of thumb for deciding whether or not to place a modal in here is simply that it should be in here if it serves a purpose that shows up in multiple places throughout the app.
- */
 import { useReactiveVar } from "@apollo/client";
 
 import { MobileNav } from "@/components/mobile-nav";
