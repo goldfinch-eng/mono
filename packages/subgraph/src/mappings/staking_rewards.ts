31bb1f3ae978aa07e076b89e2fab8e22af1ef707
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-06-01 21:50:34 -0400

    now indexing senior pool staking tokens in subgraph

--- packages/subgraph/src/mappings/staking_rewards.ts
+++ packages/subgraph/src/mappings/staking_rewards.ts
@@ -1,4 +1,11 @@
-import {RewardAdded, Staked, Unstaked} from "../../generated/templates/StakingRewards/StakingRewards"
+import {SeniorPoolStakedPosition} from "../../generated/schema"
+import {
+  RewardAdded,
+  Staked,
+  StakingRewards,
+  Unstaked,
+  Transfer,
+} from "../../generated/templates/StakingRewards/StakingRewards"
 
 import {updateCurrentEarnRate} from "../entities/staking_rewards"
 import {updateStakedSeniorPoolBalance} from "../entities/user"
@@ -10,9 +17,42 @@ export function handleRewardAdded(event: RewardAdded): void {
 export function handleStaked(event: Staked): void {
   updateCurrentEarnRate(event.address)
   updateStakedSeniorPoolBalance(event.params.user, event.params.amount)
+
+  const stakedPosition = new SeniorPoolStakedPosition(event.params.tokenId.toString())
+  stakedPosition.amount = event.params.amount
+  stakedPosition.user = event.params.user.toHexString()
+
+  const contract = StakingRewards.bind(event.address)
+  const positionsResult = contract.try_positions(event.params.tokenId)
+  if (!positionsResult.reverted) {
+    stakedPosition.startTime = positionsResult.value.value1.startTime
+    stakedPosition.endTime = positionsResult.value.value1.endTime
+  }
+
+  stakedPosition.save()
 }
 
 export function handleUnstaked(event: Unstaked): void {
   updateCurrentEarnRate(event.address)
   updateStakedSeniorPoolBalance(event.params.user, event.params.amount.neg())
+
+  const stakedPosition = assert(SeniorPoolStakedPosition.load(event.params.tokenId.toString()))
+  stakedPosition.amount = stakedPosition.amount.minus(event.params.amount)
+
+  stakedPosition.save()
+}
+
+export function handleTransfer(event: Transfer): void {
+  const stakedPosition = new SeniorPoolStakedPosition(event.params.tokenId.toString())
+  stakedPosition.user = event.params.to.toHexString()
+
+  const contract = StakingRewards.bind(event.address)
+  stakedPosition.amount = contract.stakedBalanceOf(event.params.tokenId)
+  const positionsResult = contract.try_positions(event.params.tokenId)
+  if (!positionsResult.reverted) {
+    stakedPosition.startTime = positionsResult.value.value1.startTime
+    stakedPosition.endTime = positionsResult.value.value1.endTime
+  }
+
+  stakedPosition.save()
 }
