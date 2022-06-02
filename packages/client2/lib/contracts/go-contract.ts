30d2b87b305b37755ce82b3c818d42b8a40470a4
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-06-02 13:23:17 -0400

    deleted go-contract.ts, it wasn't used because that data is in subgraph

--- packages/client2/lib/contracts/go-contract.ts
+++
@@ -1,9 +0,0 @@
-import { Web3Provider } from "@ethersproject/providers";
-
-import { CONTRACT_ADDRESSES } from "@/constants";
-import { Go__factory } from "@/types/ethers-contracts";
-
-export async function getGoContract(chainId: number, provider: Web3Provider) {
-  const goAddress = CONTRACT_ADDRESSES[chainId].Go;
-  return Go__factory.connect(goAddress, provider);
-}
--- packages/client2/lib/contracts/index.ts
+++ packages/client2/lib/contracts/index.ts
@@ -1,3 +1,2 @@
 export * from "./erc20-permit-signature";
-export * from "./go-contract";
 export * from "./use-contract";
