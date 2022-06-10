e2f0d1733e106ca48011919199d6140f37eff0a1
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-06-10 09:03:16 -0400

    fixed an issue where Transaction entities could sometimes not have unique identities

--- packages/subgraph/src/mappings/tranched_pool.ts
+++ packages/subgraph/src/mappings/tranched_pool.ts
@@ -28,7 +28,7 @@ export function handleCreditLineMigrated(event: CreditLineMigrated): void {
 export function handleDepositMade(event: DepositMade): void {
   handleDeposit(event)
 
-  const transaction = new Transaction(event.transaction.hash)
+  const transaction = new Transaction(event.transaction.hash.concatI32(event.logIndex.toI32()))
   transaction.category = "TRANCHED_POOL_DEPOSIT"
   transaction.user = event.params.owner.toHexString()
   transaction.tranchedPool = event.address.toHexString()
@@ -53,7 +53,7 @@ export function handleWithdrawalMade(event: WithdrawalMade): void {
   updatePoolCreditLine(event.address, event.block.timestamp)
   updateAllPoolBackers(event.address)
 
-  const transaction = new Transaction(event.transaction.hash)
+  const transaction = new Transaction(event.transaction.hash.concatI32(event.logIndex.toI32()))
   transaction.category = "TRANCHED_POOL_WITHDRAWAL"
   transaction.user = event.params.owner.toHexString()
   transaction.tranchedPool = event.address.toHexString()
@@ -89,7 +89,7 @@ export function handleDrawdownMade(event: DrawdownMade): void {
   updatePoolCreditLine(event.address, event.block.timestamp)
   updateAllPoolBackers(event.address)
 
-  const transaction = new Transaction(event.transaction.hash)
+  const transaction = new Transaction(event.transaction.hash.concatI32(event.logIndex.toI32()))
   transaction.category = "TRANCHED_POOL_DRAWDOWN"
   transaction.user = event.params.borrower.toHexString()
   transaction.tranchedPool = event.address.toHexString()
@@ -107,7 +107,7 @@ export function handlePaymentApplied(event: PaymentApplied): void {
   updatePoolCreditLine(event.address, event.block.timestamp)
   updateAllPoolBackersRewardsClaimable(event.address, event.block.timestamp)
 
-  const transaction = new Transaction(event.transaction.hash)
+  const transaction = new Transaction(event.transaction.hash.concatI32(event.logIndex.toI32()))
   transaction.category = "TRANCHED_POOL_REPAYMENT"
   transaction.user = event.params.payer.toHexString()
   transaction.tranchedPool = event.address.toHexString()
