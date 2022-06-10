c13b4d30f471df46d573ad5ea792e85af203504a
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-06-10 11:30:37 -0400

    removed the how it works tab from senior pool page, added back the How it Works link to docs

--- packages/client2/pages/pools/senior/index.page.tsx
+++ packages/client2/pages/pools/senior/index.page.tsx
@@ -6,11 +6,6 @@ import {
   Breadcrumb,
   Heading,
   HelperText,
-  TabGroup,
-  TabList,
-  TabButton,
-  TabPanels,
-  TabContent,
   Button,
 } from "@/components/design-system";
 import { BannerPortal } from "@/components/layout";
@@ -157,93 +152,40 @@ export default function SeniorPoolPage() {
           ) : null}
           {seniorPool ? <StatusSection seniorPool={seniorPool} /> : null}
           <div className="mb-8 mt-14">
-            <TabGroup>
-              <TabList>
-                <TabButton>Overview</TabButton>
-                <TabButton>How it works</TabButton>
-              </TabList>
-              <TabPanels>
-                <TabContent>
-                  <h2 className="mb-8 text-3xl">Overview</h2>
-                  <p className="mb-8 text-2xl font-light">
-                    The Senior Pool is the simple, lower risk, lower return
-                    option on Goldfinch. Capital is automatically diversified
-                    across Borrower pools, and protected by Backer capital.
-                  </p>
-                  <h3 className="mb-8 text-lg font-semibold">Highlights</h3>
-                  <ul className="mb-8 list-outside list-disc pl-5 text-lg">
-                    <li>
-                      Earn passive yield. Capital is automatically deployed
-                      across a diverse portfolio of Borrowers that are vetted by
-                      Backers.
-                    </li>
-                    <li>
-                      Lower risk. Losses are protected by the first-loss capital
-                      supplied by Backers.
-                    </li>
-                    <li>
-                      Stable returns. Receive USDC APY from the underlying
-                      interest, driven by real-world activity that is
-                      uncorrelated with crypto, plus GFI from liquidity mining
-                      distributions.
-                    </li>
-                  </ul>
-                  <TransactionTable />
-                </TabContent>
-                <TabContent>
-                  <h2 className="mb-8 text-3xl">How It Works</h2>
-                  <p>
-                    Liquidity Providers supply capital to the Senior Pool in
-                    order to earn passive yield. The Senior Pool automatically
-                    allocates their capital to the senior tranches of Borrower
-                    Pools.
-                  </p>
-                  <h3 className="my-8 text-lg font-semibold">
-                    Supplying to the Senior Pool
-                  </h3>
-                  <p className="mb-2">
-                    Liquidity Providers supply capital to the Senior Pool in
-                    order to earn passive yield. The Senior Pool then
-                    automatically allocates that capital across the senior
-                    tranches of Borrower Pools according to the Leverage Model.
-                    The Senior Pool thereby provides both diversification across
-                    Borrower Pools and seniority to the first-loss capital of
-                    Backers. This process does not involve seeking the
-                    permission of different Borrowers.
-                  </p>
-                  <p>
-                    To compensate Backers for both evaluating Borrowers Pools
-                    and providing first-loss capital, 20% of the Senior
-                    Pool&apos;s nominal interest is reallocated to Backers.
-                  </p>
-                  <h3 className="my-8 text-lg font-semibold">FIDU</h3>
-                  <p className="mb-2">
-                    When Liquidity Providers supply to the Senior Pool, they
-                    receive an equivalent amount of FIDU. FIDU is an ERC20
-                    token. At any time, Liquidity Providers can withdraw by
-                    redeeming their FIDU for USDC at an exchange rate based on
-                    the net asset value of the Senior Pool, minus a 0.5%
-                    withdrawal fee. This exchange rate for FIDU increases over
-                    time as interest payments are made back to the Senior Pool.
-                  </p>
-                  <p>
-                    It is possible that when a Liquidity Provider wants to
-                    withdraw, the Senior Pool may not have sufficient USDC
-                    because it has been borrowed by Borrowers. In this event,
-                    the Liquidity Provider may return when new capital enters
-                    the Senior Pool through Borrower repayments or new Liquidity
-                    Providers.
-                  </p>
-                  <h3 className="my-8 text-lg font-semibold">
-                    Summary of Liquidity Provider Incentives
-                  </h3>
-                  <p>
-                    Liquidity providers are incentivized to supply to the Senior
-                    Pool in order to earn passive yield.
-                  </p>
-                </TabContent>
-              </TabPanels>
-            </TabGroup>
+            <h2 className="mb-8 text-3xl">Overview</h2>
+            <p className="mb-8 text-2xl font-light">
+              The Senior Pool is the simple, lower risk, lower return option on
+              Goldfinch. Capital is automatically diversified across Borrower
+              pools, and protected by Backer capital.
+            </p>
+            <h3 className="mb-8 text-lg font-semibold">Highlights</h3>
+            <ul className="mb-8 list-outside list-disc pl-5 text-lg">
+              <li>
+                Earn passive yield. Capital is automatically deployed across a
+                diverse portfolio of Borrowers that are vetted by Backers.
+              </li>
+              <li>
+                Lower risk. Losses are protected by the first-loss capital
+                supplied by Backers.
+              </li>
+              <li>
+                Stable returns. Receive USDC APY from the underlying interest,
+                driven by real-world activity that is uncorrelated with crypto,
+                plus GFI from liquidity mining distributions.
+              </li>
+            </ul>
+            <div className="mb-10">
+              <Button
+                as="a"
+                size="lg"
+                href="https://docs.goldfinch.finance/goldfinch/protocol-mechanics/liquidityproviders"
+                iconRight="ArrowTopRight"
+                variant="rounded"
+              >
+                How it Works
+              </Button>
+            </div>
+            <TransactionTable />
           </div>
         </div>
       </div>
