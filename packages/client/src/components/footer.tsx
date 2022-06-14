f857548681cc0603dadc2346d6455072133adaca
Vedran Sisic <vedran.sisic@gmail.com>
2022-06-14 13:33:39 -0400

    Remove unused components

--- packages/client/src/components/footer.tsx
+++ packages/client/src/components/footer.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import {NavLink} from "react-router-dom"
-import {iconImmunefi, iconNexusMutual} from "./icons"
 
 const footerNavItems = [
   {label: "Terms", href: "/terms"},
