893bd28cc3a795b8ee2f74a24c0682aff7484098
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-06-14 15:19:30 -0400

    adjusted the populate-contract-addresses script so it consumes desired chain id env var

--- packages/client2/constants/contract-addresses/index.ts
+++
@@ -1,8 +0,0 @@
-import localhostAddresses from "./localhost.json";
-import mainnetAddresses from "./mainnet.json";
-
-export const CONTRACT_ADDRESSES: { [chainId: number]: Record<string, string> } =
-  {
-    1: mainnetAddresses,
-    31337: localhostAddresses,
-  };
--- packages/client2/scripts/populate-contract-addresses.ts
+++ packages/client2/scripts/populate-contract-addresses.ts
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-console */
 import fs from "fs";
 import path from "path";
+
+const nextEnv = require("@next/env");
+const prettier = require("prettier");
+
+const env = nextEnv.loadEnvConfig(".");
 
 console.log("Gathering local and mainnet contract addresses...");
 const localDeployments = JSON.parse(
@@ -20,30 +26,36 @@ const mainnetDeployments = JSON.parse(
 );
 const mainnetContracts = mainnetDeployments["1"].mainnet.contracts;
 
-[
-  {
-    relativePath: "../constants/contract-addresses/localhost.json",
-    contracts: localContracts,
-  },
-  {
-    relativePath: "../constants/contract-addresses/mainnet.json",
-    contracts: mainnetContracts,
-  },
-].forEach(({ relativePath, contracts }) => {
-  const addresses = {
-    USDC:
-      contracts.TestERC20?.address ??
-      "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // mainnet doesn't have TestERC20 (obviously), use the actual USDC mainnet address instead
-    SeniorPool: contracts.SeniorPool.address,
-    GFI: contracts.GFI.address,
-    Fidu: contracts.Fidu.address,
-    UniqueIdentity: contracts.UniqueIdentity.address,
-    Go: contracts.Go.address,
-    StakingRewards: contracts.StakingRewards.address,
-    Zapper: contracts.Zapper.address,
-  };
-  const pathname = path.resolve(__dirname, relativePath);
-  fs.writeFileSync(pathname, JSON.stringify(addresses, null, 2));
-});
+const desiredChainId = env.combinedEnv.NEXT_PUBLIC_DESIRED_CHAIN_ID as string;
+const contractAddressFileRelativePath =
+  "../constants/contract-addresses/index.ts";
+const contracts =
+  desiredChainId === "1"
+    ? mainnetContracts
+    : desiredChainId === "31337"
+    ? localContracts
+    : null;
+if (contracts === null) {
+  throw new Error(`Invalid desired chain id: ${desiredChainId}.`);
+}
+const addresses = {
+  USDC:
+    contracts.TestERC20?.address ??
+    "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // mainnet doesn't have TestERC20 (obviously), use the actual USDC mainnet address instead
+  SeniorPool: contracts.SeniorPool.address,
+  GFI: contracts.GFI.address,
+  Fidu: contracts.Fidu.address,
+  UniqueIdentity: contracts.UniqueIdentity.address,
+  Go: contracts.Go.address,
+  StakingRewards: contracts.StakingRewards.address,
+  Zapper: contracts.Zapper.address,
+};
+const code = `export const CONTRACT_ADDRESSES = {${desiredChainId}: ${JSON.stringify(
+  addresses
+)}}`;
+fs.writeFileSync(
+  path.resolve(__dirname, contractAddressFileRelativePath),
+  prettier.format(code, { parser: "typescript" })
+);
 
 console.log("Finished gathering local and mainnet contract addresses");
