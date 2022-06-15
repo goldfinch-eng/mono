c561ffd09370e87b40df5303eb837309e1daba15
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-06-15 01:36:30 -0400

    refactor to eliminate a lot of repeated code in deal terms

--- packages/client2/pages/pools/[address]/deal-terms-table.tsx
+++ packages/client2/pages/pools/[address]/deal-terms-table.tsx
@@ -61,141 +61,82 @@ export default function DealTermsTable({ tranchedPool }: DealTermsProps) {
       {!tranchedPool ? (
         <ShimmerLines truncateFirstLine={false} lines={8} />
       ) : (
-        <table className="w-full border border-sand-200">
-          <tbody>
-            <tr>
-              <th className="max-w-xs border-b border-sand-200 bg-sand-50 p-5 font-medium">
-                <div className="flex items-center justify-between">
-                  Interest Rate
-                  <InfoIconTooltip
-                    size="sm"
-                    content="The Pool's fixed interest rate APR."
-                  />
-                </div>
-              </th>
-              <td className="border-b border-sand-200 p-5">
-                {formatPercent(tranchedPool.creditLine.interestAprDecimal)}
-              </td>
-            </tr>
-            <tr>
-              <th className="max-w-xs border-b border-sand-200 bg-sand-50 p-5 font-medium">
-                <div className="flex items-center justify-between">
-                  Drawdown cap
-                  <InfoIconTooltip
-                    size="sm"
-                    content="The total funds that the Borrower can drawdown from this Pool."
-                  />
-                </div>
-              </th>
-              <td className="border-b border-sand-200 p-5">
-                {formatCrypto({
-                  token: SupportedCrypto.Usdc,
-                  amount: tranchedPool.creditLine.maxLimit,
-                })}
-              </td>
-            </tr>
-            <tr>
-              <th className="max-w-xs border-b border-sand-200 bg-sand-50 p-5 font-medium">
-                <div className="flex items-center justify-between">
-                  Payment frequency
-                  <InfoIconTooltip
-                    size="sm"
-                    content="Frequency of interest and principal payments."
-                  />
-                </div>
-              </th>
-              <td className="border-b border-sand-200 p-5">
-                {tranchedPool.creditLine.paymentPeriodInDays.toString()} days
-              </td>
-            </tr>
-            <tr>
-              <th className="max-w-xs border-b border-sand-200 bg-sand-50 p-5 font-medium">
-                <div className="flex items-center justify-between">
-                  Payment term
-                  <InfoIconTooltip
-                    size="sm"
-                    content="The length of time until the full principal is due."
-                  />
-                </div>
-              </th>
-              <td className="border-b border-sand-200 p-5">
-                {tranchedPool.creditLine.termInDays.toNumber()} days
-              </td>
-            </tr>
-            <tr>
-              <th className="max-w-xs border-b border-sand-200 bg-sand-50 p-5 font-medium">
-                <div className="flex items-center justify-between">
-                  Default interest rate
-                  <InfoIconTooltip
-                    size="sm"
-                    content="An additional interest rate paid by the Borrower if they are late on their payments following a 30 day grace period. The total interest rate a Borrower in default pays = interest rate + default interest rate."
-                  />
-                </div>
-              </th>
-              <td className="border-b border-sand-200 p-5">
-                {formatPercent(0.1)}
-              </td>
-            </tr>
-            <tr>
-              <th className="max-w-xs border-b border-sand-200 bg-sand-50 p-5 font-medium">
-                <div className="flex items-center justify-between">
-                  Current leverage ratio
-                  <InfoIconTooltip
-                    size="sm"
-                    content="The leverage of senior tranche to junior tranche capital in this Pool. Senior tranche capital is automatically allocated by Goldfinch's Senior Pool, according to the protocol's leverage model. Junior tranche capital is provided directly by Backer deposits. A current leverage ratio of 4x means that for every $1 of junior capital deposited by Backers, $4 of senior capital will be allocated by the Senior Pool."
-                  />
-                </div>
-              </th>
-              <td className="border-b border-sand-200 p-5">
-                {tranchedPool.estimatedLeverageRatio.toNumber()}
-              </td>
-            </tr>
-            <tr>
-              <th className="max-w-xs border-b border-sand-200 bg-sand-50 p-5 font-medium">
-                <div className="flex items-center justify-between">
-                  Opening date
-                  <InfoIconTooltip
-                    size="sm"
-                    content="The date that the Pool will be open for Backer investments."
-                  />
-                </div>
-              </th>
-              <td className="border-b border-sand-200 p-5">
-                {format(
-                  new Date(tranchedPool.fundableAt.toNumber() * 1000),
-                  "MMMM d, y"
-                )}
-              </td>
-            </tr>
-            <tr>
-              <th className="max-w-xs border-b border-sand-200 bg-sand-50 p-5 font-medium">
-                <div className="flex items-center justify-between">
-                  Contract address
-                  <InfoIconTooltip
-                    size="sm"
-                    content="The Ethereum address for this Borrower Pool smart contract."
-                  />
-                </div>
-              </th>
-              <td className="border-b border-sand-200 p-5">
-                {tranchedPool.id}
-              </td>
-            </tr>
-            <tr>
-              <th className="max-w-xs border-b border-sand-200 bg-sand-50 p-5 font-medium">
-                <div className="flex items-center justify-between">
-                  Borrower address
-                  <InfoIconTooltip
-                    size="sm"
-                    content="The Ethereum address associated with this Borrower."
-                  />
-                </div>
-              </th>
-              <td className="border-b border-sand-200 p-5">0x</td>
-            </tr>
-          </tbody>
-        </table>
+        <Table
+          rows={[
+            [
+              "Interest Rate",
+              "The Pool's fixed interest rate APR.",
+              formatPercent(tranchedPool.creditLine.interestAprDecimal),
+            ],
+            [
+              "Drawdown cap",
+              "The total funds that the Borrower can drawdown from this Pool.",
+              formatCrypto({
+                token: SupportedCrypto.Usdc,
+                amount: tranchedPool.creditLine.maxLimit,
+              }),
+            ],
+            [
+              "Payment frequency",
+              "Frequency of interest and principal payments.",
+              `${tranchedPool.creditLine.paymentPeriodInDays.toString()} days`,
+            ],
+            [
+              "Payment term",
+              "The length of time until the full principal is due.",
+              `${tranchedPool.creditLine.termInDays.toString()} days`,
+            ],
+            [
+              "Default interest rate",
+              "An additional interest rate paid by the Borrower if they are late on their payments following a 30 day grace period. The total interest rate a Borrower in default pays = interest rate + default interest rate.",
+              formatPercent(0.1),
+            ],
+            [
+              "Current leverage ratio",
+              "The leverage of senior tranche to junior tranche capital in this Pool. Senior tranche capital is automatically allocated by Goldfinch's Senior Pool, according to the protocol's leverage model. Junior tranche capital is provided directly by Backer deposits. A current leverage ratio of 4x means that for every $1 of junior capital deposited by Backers, $4 of senior capital will be allocated by the Senior Pool.",
+              tranchedPool.estimatedLeverageRatio.toString(),
+            ],
+            [
+              "Opening date",
+              "The date that the Pool will be open for Backer investments.",
+              format(
+                new Date(tranchedPool.fundableAt.toNumber() * 1000),
+                "MMMM d, y"
+              ),
+            ],
+            [
+              "Contract address",
+              "The Ethereum address for this Borrower Pool smart contract.",
+              tranchedPool.id,
+            ],
+            [
+              "Borrower address",
+              "The Ethereum address associated with this Borrower.",
+              "0x",
+            ],
+          ]}
+        />
       )}
     </div>
+  );
+}
+
+function Table({ rows }: { rows: [string, string, string][] }) {
+  return (
+    <table className="w-full border-collapse border border-sand-200 text-sand-600">
+      <tbody>
+        {rows.map(([heading, tooltip, value], index) => (
+          <tr key={index} className="border border-sand-200">
+            <th scope="row" className="p-5 font-medium">
+              <div className="flex items-center justify-between">
+                <div>{heading}</div>
+                <InfoIconTooltip size="sm" content={tooltip} />
+              </div>
+            </th>
+            <td className="p-5">{value}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
