0c89305df4839f3c1acf38a3fa76e05fc7346cd5
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-06-15 16:30:36 -0400

    fixed an issue where non-metamask connectors had the wrong RPC URLs

--- packages/client2/lib/wallet/chains.ts
+++ packages/client2/lib/wallet/chains.ts
@@ -1,13 +1,7 @@
-import { DESIRED_CHAIN_ID } from "@/constants";
-
-export const RPC_URLS: { [chainId: number]: string } =
-  process.env.NODE_ENV === "production"
-    ? {
-        1: `https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
-      }
-    : {
-        1: `https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
-        31337: "http://localhost:8545",
-      };
-
-export const ALLOWED_CHAIN_IDS = [DESIRED_CHAIN_ID];
+export const RPC_URLS: { [chainId: number]: string } = {
+  1: `https://eth-mainnet.alchemyapi.io/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`,
+  31337:
+    process.env.NEXT_PUBLIC_NETWORK_NAME === "murmuration"
+      ? "https://murmuration.goldfinch.finance/_chain"
+      : "http://localhost:8545",
+};
