cc0006013d02c4bc1e88b0ae32a4506a997065dc
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-06-24 11:03:17 -0400

    added a /api/geolocation endpoint to obscure the use of ipinfo (so it doesn't get blocked hopefully)

--- packages/client2/lib/geolocation.ts
+++ packages/client2/lib/geolocation.ts
@@ -1,36 +1,13 @@
-interface GeolocationData {
-  ip: string;
-  city: string;
-  region: string;
-  country: string;
-  loc: string;
-  org: string;
-  postal: string;
-  timezone: string;
-}
+import { Geolocation } from "./graphql/generated";
 
-export async function fetchViewerGeolocation(): Promise<GeolocationData> {
+export async function fetchViewerGeolocation(): Promise<Geolocation> {
   if (process.env.NODE_ENV !== "production") {
-    return Promise.resolve({
-      ip: "123.456.789.0",
-      city: "Oakland",
-      region: "California",
-      country: "US",
-      loc: "37.8044,-122.2708",
-      org: "AS701 MCI Communications Services, Inc. d/b/a Verizon Business",
-      postal: "94604",
-      timezone: "America/Los_Angeles",
-    });
+    return Promise.resolve({ country: "US" });
   } else {
-    const response = await fetch("https://ipinfo.io?token=679544298a8c59", {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (response.ok) {
-      return (await response.json()) as GeolocationData;
-    } else {
-      return Promise.reject(response);
+    const response = await fetch("/api/geolocation");
+    if (!response.ok) {
+      throw new Error("Could not get country");
     }
+    return (await response.json()) as Geolocation;
   }
 }
