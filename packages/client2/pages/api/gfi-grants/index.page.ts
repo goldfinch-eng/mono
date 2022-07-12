31dc9347583b3a9ee9447bdec81d213d7c9a9fd8
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-07-12 12:56:43 -0400

    first shot at building a /gfi-grants API endpoint to find matching grants of a user

---
+++ packages/client2/pages/api/gfi-grants/index.page.ts
@@ -0,0 +1,105 @@
+import fs from "fs";
+import path from "path";
+
+import { withSentry } from "@sentry/nextjs";
+import { NextApiRequest, NextApiResponse } from "next";
+
+type ExpectedQuery = {
+  account: string;
+};
+
+type Grant = {
+  amount: string;
+  vestingLength: string;
+  cliffLength: string;
+  vestingInterval: string;
+};
+
+type DirectGrant = {
+  amount: string;
+};
+
+type GrantManifest = {
+  merkleRoot: string;
+  amountTotal: string;
+  grants: {
+    index: number;
+    account: string;
+    reason: string;
+    grant: Grant | DirectGrant;
+    proof: string[];
+  }[];
+};
+
+const fileToSource = {
+  "./merkleDistributorInfo.json": "merkle",
+  "./merkleDistributorInfo.dev.json": "merkle",
+  "./backerMerkleDistributorInfo.json": "backerMerkle",
+  "./backerMerkleDistributorInfo.dev.json": "backerMerkle",
+  "./merkleDirectDistributorInfo.json": "merkleDirect",
+  "./merkleDirectDistributorInfo.dev.json": "merkleDirect",
+  "./backerMerkleDirectDistributorInfo.json": "backerMerkleDirect",
+  "./backerMerkleDirectDistributorInfo.dev.json": "backerMerkleDirect",
+};
+
+const merkleDistributorFiles: (keyof typeof fileToSource)[] =
+  process.env.NEXT_PUBLIC_NETWORK_NAME === "mainnet"
+    ? ["./merkleDistributorInfo.json", "./backerMerkleDistributorInfo.json"]
+    : [
+        "./merkleDistributorInfo.dev.json",
+        "./backerMerkleDistributorInfo.dev.json",
+      ];
+
+const merkleDirectDistributorFiles: (keyof typeof fileToSource)[] =
+  process.env.NEXT_PUBLIC_NETWORK_NAME === "mainnet"
+    ? [
+        "./merkleDirectDistributorInfo.json",
+        "./backerMerkleDirectDistributorInfo.json",
+      ]
+    : [
+        "./merkleDirectDistributorInfo.dev.json",
+        "./backerMerkleDirectDistributorInfo.dev.json",
+      ];
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const account = (req.query as ExpectedQuery).account;
+  if (!account) {
+    res.status(500).json({
+      message: "You must provide an 'account' parameter in the body.",
+    });
+    return;
+  }
+  try {
+    const filesToSearch = merkleDistributorFiles.concat(
+      merkleDirectDistributorFiles
+    );
+    const matchingGrants = (
+      await Promise.all(
+        filesToSearch.map((file) => findMatchingGrants(account, file))
+      )
+    ).flat();
+
+    res.status(200).json({ account, matchingGrants });
+  } catch (e) {
+    res
+      .status(500)
+      .json({ message: `Error searching grants: ${(e as Error).message}` });
+  }
+};
+
+async function findMatchingGrants(account: string, file: string) {
+  const pathname = path.resolve(`${process.cwd()}/pages/api/gfi-grants`, file);
+  const grantManifest: GrantManifest = JSON.parse(
+    await fs.promises.readFile(pathname, "utf8")
+  );
+  let matchingGrants = grantManifest.grants.filter(
+    (grant) => grant.account.toLowerCase() === account.toLowerCase()
+  );
+  matchingGrants = matchingGrants.map((g) => ({
+    source: fileToSource[file as keyof typeof fileToSource],
+    ...g,
+  }));
+  return matchingGrants;
+}
+
+export default withSentry(handler);
