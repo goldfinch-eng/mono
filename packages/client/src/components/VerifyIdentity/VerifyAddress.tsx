8db821147532f82f01a466c2c86667a042697599
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-07-13 15:04:35 -0400

    oops, forgot to update an import

--- packages/client/src/components/VerifyIdentity/VerifyAddress.tsx
+++ packages/client/src/components/VerifyIdentity/VerifyAddress.tsx
@@ -11,7 +11,11 @@ import USForm from "./USForm"
 import VerifyCard from "./VerifyCard"
 import {Action, CREATE_UID, US_COUNTRY_CODE} from "./constants"
 import ErrorCard from "./ErrorCard"
-import {isNonUSEntity, isUSAccreditedEntity, isUSAccreditedIndividual} from "@goldfinch-eng/utils"
+import {
+  isApprovedNonUSEntity,
+  isApprovedUSAccreditedEntity,
+  isApprovedUSAccreditedIndividual,
+} from "@goldfinch-eng/utils"
 import USAccreditedForm from "./USAccreditedForm"
 import Banner from "../banner"
 import {UIDTypeToBalance, User} from "../../ethereum/user"
@@ -25,9 +29,9 @@ export const ENTITY_ENTITY_TYPE = "Entity"
 function isEligible(kyc: KYC | undefined, user: User): boolean {
   return (
     (kyc?.status === "approved" && kyc?.countryCode !== "") ||
-    isNonUSEntity(user?.address) ||
-    isUSAccreditedEntity(user?.address) ||
-    isUSAccreditedIndividual(user?.address)
+    isApprovedNonUSEntity(user?.address) ||
+    isApprovedUSAccreditedEntity(user?.address) ||
+    isApprovedUSAccreditedIndividual(user?.address)
   )
 }
 
@@ -162,9 +166,13 @@ export default function VerifyAddress({disabled, dispatch}: {disabled: boolean; 
         onEvent={() => fetchKYCStatus(session)}
       />
     )
-  } else if (entityType === US_ACCREDITED_INDIVIDUAL_ENTITY_TYPE && !isUSAccreditedIndividual(user.address)) {
+  } else if (entityType === US_ACCREDITED_INDIVIDUAL_ENTITY_TYPE && !isApprovedUSAccreditedIndividual(user.address)) {
     return <USAccreditedForm onClose={() => setEntityType("")} />
-  } else if (entityType === ENTITY_ENTITY_TYPE && !isUSAccreditedEntity(user.address) && !isNonUSEntity(user.address)) {
+  } else if (
+    entityType === ENTITY_ENTITY_TYPE &&
+    !isApprovedUSAccreditedEntity(user.address) &&
+    !isApprovedNonUSEntity(user.address)
+  ) {
     return <EntityForm onClose={() => setEntityType("")} />
   } else if (isEligible(kyc, user)) {
     return <Banner icon={iconCircleCheck}>Your verification was approved.</Banner>
