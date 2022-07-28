48ba41662ccb49833c4f7cfc87f84837d877a775
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-07-28 13:05:38 -0400

    added startTime/endTime to indexed community reward tokens

--- packages/subgraph/src/mappings/community_rewards.ts
+++ packages/subgraph/src/mappings/community_rewards.ts
@@ -1,5 +1,21 @@
 import {CommunityRewardsToken} from "../../generated/schema"
-import {RewardPaid, GrantRevoked} from "../../generated/templates/CommunityRewards/CommunityRewards"
+import {
+  CommunityRewards as CommunityRewardsContract,
+  Granted,
+  RewardPaid,
+  GrantRevoked,
+} from "../../generated/templates/CommunityRewards/CommunityRewards"
+
+// Seems redundant, but this handler gets used to add the startTime/endTime info on tokens
+// Remember that this actually runs _before_ GrantAccepted. We can let GrantAccepted fill out the other details.
+export function handleGranted(event: Granted): void {
+  const communityRewardsToken = new CommunityRewardsToken(event.params.tokenId.toString())
+  const communityRewardsContract = CommunityRewardsContract.bind(event.address)
+  const tokenLaunchTime = communityRewardsContract.tokenLaunchTimeInSeconds()
+  communityRewardsToken.startTime = tokenLaunchTime
+  communityRewardsToken.endTime = communityRewardsToken.startTime.plus(event.params.vestingLength)
+  communityRewardsToken.save()
+}
 
 export function handleRewardPaid(event: RewardPaid): void {
   const communityRewardsToken = assert(CommunityRewardsToken.load(event.params.tokenId.toString()))
