13612e3f902aebc3b657a93ecab18633bc90c254
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-07-29 10:54:43 -0400

    some cosmetic formatting

--- packages/client2/pages/gfi/grant-card.tsx
+++ packages/client2/pages/gfi/grant-card.tsx
@@ -1,6 +1,11 @@
+import { format } from "date-fns";
 import { BigNumber } from "ethers";
 
+import { formatCrypto } from "@/lib/format";
 import { GrantWithToken, getReasonLabel } from "@/lib/gfi-rewards";
+import { SupportedCrypto } from "@/lib/graphql/generated";
+
+const TOKEN_LAUNCH_TIME = 1641920400000; // Tuesday, January 11, 2022 09:00:00 AM GMT-08:00
 
 interface GrantCardProps {
   grant: GrantWithToken;
@@ -11,12 +16,22 @@ export function GrantCard({ grant }: GrantCardProps) {
     reason,
     grant: { amount },
   } = grant;
-  const amountAsBigNumber = BigNumber.from(amount);
   return (
     <div className="rounded-xl bg-sand-100 py-4 px-6">
-      <div className="flex">
-        <div className="text-xl font-medium">{getReasonLabel(reason)}</div>
-        <div>{amountAsBigNumber.toString()}</div>
+      <div className="flex items-center">
+        <div>
+          <div className="mb-1.5 text-xl font-medium">
+            {getReasonLabel(reason)}
+          </div>
+          <div>
+            {formatCrypto({
+              token: SupportedCrypto.Gfi,
+              amount: BigNumber.from(amount),
+            })}{" "}
+            GFI - {format(TOKEN_LAUNCH_TIME, "MMM d, y")}
+          </div>
+        </div>
+        <div></div>
         <div></div>
         <div></div>
         <div></div>
