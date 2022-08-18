d2e766cab8c225c9674bc9a2a5ea3751ab0b46e3
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-08-18 18:18:14 -0400

    refactored to remove PoolBacker and shorten indexing time for pool tokens

--- packages/subgraph/src/entities/tranched_pool.ts
+++ packages/subgraph/src/entities/tranched_pool.ts
@@ -4,7 +4,6 @@ import {TranchedPool as TranchedPoolContract, DepositMade} from "../../generated
 import {SECONDS_PER_DAY, GFI_DECIMALS, USDC_DECIMALS, SECONDS_PER_YEAR, CONFIG_KEYS_ADDRESSES} from "../constants"
 import {getOrInitUser} from "./user"
 import {getOrInitCreditLine, initOrUpdateCreditLine} from "./credit_line"
-import {getOrInitPoolBacker} from "./pool_backer"
 import {getOrInitSeniorPoolStatus} from "./senior_pool"
 import {
   getLeverageRatio,
@@ -34,8 +33,7 @@ export function updatePoolCreditLine(address: Address, timestamp: BigInt): void 
 }
 
 export function handleDeposit(event: DepositMade): void {
-  const userAddress = event.params.owner
-  const user = getOrInitUser(userAddress)
+  const backer = getOrInitUser(event.params.owner)
 
   let tranchedPool = getOrInitTranchedPool(event.address, event.block.timestamp)
   const juniorTrancheInfo = JuniorTrancheInfo.load(`${event.address.toHexString()}-${event.params.tranche.toString()}`)
@@ -44,7 +42,6 @@ export function handleDeposit(event: DepositMade): void {
     juniorTrancheInfo.save()
   }
 
-  let backer = getOrInitPoolBacker(event.address, userAddress)
   if (!tranchedPool.backers.includes(backer.id)) {
     const addresses = tranchedPool.backers
     addresses.push(backer.id)
@@ -454,6 +451,22 @@ export function updatePoolRewardsClaimable(tranchedPool: TranchedPool, blockTime
     )
     if (!stakingRewardsEarnedResult.reverted) {
       poolToken.stakingRewardsClaimable = stakingRewardsEarnedResult.value
+    }
+    poolToken.save()
+  }
+}
+
+export function updatePoolTokensRedeemable(tranchedPool: TranchedPool): void {
+  const tranchedPoolContract = TranchedPoolContract.bind(Address.fromString(tranchedPool.id))
+  const poolTokenIds = tranchedPool.tokens
+  for (let i = 0; i < poolTokenIds.length; i++) {
+    const poolToken = assert(TranchedPoolToken.load(poolTokenIds[i]))
+    const availableToWithdrawResult = tranchedPoolContract.try_availableToWithdraw(BigInt.fromString(poolToken.id))
+    if (!availableToWithdrawResult.reverted) {
+      poolToken.interestRedeemable = availableToWithdrawResult.value.value0
+      poolToken.principalRedeemable = availableToWithdrawResult.value.value1
+    } else {
+      log.warning("availableToWithdraw reverted for pool token {} on TranchedPool {}", [poolToken.id, tranchedPool.id])
     }
     poolToken.save()
   }
--- packages/subgraph/src/mappings/pool_tokens.ts
+++ packages/subgraph/src/mappings/pool_tokens.ts
@@ -1,26 +1,72 @@
-import {TokenBurned, TokenMinted, TokenRedeemed, Transfer} from "../../generated/PoolTokens/PoolTokens"
-import {TranchedPoolToken} from "../../generated/schema"
-import {updatePoolBacker} from "../entities/pool_backer"
-import {handleTranchedPoolTokenTransfer, initOrUpdateTranchedPoolToken} from "../entities/pool_tokens"
+import {BigInt, store} from "@graphprotocol/graph-ts"
+import {
+  TokenBurned,
+  TokenMinted,
+  TokenRedeemed,
+  Transfer,
+  TokenPrincipalWithdrawn,
+} from "../../generated/PoolTokens/PoolTokens"
+import {TranchedPool, TranchedPoolToken} from "../../generated/schema"
+import {getOrInitUser} from "../entities/user"
+import {deleteZapAfterClaimMaybe} from "../entities/zapper"
 
 export function handleTokenBurned(event: TokenBurned): void {
-  initOrUpdateTranchedPoolToken(event.params.tokenId)
-  updatePoolBacker(event.params.owner, event.params.pool)
+  const token = TranchedPoolToken.load(event.params.tokenId.toString())
+  if (!token) {
+    return
+  }
+  store.remove("TranchedPoolToken", event.params.tokenId.toString())
 }
 
 export function handleTokenMinted(event: TokenMinted): void {
-  initOrUpdateTranchedPoolToken(event.params.tokenId)
-  updatePoolBacker(event.params.owner, event.params.pool)
-  const token = assert(TranchedPoolToken.load(event.params.tokenId.toString()))
-  token.mintedAt = event.block.timestamp
-  token.save()
+  const tranchedPool = TranchedPool.load(event.params.pool.toHexString())
+  if (tranchedPool) {
+    const token = new TranchedPoolToken(event.params.tokenId.toString())
+    token.mintedAt = event.block.timestamp
+    token.tranchedPool = tranchedPool.id
+    token.user = event.params.owner.toHexString()
+    token.principalAmount = event.params.amount
+    token.principalRedeemable = token.principalAmount
+    token.interestRedeemable = BigInt.zero()
+    token.tranche = event.params.tranche
+    token.save()
+
+    const tokensOnPool = tranchedPool.tokens
+    tokensOnPool.push(token.id)
+    tranchedPool.tokens = tokensOnPool
+    tranchedPool.save()
+  }
 }
 
 export function handleTokenRedeemed(event: TokenRedeemed): void {
-  initOrUpdateTranchedPoolToken(event.params.tokenId)
-  updatePoolBacker(event.params.owner, event.params.pool)
+  const token = TranchedPoolToken.load(event.params.tokenId.toString())
+  if (!token) {
+    return
+  }
+  token.interestRedeemable = token.interestRedeemable.minus(event.params.interestRedeemed)
+  token.interestRedeemed = token.interestRedeemed.plus(event.params.interestRedeemed)
+  token.principalRedeemable = token.principalRedeemable.minus(event.params.principalRedeemed)
+  token.principalRedeemed = token.principalRedeemed.plus(event.params.principalRedeemed)
+  token.save()
+}
+
+export function handleTokenPrincipalWithdrawn(event: TokenPrincipalWithdrawn): void {
+  const token = TranchedPoolToken.load(event.params.tokenId.toString())
+  if (!token) {
+    return
+  }
+  token.principalAmount = token.principalAmount.minus(event.params.principalWithdrawn)
+  token.principalRedeemable = token.principalRedeemable.minus(event.params.principalWithdrawn)
+  token.save()
 }
 
 export function handleTransfer(event: Transfer): void {
-  handleTranchedPoolTokenTransfer(event)
+  const token = TranchedPoolToken.load(event.params.tokenId.toString())
+  if (!token) {
+    return
+  }
+  const newOwner = getOrInitUser(event.params.to)
+  token.user = newOwner.id
+  token.save()
+  deleteZapAfterClaimMaybe(event)
 }
