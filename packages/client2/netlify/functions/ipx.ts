8953d6d8cd76962f9f316613651b7f4eb4ace5a2
Brandon Chow <brandonstchow@gmail.com>
2022-08-23 13:41:44 -0700

    Return empty ipx handler

--- packages/client2/netlify/functions/ipx.ts
+++ packages/client2/netlify/functions/ipx.ts
@@ -1,5 +1,6 @@
-import { createIPXHandler } from "@netlify/ipx";
+import { builder, Handler } from "@netlify/functions";
 
-export const handler = createIPXHandler({
-  domains: ["beta.app.goldfinch.finance"],
-});
+// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
+const unbuiltHandler: Handler = (_event, _context) => {};
+
+export const handler = builder(unbuiltHandler);
