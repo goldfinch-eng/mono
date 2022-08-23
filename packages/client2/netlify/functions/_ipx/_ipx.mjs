695729dd2d55227fdbb6075c2ad67fec83bb90f7
Brandon Chow <brandonstchow@gmail.com>
2022-08-23 15:32:43 -0700

    Convert mts -> mjs & add console debugging statemnets

--- packages/client2/netlify/functions/_ipx/_ipx.mjs
+++ packages/client2/netlify/functions/_ipx/_ipx.mjs
@@ -1,15 +1,19 @@
-import { builder, Handler } from "@netlify/functions";
+import { builder } from "@netlify/functions";
 import { createIPXHandler } from "@netlify/ipx";
 
 const standardHandler = createIPXHandler({});
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
-const unbuiltHandler: Handler = (event, _context) => {
+const unbuiltHandler = (event, _context) => {
+  // eslint-disable-next-line no-console
+  console.log(`event: ${JSON.stringify(event)}`);
   const protocol = event.headers["x-forwarded-proto"] || "http";
+  // eslint-disable-next-line no-console
+  console.log(`protocol: ${protocol}`);
   if (!["http", "https"].includes(protocol)) {
     console.error(
-      `malicious attacker attempting exploit via x-forwarded-proto: ${protocol}. DEPLOY 2!`
+      `malicious attacker attempting exploit via x-forwarded-proto: ${protocol}`
     );
-    return Promise.resolve({ statusCode: 404 });
+    return Promise.resolve({ statusCode: 404, body: "Not Found" });
   } else {
     return standardHandler(event, _context);
   }
