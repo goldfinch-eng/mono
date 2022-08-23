dcc4d5781e83bafce5084083982a85b5aceaba65
Brandon Chow <brandonstchow@gmail.com>
2022-08-23 16:51:55 -0700

    Check imports work properly

--- packages/client2/netlify/functions/_ipx/_ipx.ts
+++ packages/client2/netlify/functions/_ipx/_ipx.ts
@@ -1,8 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const handler: any = async (_event: any, _context: any) => {
-  return {
-    statusCode: 304,
-    message: "Not Modified",
-  };
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-empty-function */
+
+const ipx = require("@netlify/ipx");
+const { createIPXHandler } = ipx;
+// const standardHandler: any = createIPXHandler({});
+export const handler: any = async (event: any, _context: any) => {
+  console.log(`event: ${JSON.stringify(event)}`);
+  const protocol = event.headers["x-forwarded-proto"] || "http";
+  console.log(`protocol: ${protocol}`);
+  if (!["http", "https"].includes(protocol)) {
+    console.error(
+      `malicious attacker attempting exploit via x-forwarded-proto: ${protocol}`
+    );
+    return { statusCode: 404, body: "Not Found" };
+  } else {
+    // return standardHandler(event, _context);
+  }
 };
