2c92154bd584c382d63a85e6d16a0620c7ce7091
Brandon Chow <brandonstchow@gmail.com>
2022-08-23 16:39:49 -0700

    Do not reload cache

--- packages/client2/netlify/functions/_ipx/_ipx.ts
+++ packages/client2/netlify/functions/_ipx/_ipx.ts
@@ -1,28 +1,8 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// import { Handler } from "@netlify/functions";
-// import { createIPXHandler } from "@netlify/ipx";
-
-// const functions = require("@netlify/functions");
-const ipx = require("@netlify/ipx");
-
-// const { builder } = functions.builder;
-const { createIPXHandler } = ipx;
-
-const standardHandler: any = createIPXHandler({});
-// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
-
-export const handler: any = (event: any, _context: any) => {
-  // eslint-disable-next-line no-console
-  console.log(`event: ${JSON.stringify(event)}`);
-  const protocol = event.headers["x-forwarded-proto"] || "http";
-  // eslint-disable-next-line no-console
-  console.log(`protocol: ${protocol}`);
-  if (!["http", "https"].includes(protocol)) {
-    console.error(
-      `malicious attacker attempting exploit via x-forwarded-proto: ${protocol}`
-    );
-    return Promise.resolve({ statusCode: 404, body: "Not Found" });
-  } else {
-    return standardHandler(event, _context);
-  }
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const handler: any = async (_event: any, _context: any) => {
+  return {
+    statusCode: 304,
+    message: "Not Modified",
+  };
 };
