1f1ac62a407f7de99690e64026ac5b5f060707a7
Brandon Chow <brandonstchow@gmail.com>
2022-08-23 17:33:05 -0700

    Hardcode not modified for IPX

--- packages/client2/netlify/functions/_ipx/_ipx.ts
+++ packages/client2/netlify/functions/_ipx/_ipx.ts
@@ -1,20 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-empty-function */
-
-import { createIPXHandler } from "@netlify/ipx";
-// const standardHandler: any = createIPXHandler({});
-export const handler: any = async (event: any, _context: any) => {
-  console.log(`event: ${JSON.stringify(event)}`);
-  const protocol = event.headers["x-forwarded-proto"] || "http";
-  console.log(`protocol: ${protocol}`);
-  if (!["http", "https"].includes(protocol)) {
-    console.error(
-      `malicious attacker attempting exploit via x-forwarded-proto: ${protocol}`
-    );
-    return { statusCode: 404, body: "Not Found" };
-  } else {
-    // return standardHandler(event, _context);
-  }
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const handler: any = async (_event: any, _context: any) => {
+  return {
+    statusCode: 304,
+    message: "Not Modified",
+  };
 };
