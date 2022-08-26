af0b23b092af5ba9d93e60129da12d154af11808
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-08-26 11:06:46 -0400

    made it so user.tokens is not virtual, added a check to see if a user is fully withdrawn from a pool

--- packages/subgraph/src/mappings/pool_tokens.ts
+++ packages/subgraph/src/mappings/pool_tokens.ts
@@ -6,9 +6,10 @@ import {
   Transfer,
   TokenPrincipalWithdrawn,
 } from "../../generated/PoolTokens/PoolTokens"
-import {TranchedPool, TranchedPoolToken} from "../../generated/schema"
+import {TranchedPool, TranchedPoolToken, User} from "../../generated/schema"
 import {getOrInitUser} from "../entities/user"
 import {deleteZapAfterClaimMaybe} from "../entities/zapper"
+import {appendToList, removeFromList} from "../utils"
 
 export function handleTokenBurned(event: TokenBurned): void {
   const token = TranchedPoolToken.load(event.params.tokenId.toString())
@@ -20,6 +21,7 @@ export function handleTokenBurned(event: TokenBurned): void {
 
 export function handleTokenMinted(event: TokenMinted): void {
   const tranchedPool = TranchedPool.load(event.params.pool.toHexString())
+  const user = getOrInitUser(event.params.owner)
   if (tranchedPool) {
     const token = new TranchedPoolToken(event.params.tokenId.toString())
     token.mintedAt = event.block.timestamp
@@ -41,6 +43,11 @@ export function handleTokenMinted(event: TokenMinted): void {
     tokensOnPool.push(token.id)
     tranchedPool.tokens = tokensOnPool
     tranchedPool.save()
+
+    const tokensOnUser = user.tokens
+    tokensOnUser.push(token.id)
+    user.tokens = tokensOnUser
+    user.save()
   }
 }
 
@@ -56,6 +63,16 @@ export function handleTokenRedeemed(event: TokenRedeemed): void {
   token.save()
 }
 
+function isUserFullyWithdrawnFromPool(user: User, tranchedPool: TranchedPool): boolean {
+  for (let i = 0; i < user.tokens.length; i++) {
+    const token = assert(TranchedPoolToken.load(user.tokens[i]))
+    if (token.tranchedPool == tranchedPool.id && !token.principalAmount.isZero()) {
+      return false
+    }
+  }
+  return true
+}
+
 export function handleTokenPrincipalWithdrawn(event: TokenPrincipalWithdrawn): void {
   const token = TranchedPoolToken.load(event.params.tokenId.toString())
   if (!token) {
@@ -63,27 +80,30 @@ export function handleTokenPrincipalWithdrawn(event: TokenPrincipalWithdrawn): v
   }
   token.principalAmount = token.principalAmount.minus(event.params.principalWithdrawn)
   token.principalRedeemable = token.principalRedeemable.minus(event.params.principalWithdrawn)
+  token.save()
   if (token.principalAmount.isZero()) {
-    const tranchedPool = TranchedPool.load(event.params.pool.toHexString())
-    const backerToRemove = event.params.owner.toHexString()
-    if (tranchedPool && tranchedPool.backers.includes(backerToRemove)) {
-      let backers = tranchedPool.backers
-      const index = backers.indexOf(backerToRemove)
-      backers.splice(index, 1)
-      tranchedPool.backers = backers
-      tranchedPool.numBackers = backers.length
+    const tranchedPool = assert(TranchedPool.load(event.params.pool.toHexString()))
+    const user = assert(User.load(event.params.owner.toHexString()))
+    if (isUserFullyWithdrawnFromPool(user, tranchedPool)) {
+      tranchedPool.backers = removeFromList(tranchedPool.backers, user.id)
+      tranchedPool.numBackers = tranchedPool.backers.length
       tranchedPool.save()
     }
   }
-  token.save()
 }
 
 export function handleTransfer(event: Transfer): void {
-  const token = TranchedPoolToken.load(event.params.tokenId.toString())
+  const tokenId = event.params.tokenId.toString()
+  const token = TranchedPoolToken.load(tokenId)
   if (!token) {
     return
   }
+  const oldOwner = getOrInitUser(event.params.from)
   const newOwner = getOrInitUser(event.params.to)
+  oldOwner.tokens = removeFromList(oldOwner.tokens, tokenId)
+  oldOwner.save()
+  newOwner.tokens = appendToList(newOwner.tokens, tokenId)
+  newOwner.save()
   token.user = newOwner.id
   token.save()
   deleteZapAfterClaimMaybe(event)
--- packages/subgraph/src/utils.ts
+++ packages/subgraph/src/utils.ts
@@ -60,3 +60,28 @@ export function getAddressFromConfig<T extends ConfigBearer>(contract: T, target
   const goldfinchConfigContract = GoldfinchConfig.bind(contract.config())
   return goldfinchConfigContract.getAddress(BigInt.fromI32(target))
 }
+
+/**
+ * Returns a copy of the original array with the desired item appended to the end.
+ * @param list
+ * @param itemToAdd
+ */
+export function appendToList<T>(list: T[], itemToAdd: T): T[] {
+  const listCopy = list.slice(0)
+  listCopy.push(itemToAdd)
+  return listCopy
+}
+
+/**
+ * Takes an array and an item to be removed from the array. Returns a copy of the array with the desired item removed. If the desired item is not present in the original array, then this returns a copy of that array.
+ * @param list
+ * @param itemToRemove
+ */
+export function removeFromList<T>(list: T[], itemToRemove: T): T[] {
+  const listCopy = list.slice(0)
+  const index = list.indexOf(itemToRemove)
+  if (index >= 0) {
+    listCopy.splice(index, 1)
+  }
+  return listCopy
+}
