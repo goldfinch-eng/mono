6b1f24657b29505470f7cef239561c7d6281d4e7
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-09-02 17:26:48 -0400

    deleted some netlify-specific files

--- packages/client2/netlify/functions/_ipx/_ipx.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-export const handler: any = async (_event: any, _context: any) => {
-  return {
-    statusCode: 304,
-    message: "Not Modified",
-  };
-};
