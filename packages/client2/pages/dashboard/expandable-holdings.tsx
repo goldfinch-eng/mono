410e2c07cc152857f9146522b7bebfd2a423ccd1
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-09-02 13:20:03 -0400

    roughed out the holdings in dashboard

---
+++ packages/client2/pages/dashboard/expandable-holdings.tsx
@@ -0,0 +1,102 @@
+import clsx from "clsx";
+import { BigNumber } from "ethers";
+import { ReactNode, useState } from "react";
+
+import { Icon } from "@/components/design-system";
+import { formatCrypto, formatPercent } from "@/lib/format";
+import { CryptoAmount } from "@/lib/graphql/generated";
+
+interface Holding {
+  name: string;
+  percentage: number;
+  quantity: BigNumber;
+  usdcValue: CryptoAmount;
+}
+
+interface ExpandableHoldingsProps {
+  title: string;
+  color: string;
+  holdings: Holding[];
+  quantityFormatter: (n: BigNumber) => ReactNode;
+}
+
+export function ExpandableHoldings({
+  title,
+  color,
+  holdings,
+  quantityFormatter,
+}: ExpandableHoldingsProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const totalPercentage = holdings.reduce(
+    (prev, current) => prev + current.percentage,
+    0
+  );
+  const totalQuantity = holdings.reduce(
+    (prev, current) => prev.add(current.quantity),
+    BigNumber.from(0)
+  );
+  return (
+    <div className="overflow-hidden rounded-xl border border-sand-200">
+      <div className="relative flex justify-between bg-white px-5 py-6 hover:bg-sand-100">
+        <div className="flex items-center gap-3">
+          <div
+            className="h-3.5 w-3.5 rounded-full"
+            style={{ backgroundColor: color }}
+          />
+          {title}
+        </div>
+        <div>{formatPercent(totalPercentage)}</div>
+        <div>{quantityFormatter(totalQuantity)}</div>
+        <button
+          onClick={() => setIsExpanded(!isExpanded)}
+          className="before:absolute before:inset-0"
+        >
+          <Icon
+            name="ChevronDown"
+            size="lg"
+            className={clsx(
+              "transition-transform",
+              isExpanded ? "rotate-180" : null
+            )}
+          />
+        </button>
+      </div>
+      {isExpanded ? (
+        <div className="divide-y divide-sand-200 border-t border-sand-200 bg-sand-50 px-5">
+          {holdings.map((holding, index) => (
+            <IndividualHolding
+              key={holding.name + index}
+              name={holding.name}
+              percentage={holding.percentage}
+              quantity={holding.quantity}
+              quantityFormatter={quantityFormatter}
+              usdcValue={holding.usdcValue}
+            />
+          ))}
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+interface IndividualHoldingProps extends Holding {
+  quantityFormatter: ExpandableHoldingsProps["quantityFormatter"];
+}
+
+function IndividualHolding({
+  name,
+  percentage,
+  quantity,
+  quantityFormatter,
+  usdcValue,
+}: IndividualHoldingProps) {
+  return (
+    <div className="flex justify-between py-5">
+      <div>{name}</div>
+      <div>{formatPercent(percentage)}</div>
+      <div>{quantityFormatter(quantity)}</div>
+      <div>{formatCrypto(usdcValue)}</div>
+    </div>
+  );
+}
--- packages/client2/pages/dashboard/index.page.tsx
+++ packages/client2/pages/dashboard/index.page.tsx
@@ -1,25 +1,33 @@
 import { gql } from "@apollo/client";
+import { BigNumber } from "ethers";
 
 import { Heading } from "@/components/design-system";
-import { useDashboardPageQuery } from "@/lib/graphql/generated";
+import {
+  SupportedCrypto,
+  useDashboardPageQuery,
+} from "@/lib/graphql/generated";
 import { useWallet } from "@/lib/wallet";
 
+import { ExpandableHoldings } from "./expandable-holdings";
+
 gql`
-  query DashboardPage($userId: ID!) {
-    user(id: $userId) {
-      id
-      seniorPoolStakedPositions {
-        id
-        amount
-      }
-      tranchedPoolTokens {
-        id
-      }
-    }
+  query DashboardPage($userId: String!) {
     viewer @client {
       fiduBalance {
         token
         amount
+      }
+    }
+    tranchedPoolTokens(
+      where: { user: $userId, principalAmount_gt: 0 }
+      orderBy: mintedAt
+      orderDirection: desc
+    ) {
+      id
+      principalAmount
+      tranchedPool {
+        id
+        name @client
       }
     }
   }
@@ -29,20 +37,49 @@ export default function DashboardPage() {
   const { account } = useWallet();
   const { data, loading, error } = useDashboardPageQuery({
     variables: { userId: account?.toLowerCase() ?? "" },
-    skip: !account,
   });
 
   return (
     <div>
-      <Heading level={1}>Dashboard</Heading>
-      {!account ? (
-        <div>You must connect your wallet to view your dashboard</div>
+      <Heading level={1} className="mb-12">
+        Dashboard
+      </Heading>
+      {!account && !loading ? (
+        <div className="text-lg font-medium text-clay-500">
+          You must connect your wallet to view your dashboard
+        </div>
       ) : error ? (
         <div className="text-clay-500">Error: {error.message}</div>
       ) : !data || loading ? (
         <div>Loading</div>
       ) : (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
+        <div>
+          <Heading level={2} className="font-sans !text-3xl !font-normal">
+            Portfolio summary
+          </Heading>
+          <Heading level={3} className="font-sans !text-xl">
+            Holdings
+          </Heading>
+          <div>
+            <ExpandableHoldings
+              title="Tony"
+              color="#ff0000"
+              holdings={data.tranchedPoolTokens.map((token) => ({
+                name: token.tranchedPool.name,
+                percentage: 0,
+                quantity: BigNumber.from(1),
+                usdcValue: {
+                  token: SupportedCrypto.Usdc,
+                  amount: token.principalAmount,
+                },
+              }))}
+              quantityFormatter={(n: BigNumber) =>
+                `${n.toString()} NFT${n.gt(BigNumber.from(1)) ? "s" : ""}`
+              }
+            />
+          </div>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
       )}
     </div>
   );
