c069ca821715c9b5665af2b5e07b032083ab6c4a
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-09-15 14:40:09 +0300

    add moonbeam deployment

--- packages/protocol/clean.sh
+++ packages/protocol/clean.sh
@@ -5,5 +5,3 @@ rm -rf deployments/localhost/
 rm -rf deployments/all_dev.json
 rm -rf ../client/config/pool-metadata/localhost.json
 
-rm -rf .openzeppelin/unknown-1287.json
-rm -rf deployments/moonbeam/
