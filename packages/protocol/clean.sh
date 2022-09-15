9874a0462b3af1370572edba2bd8feec3418475f
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-09-15 13:43:42 +0300

    delete moonbeam leftovers during clean

--- packages/protocol/clean.sh
+++ packages/protocol/clean.sh
@@ -4,3 +4,6 @@ rm -rf .openzeppelin/unknown-31337.json
 rm -rf deployments/localhost/
 rm -rf deployments/all_dev.json
 rm -rf ../client/config/pool-metadata/localhost.json
+
+rm -rf .openzeppelin/unknown-1287.json
+rm -rf deployments/moonbeam/
