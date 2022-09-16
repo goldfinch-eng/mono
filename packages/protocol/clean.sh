b8d31f6ff5c42b91428c602c5b86b74d1b9b93f1
Constantin Dogaru <ctin.dogaru95@gmail.com>
2022-09-16 11:43:50 +0300

    add one more line in the clean script

--- packages/protocol/clean.sh
+++ packages/protocol/clean.sh
@@ -4,4 +4,4 @@ rm -rf .openzeppelin/unknown-31337.json
 rm -rf deployments/localhost/
 rm -rf deployments/all_dev.json
 rm -rf ../client/config/pool-metadata/localhost.json
-
+git checkout ../client2/constants/metadata/localhost.json
