a9ef0614a89081f8554a83ef38954a1dce4ffa2a
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-09-22 10:21:50 -0400

    mongo connection is now fully specified by MONGODB_URI env var

--- packages/cms/src/server.ts
+++ packages/cms/src/server.ts
@@ -15,9 +15,6 @@ app.get("/", (_, res) => {
 payload.init({
   secret: process.env.PAYLOAD_SECRET,
   mongoURL: process.env.MONGODB_URI,
-  mongoOptions: {
-    dbName: "payload",
-  },
   express: app,
   onInit: () => {
     payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
