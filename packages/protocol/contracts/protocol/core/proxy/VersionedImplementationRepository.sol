7051f7e77f01f2625e20470b52abcdac6d9823a2
Will Johnston <will@warblerlabs.com>
2022-10-04 16:08:40 -0700

    pin implementation versions

--- packages/protocol/contracts/protocol/core/proxy/VersionedImplementationRepository.sol
+++ packages/protocol/contracts/protocol/core/proxy/VersionedImplementationRepository.sol
@@ -1,11 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.12;
+pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 import {IVersioned} from "../../../interfaces/IVersioned.sol";
-import {
-  ImplementationRepository as Repo
-} from "./ImplementationRepository.sol";
+import {ImplementationRepository as Repo} from "./ImplementationRepository.sol";
 
 contract VersionedImplementationRepository is Repo {
   /// @dev abi encoded version -> implementation address
