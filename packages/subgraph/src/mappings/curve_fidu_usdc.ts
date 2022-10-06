1e43ea6042bba934a6d13dd6bbbd8612cb75853f
gregan <greg@goldfinch.finance>
2022-10-06 15:50:39 -0700

    address comments

--- packages/subgraph/src/mappings/curve_fidu_usdc.ts
+++ packages/subgraph/src/mappings/curve_fidu_usdc.ts
@@ -5,16 +5,15 @@ import {getOrInitUser} from "../entities/user"
 
 export function handleTokenExchange(event: TokenExchange): void {
   const buyer = event.params.buyer
-  const soldId = event.params.sold_id
   const boughtId = event.params.bought_id
   const tokensSold = event.params.tokens_sold
   const tokensBought = event.params.tokens_bought
   getOrInitUser(buyer)
 
-  const curveUsdcId = BigInt.fromI32(1)
+  const curveFiduId = BigInt.fromI32(0)
 
   // Sells USDC and acquires FIDU
-  if (soldId.equals(curveUsdcId)) {
+  if (boughtId.equals(curveFiduId)) {
     const transaction = createTransactionFromEvent(event, "CURVE_FIDU_BUY", buyer)
     transaction.category = "CURVE_FIDU_BUY"
     transaction.amount = tokensBought
