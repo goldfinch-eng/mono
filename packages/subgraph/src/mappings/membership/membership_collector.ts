3e7b8c435faf5af125ec2fc5f101f6efbed4cac5
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-10-31 13:42:26 -0400

    added the MembershipEpoch entity to subgraph

---
+++ packages/subgraph/src/mappings/membership/membership_collector.ts
@@ -0,0 +1,9 @@
+import {EpochFinalized} from "../../../generated/MembershipCollector/MembershipCollector"
+import {MembershipEpoch} from "../../../generated/schema"
+
+export function handleEpochFinalized(event: EpochFinalized): void {
+  const epoch = new MembershipEpoch(event.params.epoch.toString())
+  epoch.epoch = event.params.epoch
+  epoch.totalRewards = event.params.totalRewards
+  epoch.save()
+}
