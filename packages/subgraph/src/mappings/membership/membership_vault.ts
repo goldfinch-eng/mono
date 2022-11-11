a35a9327ee8ee2fe9211f5e3ebbae84ccd9f81dc
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-11-10 19:53:01 -0500

    added subgraph code to index memberhip reward disbursements

---
+++ packages/subgraph/src/mappings/membership/membership_vault.ts
@@ -0,0 +1,58 @@
+import {Address, BigInt} from "@graphprotocol/graph-ts"
+import {
+  IncreasedHoldings,
+  DecreasedHoldings,
+  VaultTotalUpdate,
+} from "../../../generated/MembershipVault/MembershipVault"
+import {Membership, MembershipRoster} from "../../../generated/schema"
+
+export function getOrInitMembershipRoster(): MembershipRoster {
+  let membershipRoster = MembershipRoster.load("1")
+  if (!membershipRoster) {
+    membershipRoster = new MembershipRoster("1")
+    membershipRoster.members = []
+    membershipRoster.eligibleScoreTotal = BigInt.zero()
+    membershipRoster.nextEpochScoreTotal = BigInt.zero()
+  }
+  return membershipRoster
+}
+
+function getOrInitMembership(memberAddress: Address): Membership {
+  let membership = Membership.load(memberAddress.toHexString())
+  if (!membership) {
+    membership = new Membership(memberAddress.toHexString())
+    membership.user = memberAddress.toHexString()
+    membership.eligibleScore = BigInt.zero()
+    membership.nextEpochScore = BigInt.zero()
+    membership.save()
+  }
+  return membership
+}
+
+export function handleIncreasedHoldings(event: IncreasedHoldings): void {
+  const membership = getOrInitMembership(event.params.owner)
+  membership.nextEpochScore = event.params.amount
+  membership.save()
+
+  const membershipRoster = getOrInitMembershipRoster()
+  if (!membershipRoster.members.includes(membership.id)) {
+    membershipRoster.members = membershipRoster.members.concat([membership.id])
+    membershipRoster.save()
+  }
+}
+
+export function handleDecreasedHoldings(event: DecreasedHoldings): void {
+  const membership = getOrInitMembership(event.params.owner)
+  membership.eligibleScore = event.params.eligibleAmount
+  membership.nextEpochScore = event.params.nextEpochAmount
+  membership.save()
+
+  // TODO maybe some logic here to remove someone from the member roster if their score becomes 0
+}
+
+export function handleVaultTotalUpdate(event: VaultTotalUpdate): void {
+  const membershipRoster = getOrInitMembershipRoster()
+  membershipRoster.eligibleScoreTotal = event.params.eligibleAmount
+  membershipRoster.nextEpochScoreTotal = event.params.nextEpochAmount
+  membershipRoster.save()
+}
