59ca9614fe7900580a7cde64b083ca223ffdbce0
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-11-18 12:52:08 -0500

    updated the content inside membership explainer

--- packages/client2/pages/membership/explainer.tsx
+++ packages/client2/pages/membership/explainer.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-import { Drawer, DrawerProps } from "@/components/design-system";
+import { Drawer, DrawerProps, Link } from "@/components/design-system";
 
 type ExplainerProps = Omit<DrawerProps, "children" | "size" | "from">;
 
@@ -15,28 +15,94 @@ export function Explainer({ isOpen, onClose }: ExplainerProps) {
     >
       <div className="space-y-4">
         <Section heading="Introduction to vaults">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-          <div className="mt-4 flex h-64 items-center justify-center border border-dashed border-black">
-            Media goes here
+          <div className="space-y-4">
+            <p>
+              Goldfinch Membership is the first phase of a broader tokenomics
+              redesign (Tokenomics v2), which was approved by the
+              protocol&apos;s community in 2022 and focuses on enhancing the
+              utility of GFI. Membership is designed to empower Goldfinch
+              Investors to support the protocol&apos;s security and expansion
+              while increasing their participation.
+            </p>
+            <p>
+              Goldfinch Members receive yield enhancements via Member Rewards,
+              which have been earmarked from the Goldfinch treasury and are
+              distributed pro-rata based on one&apos;s Membership Vault
+              position. In addition, Members will gain access to exclusive
+              communication channels, special offers, and more.
+            </p>
+            <p>
+              There are only two requirements to participate in Goldfinch
+              Membership: being a Senior Pool LP or Backer on Goldfinch
+              (represented by holding staked{" "}
+              <Link
+                openInNewTab
+                href="https://docs.goldfinch.finance/goldfinch/protocol-mechanics/liquidityproviders#fidu"
+              >
+                FIDU
+              </Link>{" "}
+              and/or an active{" "}
+              <Link
+                openInNewTab
+                href="https://docs.goldfinch.finance/goldfinch/protocol-mechanics/backers"
+              >
+                Backer NFT
+              </Link>
+              ), and holding{" "}
+              <Link
+                openInNewTab
+                href="https://medium.com/goldfinch-fi/introducing-the-goldfinch-protocol-token-gfi-e09579fd9740"
+              >
+                GFI
+              </Link>
+              .
+            </p>
+            <p>
+              To become a Member, one only needs to lock their staked FIDU or
+              Backer NFTs, plus GFI, into a Membership Vault. To optimize the
+              yields you can receive, balanced is best: matching the dollar
+              value of the GFI and assets you deposit to the vault will balance
+              the vault ratio and increase your potential Member Reward yield.
+            </p>
           </div>
         </Section>
-        <Section heading="How is the estimated protocol revenue earnings calculated?">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-          <div className="mt-4 flex h-64 items-center justify-center border border-dashed border-black">
-            Media goes here
-          </div>
+        <Section heading="How are the estimated Member Rewards calculated?">
+          <p className="mb-4">
+            Estimated Member Rewards are calculated based on a) one&apos;s
+            Membership Vault position, and b) on the distribution of total
+            capital in the Membership Vault overall.
+          </p>
+          <ul className="list-disc space-y-4 pl-5">
+            <li>
+              This number is an estimate because rewards are distributed
+              pro-rata to Members at the end of each reward cycle—as such, other
+              Investors entering or exiting the Membership Vault during a reward
+              cycle will change the percentage of rewards one is expected to
+              receive, due to changing what percentage of the Vault one&apos;s
+              individual position represents.
+            </li>
+            <li>
+              The estimated Member Rewards displayed is a dynamic number that
+              reflects the distribution of total capital in the Membership Vault
+              overall at that moment in time and one&apos;s Member Vault
+              position at that moment in time.
+            </li>
+            <li>
+              A Member&apos;s precise share of Member Rewards is calculated
+              using the Cobb-Douglas function, an equation commonly used in
+              economic analysis for balanced input relationships generating an
+              output.
+            </li>
+            <li>
+              If a participant deposits assets into the Membership Vault during
+              a weekly reward cycle their assets will begin accumulating Member
+              Rewards at the beginning of the next weekly reward cycle. While
+              Members can withdraw their deposited assets at any time to exit
+              Membership, if they withdraw before the end of a weekly reward
+              cycle they will forfeit any Member Rewards they would have
+              received during that cycle.
+            </li>
+          </ul>
         </Section>
       </div>
     </Drawer>
