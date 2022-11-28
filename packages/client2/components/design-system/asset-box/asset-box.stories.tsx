0b6fbfe0eb1cb114ba9712d5d59c1b48b60d28d1
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-11-28 10:14:58 -0500

    fixed the asset box stories

--- packages/client2/components/design-system/asset-box/asset-box.stories.tsx
+++ packages/client2/components/design-system/asset-box/asset-box.stories.tsx
@@ -1,52 +1,55 @@
-import { ComponentMeta } from "@storybook/react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { BigNumber } from "ethers";
+import { useForm } from "react-hook-form";
 
-import { AssetBox } from "./index";
+import { formatCrypto, stringToCryptoAmount } from "@/lib/format";
+import { SupportedCrypto } from "@/lib/graphql/generated";
+
+import { Form } from "../form";
+import { AssetBox, AssetInputBox } from "./index";
 
 export default {
   title: "Components/AssetBox",
   component: AssetBox,
 } as ComponentMeta<typeof AssetBox>;
 
-// const gfiAsset = {
-//   name: "GFI",
-//   description: "Goldfinch Token",
-//   nativeAmount: {
-//     token: SupportedCrypto.Gfi,
-//     amount: BigNumber.from("100000000000000000000"),
-//   },
-//   usdcAmount: {
-//     token: SupportedCrypto.Usdc,
-//     amount: BigNumber.from("200000000"),
-//   },
-// };
+const gfiAsset = {
+  name: "GFI",
+  description: "Goldfinch Token",
+  nativeAmount: {
+    token: SupportedCrypto.Gfi,
+    amount: BigNumber.from("100000000000000000000"),
+  },
+  usdcAmount: {
+    token: SupportedCrypto.Usdc,
+    amount: BigNumber.from("200000000"),
+  },
+};
 
-// export const AssetBoxStory: ComponentStory<typeof AssetBox> = () => {
-//   return (
-//     <div className="max-w-lg bg-sand-100 p-5">
-//       <AssetBox asset={gfiAsset} />
-//     </div>
-//   );
-// };
+export const AssetBoxStory: ComponentStory<typeof AssetBox> = () => {
+  return (
+    <div className="max-w-lg bg-sand-100 p-5">
+      <AssetBox asset={gfiAsset} />
+    </div>
+  );
+};
 
-// export const AssetInputBoxStory: ComponentStory<typeof AssetInputBox> = () => {
-//   const rhfMethods = useForm<{ gfi: string }>();
-//   return (
-//     <Form
-//       rhfMethods={rhfMethods}
-//       onSubmit={(data) =>
-//         alert(formatCrypto(stringToCryptoAmount(data.gfi, SupportedCrypto.Gfi)))
-//       }
-//     >
-//       <AssetInputBox
-//         asset={gfiAsset}
-//         control={rhfMethods.control}
-//         name="gfi"
-//         label="Enter GFI"
-//         fiatPerGfi={2}
-//       />
-//     </Form>
-//   );
-// };
-export const AssetBoxStory = () => {
-  return "TODO. These stories can't exist yet. It's because the components depend on lib files that have Apollo code mixed in. Storybook can't mock Apollo successfully.";
+export const AssetInputBoxStory: ComponentStory<typeof AssetInputBox> = () => {
+  const rhfMethods = useForm<{ gfi: string }>();
+  return (
+    <Form
+      rhfMethods={rhfMethods}
+      onSubmit={(data) =>
+        alert(formatCrypto(stringToCryptoAmount(data.gfi, SupportedCrypto.Gfi)))
+      }
+    >
+      <AssetInputBox
+        asset={gfiAsset}
+        control={rhfMethods.control}
+        name="gfi"
+        label="Enter GFI"
+        fiatPerGfi={2}
+      />
+    </Form>
+  );
 };
