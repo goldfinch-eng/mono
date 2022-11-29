7f06a57efd58725b91d63ed88c24202f6a9c6e7f
Dalton Sweeney <daltyboy11@gmail.com>
2022-11-29 15:06:55 -0800

    Fix cancellation fee mainnet forking test (#1216)

--- packages/protocol/test/mainnet_forking/MainnetForking.test.ts
+++ packages/protocol/test/mainnet_forking/MainnetForking.test.ts
@@ -507,25 +507,21 @@ describe("mainnet forking tests", async function () {
       }
     })
 
-    it.skip("takes cancelation fees according to SeniorPoolWithdrawalCancelationFeeInBps", async () => {
-      const fiduHolder = {
-        address: "0x3ccf9578728cf103a4435ec83c716090d5752043",
-        // ~42K FIDU
-        stakingRewardsTokenId: 2108,
-      }
+    it("takes cancelation fees according to SeniorPoolWithdrawalCancelationFeeInBps", async () => {
+      const fiduHolder = stakedFiduHolders[1]!
       await impersonateAccount(hre, fiduHolder.address)
-      await stakingRewards.unstake(fiduHolder.stakingRewardsTokenId, fiduVal(30_000), {from: fiduHolder.address})
-      await fidu.approve(seniorPool.address, fiduVal(30_000), {from: fiduHolder.address})
-      await seniorPool.requestWithdrawal(fiduVal(30_000), {from: fiduHolder.address})
+      await stakingRewards.unstake(fiduHolder.stakingRewardsTokenId, fiduVal(10_000), {from: fiduHolder.address})
+      await fidu.approve(seniorPool.address, fiduVal(10_000), {from: fiduHolder.address})
+      await seniorPool.requestWithdrawal(fiduVal(10_000), {from: fiduHolder.address})
 
       const cancelationFeeInBps = await goldfinchConfig.getNumber(CONFIG_KEYS.SeniorPoolWithdrawalCancelationFeeInBps)
-      const expectedCancelationFee = fiduVal(30_000).mul(cancelationFeeInBps).div(new BN(10_000))
-      const expectedFiduReturnedToUser = fiduVal(30_000).sub(expectedCancelationFee)
+      const expectedCancelationFee = fiduVal(10_000).mul(cancelationFeeInBps).div(new BN(10_000))
+      const expectedFiduReturnedToUser = fiduVal(10_000).sub(expectedCancelationFee)
 
       await expectAction(() => seniorPool.cancelWithdrawalRequest("1", {from: fiduHolder.address})).toChange([
         [() => fidu.balanceOf(fiduHolder.address), {by: expectedFiduReturnedToUser}],
         [() => fidu.balanceOf(reserveAddress), {by: expectedCancelationFee}],
-        [() => fidu.balanceOf(seniorPool.address), {by: fiduVal(30_000).neg()}],
+        [() => fidu.balanceOf(seniorPool.address), {by: fiduVal(10_000).neg()}],
       ])
     })
   })
