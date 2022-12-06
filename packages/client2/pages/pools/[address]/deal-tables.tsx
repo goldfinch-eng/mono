ad1f25a425f9f5d5e09b65d2f820965168625f20
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-12-06 15:36:20 -0500

    made the way leverage ratio is written look more aesthetic

--- packages/client2/pages/pools/[address]/deal-tables.tsx
+++ packages/client2/pages/pools/[address]/deal-tables.tsx
@@ -179,7 +179,9 @@ export function DealTermsTable({
               "Current leverage ratio",
               "The leverage of senior tranche to junior tranche capital in this Pool. Senior tranche capital is automatically allocated by Goldfinch's Senior Pool, according to the protocol's leverage model. Junior tranche capital is provided directly by Backer investments. A current leverage ratio of 4x means that for every $1 of junior capital deposited by Backers, $4 of senior capital will be allocated by the Senior Pool.",
               isMultitranche && tranchedPool.estimatedLeverageRatio
-                ? tranchedPool.estimatedLeverageRatio.toString()
+                ? `${tranchedPool.estimatedLeverageRatio
+                    .toUnsafeFloat()
+                    .toString()}x`
                 : "N/A",
             ],
             [
