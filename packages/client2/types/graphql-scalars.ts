634c46dc4fbca565d96737a039519e9ee4737263
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2022-12-08 14:23:44 -0500

    added a comment regarding global types in types/graphql-scalars.ts

--- packages/client2/types/graphql-scalars.ts
+++ packages/client2/types/graphql-scalars.ts
@@ -2,6 +2,7 @@ import { BigNumber, FixedNumber } from "ethers";
 
 import { SupportedCrypto } from "@/lib/graphql/generated";
 
+// These types must be defined globally so that the generated GraphQL types (located in lib/graphql/generated) have them in scope. Otherwise that file will fail TypeScript validation.
 declare global {
   export type TheGraph_BigInt = BigNumber;
   export type TheGraph_BigDecimal = FixedNumber;
