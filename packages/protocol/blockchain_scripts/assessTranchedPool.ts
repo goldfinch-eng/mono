4478c5fe8d78f6e84d880f0cd03d74910503c44e
Michael Zadra <michael@lazertechnologies.com>
2022-12-15 11:51:54 -0500

    Updates comments

--- packages/protocol/blockchain_scripts/assessTranchedPool.ts
+++ packages/protocol/blockchain_scripts/assessTranchedPool.ts
@@ -5,8 +5,9 @@ import {getEthersContract} from "./deployHelpers"
  * Assesses a TranchedPool in order to trigger a repayment from the Credit Line.
  *
  * On mainnet, we have a Defender autotask that runs every hour and calls TranchedPoool.assess(). This collects
- * payment from the Credit Line for the most recently *past* nextDueTime. On local we do not have this bot, thus
- * this allows us to manually trigger the assess for testing purposes
+ * payment from the Credit Line for the most recently *past* nextDueTime and updates the Credit Line accordingly.
+ *
+ * On local we do not have this bot, thus this allows us to manually trigger the assess for testing purposes.
  */
 export async function assessTranchedPool(tranchedPoolAddress: string) {
   console.log("💳 Start assessTranchedPool...")
