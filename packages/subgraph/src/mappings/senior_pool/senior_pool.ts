edec6dcef41c55f1f029c4214cb9a0df5312bfda
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2023-01-13 10:20:22 -0500

    added whitespace and improved code legibility

--- packages/subgraph/src/mappings/senior_pool/senior_pool.ts
+++ packages/subgraph/src/mappings/senior_pool/senior_pool.ts
@@ -36,14 +36,16 @@ import {
 } from "./helpers"
 
 export function handleDepositMade(event: DepositMade): void {
-  getOrInitUser(event.params.capitalProvider)
-
   const seniorPool = getOrInitSeniorPool()
   const seniorPoolContract = SeniorPoolContract.bind(event.address)
   const fiduContract = FiduContract.bind(getAddressFromConfig(seniorPoolContract, CONFIG_KEYS_ADDRESSES.Fidu))
+
   seniorPool.totalShares = fiduContract.totalSupply()
   seniorPool.assets = seniorPoolContract.assets()
+
   updateEstimatedSeniorPoolApy(seniorPool)
+  getOrInitUser(event.params.capitalProvider)
+
   seniorPool.save()
 
   const stakingRewardsAddress = Address.fromString(STAKING_REWARDS_ADDRESS)
@@ -56,7 +58,6 @@ export function handleDepositMade(event: DepositMade): void {
     transaction.sentToken = "USDC"
     transaction.receivedAmount = event.params.shares
     transaction.receivedToken = "FIDU"
-
     // usdc / fidu
     transaction.fiduPrice = usdcWithFiduPrecision(event.params.amount).div(event.params.shares)
 
@@ -67,8 +68,11 @@ export function handleDepositMade(event: DepositMade): void {
 export function handleInterestCollected(event: InterestCollected): void {
   const seniorPool = getOrInitSeniorPool()
   const seniorPoolContract = SeniorPoolContract.bind(event.address)
+
   seniorPool.sharePrice = seniorPoolContract.sharePrice()
+
   updateEstimatedApyFromGfiRaw(seniorPool)
+
   seniorPool.save()
 }
 
@@ -79,14 +83,18 @@ function handleInvestmentMadeInTranchedPool(
 ): void {
   const seniorPool = getOrInitSeniorPool()
   const seniorPoolContract = SeniorPoolContract.bind(seniorPoolAddress)
+  const tranchedPoolAddressString = tranchedPoolAddress.toHexString()
+
   seniorPool.totalLoansOutstanding = seniorPoolContract.totalLoansOutstanding()
   seniorPool.assets = seniorPoolContract.assets()
-  const tranchedPoolAddressString = tranchedPoolAddress.toHexString()
+
   if (!seniorPool.tranchedPools.includes(tranchedPoolAddressString)) {
     seniorPool.tranchedPools = seniorPool.tranchedPools.concat([tranchedPoolAddressString])
-    updateEstimatedSeniorPoolApy(seniorPool)
     seniorPool.totalInvested = seniorPool.totalInvested.plus(investedAmount)
+
+    updateEstimatedSeniorPoolApy(seniorPool)
   }
+
   seniorPool.save()
 }
 
@@ -103,23 +111,29 @@ export function handleInvestmentMadeInSenior(event: InvestmentMadeInSenior): voi
 export function handlePrincipalCollected(event: PrincipalCollected): void {
   const seniorPool = getOrInitSeniorPool()
   const seniorPoolContract = SeniorPoolContract.bind(event.address)
+
   seniorPool.sharePrice = seniorPoolContract.sharePrice()
-  updateEstimatedApyFromGfiRaw(seniorPool)
   seniorPool.totalLoansOutstanding = seniorPoolContract.totalLoansOutstanding()
   seniorPool.assets = seniorPoolContract.assets() // assets are updated when totalLoansOutstanding changes
+
+  updateEstimatedApyFromGfiRaw(seniorPool)
   updateEstimatedSeniorPoolApy(seniorPool)
+
   seniorPool.save()
 }
 
 export function handlePrincipalWrittenDown(event: PrincipalWrittenDown): void {
   const seniorPool = getOrInitSeniorPool()
   const seniorPoolContract = SeniorPoolContract.bind(event.address)
+
   seniorPool.sharePrice = seniorPoolContract.sharePrice()
-  updateEstimatedApyFromGfiRaw(seniorPool)
   seniorPool.assets = seniorPoolContract.assets()
-  updateEstimatedSeniorPoolApy(seniorPool)
   seniorPool.totalWrittenDown = seniorPool.totalWrittenDown.plus(event.params.amount)
+
+  updateEstimatedApyFromGfiRaw(seniorPool)
+  updateEstimatedSeniorPoolApy(seniorPool)
   updateDefaultRate(seniorPool)
+
   seniorPool.save()
 }
 
@@ -127,9 +141,12 @@ export function handleWithdrawalMade(event: WithdrawalMade): void {
   const seniorPool = getOrInitSeniorPool()
   const seniorPoolContract = SeniorPoolContract.bind(event.address)
   const fiduContract = FiduContract.bind(getAddressFromConfig(seniorPoolContract, CONFIG_KEYS_ADDRESSES.Fidu))
+
   seniorPool.assets = seniorPoolContract.assets()
-  updateEstimatedSeniorPoolApy(seniorPool)
   seniorPool.totalShares = fiduContract.totalSupply()
+
+  updateEstimatedSeniorPoolApy(seniorPool)
+
   seniorPool.save()
 
   const stakingRewardsAddress = Address.fromString(STAKING_REWARDS_ADDRESS)
@@ -140,6 +157,7 @@ export function handleWithdrawalMade(event: WithdrawalMade): void {
 
     const seniorPoolContract = SeniorPoolContract.bind(event.address)
     const sharePrice = seniorPoolContract.sharePrice()
+
     updateEstimatedApyFromGfiRaw(seniorPool)
 
     transaction.sentAmount = event.params.userAmount
@@ -165,46 +183,60 @@ export function handleWithdrawalMade(event: WithdrawalMade): void {
 
 export function handleWithdrawalRequest(event: WithdrawalRequested): void {
   const withdrawalRequest = new SeniorPoolWithdrawalRequest(event.params.operator.toHexString())
+
   withdrawalRequest.tokenId = event.params.tokenId
   withdrawalRequest.user = getOrInitUser(event.params.operator).id
   withdrawalRequest.usdcWithdrawable = BigInt.zero()
   withdrawalRequest.fiduRequested = event.params.fiduRequested
   withdrawalRequest.requestedAt = event.block.timestamp.toI32()
+
   withdrawalRequest.save()
 
   const roster = getOrInitSeniorPoolWithdrawalRoster()
+
   roster.requests = roster.requests.concat([withdrawalRequest.id])
+
   roster.save()
 
   const transaction = createTransactionFromEvent(event, "SENIOR_POOL_WITHDRAWAL_REQUEST", event.params.operator)
+
   transaction.sentAmount = event.params.fiduRequested
   transaction.sentToken = "FIDU"
+
   transaction.save()
 }
 
 export function handleAddToWithdrawalRequest(event: WithdrawalAddedTo): void {
   const withdrawalRequest = assert(SeniorPoolWithdrawalRequest.load(event.params.operator.toHexString()))
+
   withdrawalRequest.fiduRequested = withdrawalRequest.fiduRequested.plus(event.params.fiduRequested)
   withdrawalRequest.increasedAt = event.block.timestamp.toI32()
+
   withdrawalRequest.save()
 
   const transaction = createTransactionFromEvent(event, "SENIOR_POOL_ADD_TO_WITHDRAWAL_REQUEST", event.params.operator)
+
   transaction.sentAmount = event.params.fiduRequested
   transaction.sentToken = "FIDU"
+
   transaction.save()
 }
 
 export function handleWithdrawalRequestCanceled(event: WithdrawalCanceled): void {
   const withdrawalRequest = SeniorPoolWithdrawalRequest.load(event.params.operator.toHexString())
+
   if (withdrawalRequest) {
     withdrawalRequest.fiduRequested = BigInt.zero()
     withdrawalRequest.canceledAt = event.block.timestamp.toI32()
+
     withdrawalRequest.save()
   }
 
   const transaction = createTransactionFromEvent(event, "SENIOR_POOL_CANCEL_WITHDRAWAL_REQUEST", event.params.operator)
+
   transaction.receivedAmount = event.params.fiduCanceled
   transaction.receivedToken = "FIDU"
+
   transaction.save()
 }
 
@@ -212,19 +244,24 @@ const fiduZeroingThreshold = BigInt.fromString("10").pow(12)
 
 export function handleEpochEnded(event: EpochEnded): void {
   const epoch = new SeniorPoolWithdrawalEpoch(event.params.epochId.toString())
+
   epoch.epoch = event.params.epochId
   epoch.endsAt = event.params.endTime.toI32()
   epoch.fiduRequested = event.params.fiduRequested
   epoch.fiduLiquidated = event.params.fiduLiquidated
   epoch.usdcAllocated = event.params.usdcAllocated
+
   epoch.save()
 
   const transaction = createTransactionFromEvent(event, "SENIOR_POOL_DISTRIBUTION", event.address)
+
   transaction.sentAmount = epoch.usdcAllocated
   transaction.sentToken = "USDC"
+
   transaction.save()
 
   const roster = getOrInitSeniorPoolWithdrawalRoster()
+
   for (let i = 0; i < roster.requests.length; i++) {
     const withdrawalRequest = SeniorPoolWithdrawalRequest.load(roster.requests[i])
     if (!withdrawalRequest || withdrawalRequest.fiduRequested.isZero()) {
@@ -242,21 +279,25 @@ export function handleEpochEnded(event: EpochEnded): void {
     } else {
       withdrawalRequest.fiduRequested = newFiduRequested
     }
+
     withdrawalRequest.save()
 
     const disbursement = new SeniorPoolWithdrawalDisbursement(`${epoch.id}-${withdrawalRequest.id}`)
+
     disbursement.user = withdrawalRequest.user
     disbursement.tokenId = withdrawalRequest.tokenId
     disbursement.epoch = event.params.epochId
     disbursement.allocatedAt = epoch.endsAt
     disbursement.usdcAllocated = proRataUsdc
     disbursement.fiduLiquidated = fiduLiquidated
+
     disbursement.save()
   }
 }
 
 export function handleEpochExtended(event: EpochExtended): void {
   const roster = getOrInitSeniorPoolWithdrawalRoster()
+
   for (let i = 0; i < roster.requests.length; i++) {
     const withdrawalRequest = SeniorPoolWithdrawalRequest.load(roster.requests[i])
     if (!withdrawalRequest || withdrawalRequest.fiduRequested.isZero()) {
@@ -266,11 +307,13 @@ export function handleEpochExtended(event: EpochExtended): void {
     const postponement = new SeniorPoolWithdrawalDisbursementPostponement(
       `${event.params.epochId}-${withdrawalRequest.id}-${event.params.newEndTime}`
     )
+
     postponement.user = withdrawalRequest.user
     postponement.tokenId = withdrawalRequest.tokenId
     postponement.extendedEpoch = event.params.epochId
     postponement.oldEndsAt = event.params.oldEndTime.toI32()
     postponement.newEndsAt = event.params.newEndTime.toI32()
+
     postponement.save()
   }
 }
