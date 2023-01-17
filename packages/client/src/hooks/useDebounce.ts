cc64d797659f416a2368b892baa210d637735ac5
Michael Zadra <michael@lazertechnologies.com>
2023-01-16 21:29:01 -0500

    Removes client1 & subgraph legacy 🚮

--- packages/client/src/hooks/useDebounce.ts
+++
@@ -1,9 +0,0 @@
-import {useMemo} from "react"
-import _ from "lodash"
-
-function useDebounce(fn, ms = 500) {
-  const debounced = useMemo(() => _.debounce(fn, ms), [fn, ms])
-  return debounced
-}
-
-export default useDebounce
--- packages/client/src/types/util.ts
+++
@@ -1,4 +0,0 @@
-// Cf. https://jpwilliams.dev/how-to-unpack-the-return-type-of-a-promise-in-typescript
-export type AsyncReturnType<T extends (...args: any) => unknown> = T extends (...args: unknown[]) => Promise<infer U>
-  ? U
-  : never
