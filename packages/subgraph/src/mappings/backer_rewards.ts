cdbda655923b9777259bd7cee814f82b7195b36d
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2023-01-30 14:27:31 -0500

    activity log for harvested assets now works

--- packages/subgraph/src/mappings/backer_rewards.ts
+++ packages/subgraph/src/mappings/backer_rewards.ts
@@ -1,5 +1,6 @@
-import {BigInt} from "@graphprotocol/graph-ts"
-import {TranchedPoolToken} from "../../generated/schema"
+import {Address, BigInt, ethereum} from "@graphprotocol/graph-ts"
+
+import {TranchedPoolToken, VaultedPoolToken} from "../../generated/schema"
 import {
   BackerRewardsSetTotalRewards,
   BackerRewardsSetMaxInterestDollarsEligible,
@@ -23,17 +24,30 @@ export function handleSetMaxInterestDollarsEligible(event: BackerRewardsSetMaxIn
   calculateApyFromGfiForAllPools(event.block.timestamp)
 }
 
+function saveBackerRewardsClaimedTransaction(
+  event: ethereum.Event,
+  gfiAmount: BigInt,
+  poolToken: TranchedPoolToken
+): void {
+  let underlyingOwner = poolToken.user
+  if (poolToken.vaultedAsset != null) {
+    const vaultedPoolToken = assert(VaultedPoolToken.load(poolToken.vaultedAsset as string))
+    underlyingOwner = vaultedPoolToken.user
+  }
+  const transaction = createTransactionFromEvent(event, "BACKER_REWARDS_CLAIMED", Address.fromString(underlyingOwner))
+  transaction.receivedAmount = gfiAmount
+  transaction.receivedToken = "GFI"
+  transaction.tranchedPool = poolToken.tranchedPool
+  transaction.save()
+}
+
 export function handleBackerRewardsClaimed(event: BackerRewardsClaimed): void {
   const poolToken = assert(TranchedPoolToken.load(event.params.tokenId.toString()))
   poolToken.rewardsClaimed = event.params.amount
   poolToken.rewardsClaimable = BigInt.zero()
   poolToken.save()
 
-  const transaction = createTransactionFromEvent(event, "BACKER_REWARDS_CLAIMED", event.params.owner)
-  transaction.receivedAmount = event.params.amount
-  transaction.receivedToken = "GFI"
-  transaction.tranchedPool = poolToken.tranchedPool
-  transaction.save()
+  saveBackerRewardsClaimedTransaction(event, event.params.amount, poolToken)
 }
 
 export function handleBackerRewardsClaimed1(event: BackerRewardsClaimed1): void {
@@ -43,4 +57,10 @@ export function handleBackerRewardsClaimed1(event: BackerRewardsClaimed1): void 
   poolToken.rewardsClaimable = BigInt.zero()
   poolToken.stakingRewardsClaimable = BigInt.zero()
   poolToken.save()
+
+  saveBackerRewardsClaimedTransaction(
+    event,
+    event.params.amountOfTranchedPoolRewards.plus(event.params.amountOfSeniorPoolRewards),
+    poolToken
+  )
 }
