a929e15ed0ac11db1b7daf7050db8c476a76cae6
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2023-01-30 14:51:16 -0500

    unscramble comment

--- packages/client2/components/nuxes/all-nuxes.tsx
+++ packages/client2/components/nuxes/all-nuxes.tsx
@@ -1,4 +1,4 @@
-// If you want a nux to show when a new user visits, just add it to the return value for this component
+// If you want to show a nux when a new user visits, just add it to the return value for this component
 export function AllNuxes() {
   return null;
 }
