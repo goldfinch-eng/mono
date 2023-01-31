270197fee90e97dd986767ffbf05d8beaf98d9a1
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2023-01-31 11:03:01 -0500

    fixed a copy error in the withdrawal message tooltip

--- packages/client2/pages/pools/[address]/withdrawal-panel.tsx
+++ packages/client2/pages/pools/[address]/withdrawal-panel.tsx
@@ -167,7 +167,7 @@ export function WithdrawalPanel({
         <div className="mt-3 flex items-center justify-center gap-2 text-sm text-sand-700">
           <InfoIconTooltip
             size="sm"
-            content="While this Pool is still open for Backer investments, you can instantly withdraw any amount of the funds you have already invested. Once the Pool is has reached its Pool limit for funding and is closed for further investment, you will only be able to withdraw your share of the Pool's interest and principal repayments."
+            content="While this Pool is still open for Backer investments, you can instantly withdraw any amount of the funds you have already invested. Once the Pool has reached its limit for funding and is closed for further investment, you will only be able to withdraw your share of the Pool's interest and principal repayments."
           />
           You can withdraw capital until the pool is closed.
         </div>
