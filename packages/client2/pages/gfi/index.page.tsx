8e87aa1c6bb8b0785947336e8f36ef95a6a64736
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2023-02-10 10:11:48 -0500

    fixed a bug where the GFI page was not counting vaulted assets in its total claimable/locked

--- packages/client2/pages/gfi/index.page.tsx
+++ packages/client2/pages/gfi/index.page.tsx
@@ -89,17 +89,25 @@ export default function GfiPage() {
 
   const totalClaimable = sumTotalClaimable(
     grantsWithTokens,
-    data?.tranchedPoolTokens,
-    data?.seniorPoolStakedPositions
+    data?.tranchedPoolTokens.concat(
+      data?.vaultedPoolTokens.map((vpt) => vpt.poolToken)
+    ),
+    data?.seniorPoolStakedPositions.concat(
+      data?.vaultedStakedPositions.map((vst) => vst.seniorPoolStakedPosition)
+    )
   );
   const totalLocked = sumTotalLocked(
     grantsWithTokens,
-    data?.seniorPoolStakedPositions
+    data?.seniorPoolStakedPositions.concat(
+      data?.vaultedStakedPositions.map((vst) => vst.seniorPoolStakedPosition)
+    )
   );
 
   const userHasRewards =
     (data?.seniorPoolStakedPositions.length ?? 0) +
+      (data?.vaultedStakedPositions.length ?? 0) +
       (data?.tranchedPoolTokens.length ?? 0) +
+      (data?.vaultedPoolTokens.length ?? 0) +
       (grantsWithTokens?.length ?? 0) >
     0;
 
