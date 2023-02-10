a90a7c8ac3a7b4c3eaec0991eb828523b1f869d8
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2023-02-10 15:42:10 -0500

    initial working version of the ScrollingSectionedContainer

---
+++ packages/client2/components/design-system/scrolling-sectioned-container/scrolling-sectioned-container.tsx
@@ -0,0 +1,116 @@
+import clsx from "clsx";
+import { ReactNode, useEffect, useRef, useState } from "react";
+
+import { Sentinel } from "@/components/design-system";
+
+interface ScrollingSectionedContainerProps {
+  sections: { title: string; navTitle: string; content: ReactNode }[];
+  navAddons?: ReactNode;
+}
+
+const kebabCaseify = (s: string) => s.toLocaleLowerCase().replace(/\s/, "-");
+
+export function ScrollingSectionedContainer({
+  sections,
+  navAddons,
+}: ScrollingSectionedContainerProps) {
+  const sectionNodes = useRef<HTMLDivElement[]>([]);
+  const [isTopScrolled, setIsTopScrolled] = useState(true);
+  const [scrolledNavIndex, setScrolledNavIndex] = useState(0);
+
+  useEffect(() => {
+    if (!sectionNodes.current) {
+      return;
+    }
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        for (let i = 0; i < entries.length; i++) {
+          if (entries[i].isIntersecting) {
+            const index = parseInt(
+              entries[i].target.getAttribute("data-index") as string
+            );
+            setScrolledNavIndex(index);
+            break;
+          }
+        }
+      },
+      { threshold: 0.6, rootMargin: "20% 0% 0% 0%" }
+    );
+    for (const sectionNode of sectionNodes.current) {
+      intersectionObserver.observe(sectionNode);
+    }
+    return () => intersectionObserver.disconnect();
+  });
+
+  return (
+    <div className="relative">
+      <Sentinel onVisibilityChange={setIsTopScrolled} />
+      <div
+        className={clsx(
+          "sticky top-0 flex max-w-max gap-1 rounded-full bg-mustard-50 transition-shadow",
+          !isTopScrolled ? "shadow-md" : null
+        )}
+      >
+        {sections.map(({ navTitle }, index) => (
+          <ScrollNavLink
+            key={navTitle}
+            href={`#${kebabCaseify(navTitle)}`}
+            isScrolled={scrolledNavIndex === index}
+          >
+            {navTitle}
+          </ScrollNavLink>
+        ))}
+        {navAddons}
+      </div>
+      <div className="mt-8 space-y-15 px-4">
+        {sections.map(({ navTitle, title, content }, index) => (
+          <div
+            key={navTitle}
+            ref={(node) => {
+              if (node && sectionNodes.current) {
+                sectionNodes.current[index] = node;
+              }
+            }}
+            data-index={index}
+          >
+            <h2 className="font-base mb-2 font-semibold">
+              <a
+                className="group scroll-mt-15 hover:underline"
+                id={kebabCaseify(navTitle)}
+                href={`#${kebabCaseify(navTitle)}`}
+              >
+                {title}
+                <span className="hidden text-sand-400 group-hover:inline">
+                  &nbsp;#
+                </span>
+              </a>
+            </h2>
+            {content}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ScrollNavLink({
+  children,
+  href,
+  isScrolled,
+}: {
+  children: ReactNode;
+  href: string;
+  isScrolled: boolean;
+}) {
+  return (
+    <a
+      className={clsx(
+        "rounded-full px-4 py-2.5",
+        isScrolled ? "bg-mustard-100 font-medium" : "font-normal"
+      )}
+      href={href}
+    >
+      {children}
+    </a>
+  );
+}
