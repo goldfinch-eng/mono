f0f1b6942fa24a2381dd4b1f6a4594ec235f180d
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2023-02-14 15:56:17 -0500

    tweaks to colours on OpenDealCard

--- packages/client2/pages/earn/open-deal-card.tsx
+++ packages/client2/pages/earn/open-deal-card.tsx
@@ -40,12 +40,12 @@ export function OpenDealCard({
   return (
     <div
       className={clsx(
-        "group relative flex flex-col justify-between rounded-3xl border border-mustard-100 bg-mustard-100 py-8 px-10 transition-colors hover:bg-mustard-200",
+        "group relative flex flex-col justify-between rounded-3xl border border-mustard-200 bg-mustard-100 py-8 px-10 transition-colors hover:bg-mustard-200",
         className
       )}
     >
       <div className="mb-15 flex items-center">
-        <div className="relative h-12 w-12 shrink-0 overflow-hidden rounded-full bg-white">
+        <div className="relative h-12 w-12 shrink-0 overflow-hidden rounded-full bg-mustard-500">
           {icon ? (
             <Image
               src={icon}
@@ -112,7 +112,7 @@ export function OpenDealCardPlaceholder() {
   return (
     <div className="relative flex flex-col justify-between rounded-3xl border border-mustard-100 bg-mustard-100 py-8 px-10">
       <div className="mb-15 flex items-center">
-        <div className="relative h-12 w-12 shrink-0 overflow-hidden rounded-full bg-white" />
+        <div className="relative h-12 w-12 shrink-0 overflow-hidden rounded-full bg-sand-100" />
         <div className="ml-4 flex w-full flex-col">
           <div className="mb-1 font-semibold">
             <ShimmerLines lines={1} className="w-full" />
