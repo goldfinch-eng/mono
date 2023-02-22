886494f41b7f51773ae47c2aa1e8ffe70cdc1ccf
Carter Appleton <carter@warblerlabs.com>
2023-02-22 10:27:52 -0800

    [Autotask][GFI-1395] Assessor should call when pool is late (#1430)

--- packages/autotasks/assessor/index.ts
+++ packages/autotasks/assessor/index.ts
@@ -1,15 +1,17 @@
-import {ethers} from "ethers"
+import {BigNumber, ethers} from "ethers"
 import {Relayer} from "defender-relay-client"
 import {DefenderRelaySigner, DefenderRelayProvider} from "defender-relay-client/lib/ethers"
 import axios from "axios"
 import {asNonNullable, INVALID_POOLS} from "@goldfinch-eng/utils"
 import baseHandler from "../core/handler"
+import {CreditLine, ERC20, PoolTokens, SeniorPool, TranchedPool} from "@goldfinch-eng/protocol/typechain/ethers"
 
 const CONFIG = {
   mainnet: {
     factoryAddress: "0xd20508E1E971b80EE172c73517905bfFfcBD87f9",
     poolTokensAddress: "0x57686612C601Cb5213b01AA8e80AfEb24BBd01df",
     seniorPoolAddress: "0x8481a6EbAf5c7DABc3F7e09e44A89531fd31F822",
+    usdcAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     etherscanApi: "https://api.etherscan.io/api",
   },
 }
@@ -34,9 +36,11 @@ exports.handler = baseHandler("assessor", async function (credentials) {
   const factoryAbi = await getAbifor(config.etherscanApi, goldfinchFactoryAddress, provider, etherscanApiKey)
   const factory = new ethers.Contract(goldfinchFactoryAddress, factoryAbi, signer)
   const poolTokensAbi = await getAbifor(config.etherscanApi, config.poolTokensAddress, provider, etherscanApiKey)
-  const poolTokens = new ethers.Contract(config.poolTokensAddress, poolTokensAbi, signer)
+  const poolTokens = new ethers.Contract(config.poolTokensAddress, poolTokensAbi, signer) as PoolTokens
   const seniorPoolAbi = await getAbifor(config.etherscanApi, config.seniorPoolAddress, provider, etherscanApiKey)
-  const seniorPool = new ethers.Contract(config.seniorPoolAddress, seniorPoolAbi, signer)
+  const seniorPool = new ethers.Contract(config.seniorPoolAddress, seniorPoolAbi, signer) as SeniorPool
+  const usdcAbi = await getAbifor(config.etherscanApi, config.usdcAddress, provider, etherscanApiKey)
+  const usdc = new ethers.Contract(config.usdc, usdcAbi, signer) as ERC20
 
   const filter = asNonNullable(factory.filters.PoolCreated)
   const result = await factory.queryFilter(filter(null, null))
@@ -59,18 +63,18 @@ exports.handler = baseHandler("assessor", async function (credentials) {
   }
 
   const poolAbi = await getAbifor(config.etherscanApi, pools[0], provider, etherscanApiKey)
-  const pool = new ethers.Contract(asNonNullable(pools[0]), poolAbi, signer)
+  const pool = new ethers.Contract(asNonNullable(pools[0]), poolAbi, signer) as TranchedPool
 
   const creditLineAddress = await pool.creditLine()
   const creditLineAbi = await getAbifor(config.etherscanApi, creditLineAddress, provider, etherscanApiKey)
-  const creditLine = new ethers.Contract(creditLineAddress, creditLineAbi, signer)
+  const creditLine = new ethers.Contract(creditLineAddress, creditLineAbi, signer) as CreditLine
 
   console.log(`Found ${pools.length} tranched pools`)
   let success = 0
   for (const poolAddress of pools) {
     try {
       console.log(`Assessing ${poolAddress}`)
-      await assessIfRequired(pool.attach(poolAddress), creditLine, provider, seniorPool, poolTokens)
+      await assessIfRequired(pool.attach(poolAddress), creditLine, provider, seniorPool, poolTokens, usdc)
       success += 1
     } catch (err) {
       console.log(`Error trying to assess creditline: ${err}`)
@@ -85,45 +89,94 @@ exports.handler = baseHandler("assessor", async function (credentials) {
 })
 
 const assessIfRequired = async function assessIfRequired(
-  tranchedPool,
-  creditLineContract,
+  tranchedPool: TranchedPool,
+  creditLineContract: CreditLine,
   provider,
-  seniorPool,
-  poolTokens
-) {
-  // Normalize everything to ethers.BigNumber because tests use Truffle and therefore bn.js
-  // which is incompatible with BigNumber
+  seniorPool: SeniorPool,
+  poolTokens: PoolTokens,
+  usdc: ERC20
+): Promise<boolean> {
   const creditLineAddress = await tranchedPool.creditLine()
   const creditLine = creditLineContract.attach(creditLineAddress)
-  const currentTime = ethers.BigNumber.from((await provider.getBlock("latest")).timestamp.toString())
-  const nextDueTime = ethers.BigNumber.from((await creditLine.nextDueTime()).toString())
-  const termEndTime = ethers.BigNumber.from((await creditLine.termEndTime()).toString())
+  const {timestamp: currentTime} = await provider.getBlock("latest")
+  const nextDueTime = await creditLine.nextDueTime()
+  const termEndTime = await creditLine.termEndTime()
+  const balance = await creditLine.balance()
+  const usdcBalance = await usdc.balanceOf(creditLineAddress)
   const limit = await creditLine.limit()
 
   if (limit.isZero()) {
     console.log(`Assess ${creditLine.address}: Skipped (Closed)`)
-    return
+    return false
   }
 
   if (nextDueTime.isZero()) {
-    const balance = await creditLine.balance()
     if (!balance.isZero()) {
       throw new Error(`Non-zero balance (${balance}) for pool ${tranchedPool.address} without a nextDueTime`)
     }
     console.log(`Assess ${tranchedPool.address}: Skipped (Zero balance)`)
-  } else {
-    if (currentTime.gte(termEndTime)) {
-      // Currently we don't have a good way to track the last time we assessed a creditLine past it's
-      // term end block. So we're going to keep assessing it everytime the script runs for now.
-      console.log(`Assessing pool beyond the end time: ${tranchedPool.address}`)
-      await assessAndRedeem(tranchedPool, seniorPool, poolTokens)
-    } else if (currentTime.gte(nextDueTime)) {
-      console.log(`Assessing ${tranchedPool.address}`)
-      await assessAndRedeem(tranchedPool, seniorPool, poolTokens)
-    } else {
-      const nextDueTimeFormatted = new Date(nextDueTime.toNumber() * 1000).toISOString()
-      console.log(`Assess ${tranchedPool.address}: Skipped (Already assessed). Next Due: ${nextDueTimeFormatted}`)
+    return false
+  }
+
+  if (currentTime.gte(termEndTime)) {
+    // Currently we don't have a good way to track the last time we assessed a creditLine past it's
+    // term end block. So we're going to keep assessing it everytime the script runs for now.
+
+    console.log(`Assessing pool beyond the end time: ${tranchedPool.address}`)
+    await assessAndRedeem(tranchedPool, seniorPool, poolTokens)
+    return true
+  }
+
+  if (currentTime.gte(nextDueTime)) {
+    // CreditLine is past the due time, assess in case there's anything waiting to be allocated.
+
+    console.log(`Assessing ${tranchedPool.address} (past next due time)`)
+    await assessAndRedeem(tranchedPool, seniorPool, poolTokens)
+    return true
+  }
+
+  if (await isLate(creditLine, balance, currentTime)) {
+    // CreditLine might have been paid on time, but only partially. Check if it's late on payments
+    // and assess if there's USDC waiting for assessment.
+
+    if (usdcBalance.isZero()) {
+      // Skip assessing if usdc balance is zero because assessing will have no impact and just burn gas
+      console.log(`Skipped assessing late pool as there's no pending USDC: ${tranchedPool.address}`)
+      return false
     }
+
+    console.log(`Assessing ${tranchedPool.address} (late on payment)`)
+    await assessAndRedeem(tranchedPool, seniorPool, poolTokens)
+    return true
+  }
+
+  // At this point, the credit line
+  // 1. Is not closed
+  // 2. Is not new
+  // 3. Is not past its end time
+  // 4. Is not past its due time
+  // 5. Is not late
+  // So there's no reason to assess
+
+  const nextDueTimeFormatted = new Date(nextDueTime.toNumber() * 1000).toISOString()
+  console.log(`Assess ${tranchedPool.address}: Skipped (Already assessed). Next Due: ${nextDueTimeFormatted}`)
+  return false
+}
+
+async function isLate(creditLine: CreditLine, balance: BigNumber, timestamp: BigNumber) {
+  try {
+    return await creditLine.isLate()
+  } catch {
+    // Not all CredtLines have a public isLate. For those, calculate isLate using the
+    // same logic they declare internally.
+
+    const SECONDS_PER_DAY = BigNumber.from(24 * 60 * 60)
+    const [lastFullPaymentTime, paymentPeriodInDays] = await Promise.all([
+      creditLine.lastFullPaymentTime(),
+      creditLine.paymentPeriodInDays(),
+    ])
+    const secondsElapsedSinceFullPayment = timestamp.sub(lastFullPaymentTime)
+    return !balance.isZero() && secondsElapsedSinceFullPayment.gt(paymentPeriodInDays.mul(SECONDS_PER_DAY))
   }
 }
 
