a2e3cd458fac80f141e105b7b0b3114c40c10f20
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2023-02-27 21:49:18 -0500

    added perspective to senior pool repayment

--- packages/client2/pages/pools/senior/senior-pool-repayment.tsx
+++ packages/client2/pages/pools/senior/senior-pool-repayment.tsx
@@ -1,8 +1,10 @@
 import { gql } from "@apollo/client";
+import { Menu } from "@headlessui/react";
 import { format as formatDate } from "date-fns";
 import Image from "next/future/image";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
+import { Icon } from "@/components/design-system";
 import { formatCrypto } from "@/lib/format";
 import { SeniorPoolRepaymentFieldsFragment } from "@/lib/graphql/generated";
 import {
@@ -34,6 +36,7 @@ export function SeniorPoolRepaymentSection({
   seniorPool,
 }: SeniorPoolRepaymentSectionProps) {
   const { repayingPools } = seniorPool;
+  const [perspective, setPerspective] = useState<"past" | "future">("future");
   const allIncomingRepayments = useMemo(() => {
     const now = new Date();
     const beginningOfThisMonth = new Date(
@@ -56,25 +59,59 @@ export function SeniorPoolRepaymentSection({
           borrowerLogo: pool.borrowerLogo,
         }))
       )
-      .filter(
-        (repayment) =>
-          repayment.estimatedPaymentDate >= beginningOfThisMonth.getTime() &&
-          repayment.estimatedPaymentDate <=
-            oneYearFromBeginningOfMonth.getTime()
-      );
+      .filter((repayment) => {
+        if (perspective === "future") {
+          return (
+            repayment.estimatedPaymentDate >= beginningOfThisMonth.getTime() &&
+            repayment.estimatedPaymentDate <=
+              oneYearFromBeginningOfMonth.getTime()
+          );
+        } else {
+          return repayment.estimatedPaymentDate < now.getTime();
+        }
+      });
     allIncomingRepayments.sort(
       (a, b) => a.estimatedPaymentDate - b.estimatedPaymentDate
     );
     return allIncomingRepayments;
-  }, [repayingPools]);
+  }, [repayingPools, perspective]);
   return (
     <div className="overflow-hidden rounded-lg border border-sand-300">
-      <div className="py-8 px-6">chart goes here</div>
+      <div className="py-8 px-6">
+        <div>
+          <Menu as="div" className="relative">
+            <Menu.Button className="flex items-center gap-2 text-xs font-medium">
+              {perspective === "future"
+                ? "Future repayments"
+                : "Past repayments"}
+              <Icon name="ChevronDown" />
+            </Menu.Button>
+            <Menu.Items className="absolute left-0 z-50 mt-1 flex flex-col rounded-md bg-white text-xs">
+              <Menu.Item>
+                <button
+                  className="block p-2 text-left hover:bg-sand-50"
+                  onClick={() => setPerspective("future")}
+                >
+                  Future repayments
+                </button>
+              </Menu.Item>
+              <Menu.Item>
+                <button
+                  className="block p-2 text-left hover:bg-sand-50"
+                  onClick={() => setPerspective("past")}
+                >
+                  Past repayments
+                </button>
+              </Menu.Item>
+            </Menu.Items>
+          </Menu>
+        </div>
+      </div>
       <div className="max-h-80 overflow-y-auto">
         <table className="w-full text-xs [&_th]:px-3.5 [&_th]:py-2 [&_th]:font-normal [&_td]:px-3.5 [&_td]:py-2">
           <thead>
             <tr className="sticky top-0 z-10 bg-mustard-100">
-              <th scope="col" className="text-left">
+              <th scope="col" className="w-1/2 text-left">
                 Source
               </th>
               <th scope="col" className="text-left">
@@ -100,7 +137,7 @@ export function SeniorPoolRepaymentSection({
                 <tr key={index}>
                   <td className="flex items-center gap-1 text-left">
                     {borrowerLogo ? (
-                      <div className="relative h-3 w-3 overflow-hidden rounded-full">
+                      <div className="relative h-3 w-3 shrink-0 overflow-hidden rounded-full">
                         <Image src={borrowerLogo} fill sizes="12px" alt="" />
                       </div>
                     ) : null}
