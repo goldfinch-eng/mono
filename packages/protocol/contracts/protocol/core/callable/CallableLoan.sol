80ace9c41782bd2956c548a9dba20a3597fa751c
Brandon Chow <brandonstchow@gmail.com>
2023-02-28 21:09:55 -0800

    Deposit changes wip

--- packages/protocol/contracts/protocol/core/callable/CallableLoan.sol
+++ packages/protocol/contracts/protocol/core/callable/CallableLoan.sol
@@ -231,19 +231,14 @@ contract CallableLoan is
 
   /// @inheritdoc ILoan
   /// @notice Supply capital to the loan.
-  /// @param tranche *UNSUPPORTED* - Should always be uncalled capital tranche index.
   /// @param amount amount of capital to supply
   /// @return tokenId NFT representing your position in this pool
-  function deposit(
-    uint256 tranche,
-    uint256 amount
-  ) external override nonReentrant whenNotPaused returns (uint256) {
-    return _deposit(tranche, amount);
+  function deposit(uint256 amount) external override nonReentrant whenNotPaused returns (uint256) {
+    return _deposit(amount);
   }
 
   /// @inheritdoc ILoan
   /// @notice Supply capital to the loan.
-  /// @param tranche *UNSUPPORTED* -
   /// @param amount amount of capital to supply
   /// @param deadline deadline of permit operation
   /// @param v v portion of signature
@@ -251,7 +246,6 @@ contract CallableLoan is
   /// @param s s portion of signature
   /// @return tokenId NFT representing your position in this pool
   function depositWithPermit(
-    uint256 tranche,
     uint256 amount,
     uint256 deadline,
     uint8 v,
@@ -267,7 +261,7 @@ contract CallableLoan is
       r,
       s
     );
-    return _deposit(tranche, amount);
+    return _deposit(amount);
   }
 
   /// @inheritdoc ILoan
@@ -494,24 +488,22 @@ contract CallableLoan is
   /// @dev NA: not authorized. Must have correct UID or be go listed
   /// @dev NF: not open for funding. Must be after fundableAt
   /// @notice Supply capital to the loan.
-  /// @param tranche *UNSUPPORTED* - Should always be uncalled capital tranche index.
   /// @param amount amount of capital to supply
   /// @return tokenId NFT representing your position in this pool
-  function _deposit(uint256 tranche, uint256 amount) internal returns (uint256) {
+  function _deposit(uint256 amount) internal returns (uint256) {
     CallableCreditLine storage cl = _staleCreditLine.checkpoint();
     require(amount > 0, "ZA");
-    require(tranche == cl.uncalledCapitalTrancheIndex(), "IT");
     require(hasAllowedUID(msg.sender), "NA");
     require(block.timestamp >= fundableAt, "NF");
 
     cl.deposit(amount);
     uint256 tokenId = config.getPoolTokens().mint(
-      IPoolTokens.MintParams({tranche: tranche, principalAmount: amount}),
+      IPoolTokens.MintParams({tranche: cl.uncalledCapitalTrancheIndex(), principalAmount: amount}),
       msg.sender
     );
     config.getUSDC().safeTransferFrom(msg.sender, address(this), amount);
 
-    emit DepositMade(msg.sender, tranche, tokenId, amount);
+    emit DepositMade(msg.sender, cl.uncalledCapitalTrancheIndex(), tokenId, amount);
     return tokenId;
   }
 
