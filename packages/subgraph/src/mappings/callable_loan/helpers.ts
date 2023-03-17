81cc9cab40494b5545a97bab690c1d622d0a8396
Michael Zadra <michael@lazertechnologies.com>
2023-03-17 08:59:26 -0400

    Use 1 line for credit line address set

--- packages/subgraph/src/mappings/callable_loan/helpers.ts
+++ packages/subgraph/src/mappings/callable_loan/helpers.ts
@@ -10,8 +10,7 @@ export function initCallableLoan(address: Address, block: ethereum.Block): Calla
   const callableLoan = new CallableLoan(id)
   const callableLoanContract = CallableLoanContract.bind(address)
   callableLoan.address = address
-  const creditLineAddress = callableLoanContract.creditLine()
-  callableLoan.creditLineAddress = creditLineAddress
+  callableLoan.creditLineAddress = callableLoanContract.creditLine()
   callableLoan.fundingLimit = callableLoanContract.limit()
   callableLoan.principalAmount = BigInt.zero()
   callableLoan.initialInterestOwed = BigInt.zero() // TODO figure this out. There may be a view function for this
