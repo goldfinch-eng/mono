d4e2e57a83445328b71cb2d9608966b68d082a97
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2023-03-19 20:14:31 -0400

    call requests are disabled during drawdown phase now

--- packages/client2/pages/pools/[address]/call/call-panel.tsx
+++ packages/client2/pages/pools/[address]/call/call-panel.tsx
@@ -27,6 +27,7 @@ gql`
   fragment CallPanelCallableLoanFields on CallableLoan {
     id
     address
+    loanPhase @client
     inLockupPeriod @client
     nextPrincipalDueTime @client
   }
@@ -39,6 +40,8 @@ interface CallPanel {
 }
 
 export function CallPanel({ className, callableLoan, poolTokens }: CallPanel) {
+  const shouldDisableCalls =
+    callableLoan.inLockupPeriod || callableLoan.loanPhase !== "InProgress";
   const [isCallRequestModalOpen, setIsCallRequestModalOpen] = useState(false);
   const callablePoolTokens = poolTokens.filter((pt) => !pt.isCapitalCalled);
   const calledPoolTokens = poolTokens.filter((pt) => pt.isCapitalCalled);
@@ -68,45 +71,43 @@ export function CallPanel({ className, callableLoan, poolTokens }: CallPanel) {
 
   return (
     <div className={className}>
-      {calledPoolTokens.length === 0 ? (
-        <div>
-          <Button
-            size="xl"
-            colorScheme="transparent-mustard"
-            className="block w-full"
-            onClick={() => setIsCallRequestModalOpen(true)}
-            disabled={callableLoan.inLockupPeriod}
-          >
-            Submit call request
-          </Button>
-          {callableLoan.inLockupPeriod ? (
-            <div className="mt-1 text-sm text-sand-500">
-              You may not call capital during the lockup period. Call requests
-              become available after{" "}
-              {formatDate(
-                callableLoan.nextPrincipalDueTime * 1000,
-                "MMM d, yyyy"
-              )}
-            </div>
-          ) : null}
-        </div>
-      ) : (
-        <div>
+      {calledPoolTokens.length > 0 ? (
+        <>
           <div className="mb-3 flex justify-between text-sm">
             <div>Call requests</div>
             <InfoIconTooltip content="Call requests you have made for this deal." />
           </div>
-          <MiniTable colorScheme="mustard" rows={miniTableRows} />
-          <Button
-            size="xl"
-            colorScheme="transparent-mustard"
-            className="mt-5 block w-full"
-            onClick={() => setIsCallRequestModalOpen(true)}
-          >
-            Submit another call request
-          </Button>
+          <MiniTable
+            className="mb-5"
+            colorScheme="mustard"
+            rows={miniTableRows}
+          />
+        </>
+      ) : null}
+      <Button
+        size="xl"
+        colorScheme="transparent-mustard"
+        className="block w-full"
+        onClick={() => setIsCallRequestModalOpen(true)}
+        disabled={shouldDisableCalls}
+      >
+        {calledPoolTokens.length > 0
+          ? "Submit another call request"
+          : "Submit call request"}
+      </Button>
+      {shouldDisableCalls ? (
+        <div className="mt-1 text-sm text-sand-500">
+          {callableLoan.inLockupPeriod
+            ? `You may not call capital during the lockup period. Call requests
+              become available after ${formatDate(
+                callableLoan.nextPrincipalDueTime * 1000,
+                "MMM d, yyyy"
+              )}.`
+            : callableLoan.loanPhase === "DrawdownPeriod"
+            ? "You may not submit a call yet because this loan is still in the drawdown phase."
+            : null}
         </div>
-      )}
+      ) : null}
 
       <SubmitCallModal
         callableLoanAddress={callableLoan.address}
