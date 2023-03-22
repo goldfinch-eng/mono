6eede0844bf63d80c86aa9632ff2b5a081242a77
daltyboy11 <daltyboy11@gmail.com>
2023-03-22 12:33:48 -0400

    add invariants for PoolTokenInfo on the most recently interacted with token

--- packages/protocol/contracts/test/core/callable/scenarios/CallableLoan.funding.invariant.t.sol
+++ packages/protocol/contracts/test/core/callable/scenarios/CallableLoan.funding.invariant.t.sol
@@ -12,10 +12,17 @@ import {IERC20} from "../../../../interfaces/IERC20.sol";
 // import {console2 as console} from "forge-std/console2.sol";
 
 contract CallableLoanHandler is Test {
+  struct TokenInfo {
+    uint256 tokenId;
+    uint256 originalDeposited;
+    uint256 withdrawn;
+  }
+
   CallableLoan public loan;
   uint256 public sumDeposited;
   uint256 public sumWithdrawn;
-  uint256[] public poolTokens;
+  TokenInfo[] public poolTokens;
+  uint256 public indexOfLastTokenInteractedWith = type(uint256).max;
 
   constructor(CallableLoan _loan, IERC20 usdc) public {
     // vm = _vm;
@@ -34,8 +41,9 @@ contract CallableLoanHandler is Test {
 
     uint256 token = loan.deposit(loan.uncalledCapitalTrancheIndex(), amount);
 
+    indexOfLastTokenInteractedWith = poolTokens.length;
     sumDeposited += amount;
-    poolTokens.push(token);
+    poolTokens.push(TokenInfo({tokenId: token, originalDeposited: amount, withdrawn: 0}));
   }
 
   function withdraw(uint256 tokenIndex, uint256 amount) public {
@@ -45,8 +53,8 @@ contract CallableLoanHandler is Test {
     }
 
     tokenIndex = bound(tokenIndex, 0, poolTokens.length - 1);
-    uint256 token = poolTokens[tokenIndex];
-    (, uint256 principalRedeemable) = loan.availableToWithdraw(token);
+    TokenInfo storage tokenInfo = poolTokens[tokenIndex];
+    (, uint256 principalRedeemable) = loan.availableToWithdraw(tokenInfo.tokenId);
 
     if (principalRedeemable == 0) {
       // Pool token already fully withdrawn - return!
@@ -55,9 +63,15 @@ contract CallableLoanHandler is Test {
 
     amount = bound(amount, 1, principalRedeemable);
 
-    loan.withdraw(token, amount);
+    loan.withdraw(tokenInfo.tokenId, amount);
 
     sumWithdrawn += amount;
+    tokenInfo.withdrawn += amount;
+    indexOfLastTokenInteractedWith = tokenIndex;
+  }
+
+  function getTokenInfoByIndex(uint256 i) public view returns (TokenInfo memory) {
+    return poolTokens[i];
   }
 
   // So we can mint UIDs to the test class
@@ -95,32 +109,109 @@ contract CallableLoanFundingInvariantTest is CallableLoanBaseTest, InvariantTest
     targetSelector(FuzzSelector({addr: address(handler), selectors: selectors}));
   }
 
-  function invariantBalanceOfPoolIsDepositsLessWithdrawals() public {
+  /// USDC balance invariants
+
+  function invariant_BalanceOfPoolIsDepositsLessWithdrawals() public {
     assertEq(
       usdc.balanceOf(address(handler.loan())),
       handler.sumDeposited() - handler.sumWithdrawn()
     );
   }
 
-  function invariantBalanceOfDepositorIsOriginalBalanceLessDepositsPlusWithdrawals() public {
+  function invariant_BalanceOfDepositorIsOriginalBalanceLessDepositsPlusWithdrawals() public {
     assertEq(
       usdc.balanceOf(address(handler)),
       handlerOriginalUsdcBalance - handler.sumDeposited() + handler.sumWithdrawn()
     );
   }
 
-  function invariantUncalledCapitalInfoPrincipalDepositedIsDepositsLessWithdrawals() public {
+  /// UncalledCapitalInfo invariants
+
+  function invariant_UncalledCapitalInfoPrincipalDepositedIsDepositsLessWithdrawals() public {
     assertEq(
       handler.loan().getUncalledCapitalInfo().principalDeposited,
       handler.sumDeposited() - handler.sumWithdrawn()
     );
   }
 
-  function invariantUncalledCapitalInfoPrincipalPaidIsZero() public {
-    // Should this be 0?
+  function invariant_UncalledCapitalInfoPrincipalPaidIsZero() public {
     assertEq(
       handler.loan().getUncalledCapitalInfo().principalPaid,
       handler.sumDeposited() - handler.sumWithdrawn()
+    );
+  }
+
+  function invariant_UncalledCapitalInfoPrincipalReservedIsZero() public {
+    assertZero(handler.loan().getUncalledCapitalInfo().principalReserved);
+  }
+
+  function invariant_UncalledCapitalInfoInterestPaidIsZero() public {
+    assertZero(handler.loan().getUncalledCapitalInfo().interestPaid);
+  }
+
+  /// PoolTokenInfo invariants
+
+  function invariant_TokenInfoTrancheIsUncalledCapitalTranche() public {
+    uint256 tokenIndex = handler.indexOfLastTokenInteractedWith();
+    if (tokenIndex == type(uint256).max) {
+      // Early return if there have been no deposits yet
+      return;
+    }
+
+    assertEq(
+      poolTokens.getTokenInfo(handler.getTokenInfoByIndex(tokenIndex).tokenId).tranche,
+      handler.loan().uncalledCapitalTrancheIndex()
+    );
+  }
+
+  function invariant_TokenInfoPoolIsCallableLoan() public {
+    uint256 tokenIndex = handler.indexOfLastTokenInteractedWith();
+    if (tokenIndex == type(uint256).max) {
+      // Early return if there have been no deposits yet
+      return;
+    }
+
+    assertEq(
+      poolTokens.getTokenInfo(handler.getTokenInfoByIndex(tokenIndex).tokenId).pool,
+      address(handler.loan())
+    );
+  }
+
+  function invariant_TokenInfoPrincipalAmountIsAmountDeposited() public {
+    uint256 tokenIndex = handler.indexOfLastTokenInteractedWith();
+    if (tokenIndex == type(uint256).max) {
+      // Early return if there have been no deposits yet
+      return;
+    }
+
+    assertEq(
+      poolTokens.getTokenInfo(handler.getTokenInfoByIndex(tokenIndex).tokenId).principalAmount,
+      handler.getTokenInfoByIndex(tokenIndex).originalDeposited -
+        handler.getTokenInfoByIndex(tokenIndex).withdrawn
+    );
+  }
+
+  function invariant_TokenInfoPrincipalRedeemedIsZero() public {
+    uint256 tokenIndex = handler.indexOfLastTokenInteractedWith();
+    if (tokenIndex == type(uint256).max) {
+      // Early return if there have been no deposits yet
+      return;
+    }
+
+    assertZero(
+      poolTokens.getTokenInfo(handler.getTokenInfoByIndex(tokenIndex).tokenId).principalRedeemed
+    );
+  }
+
+  function invariant_TokenInfoInterestRedeemedIsZero() public {
+    uint256 tokenIndex = handler.indexOfLastTokenInteractedWith();
+    if (tokenIndex == type(uint256).max) {
+      // Early return if there have been no deposits yet
+      return;
+    }
+
+    assertZero(
+      poolTokens.getTokenInfo(handler.getTokenInfoByIndex(tokenIndex).tokenId).interestRedeemed
     );
   }
 }
