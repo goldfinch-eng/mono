8cec92d8e4131849f8d8b94821749fd8e081e532
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2023-03-28 11:55:47 -0400

    added a nux for the Fazz deal to AllNuxes

--- packages/client2/components/nuxes/all-nuxes.tsx
+++ packages/client2/components/nuxes/all-nuxes.tsx
@@ -1,4 +1,6 @@
+import { CallableLoanNux } from "./callable-loan-nux";
+
 // If you want to show a nux when a new user visits, just add it to the return value for this component
 export function AllNuxes() {
-  return null;
+  return <CallableLoanNux />;
 }
