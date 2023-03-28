0f27fd81083d036c0818f58b0ba68749c9fef5a3
Shalini Pyapali <shalu.pyapali@gmail.com>
2023-03-27 21:41:01 -0400

    Add link to GIP-44

--- packages/client2/components/nuxes/senior-pool-cancellation-fee-nux.tsx
+++ packages/client2/components/nuxes/senior-pool-cancellation-fee-nux.tsx
@@ -4,8 +4,15 @@ export function SeniorPoolCancellationFeeNux() {
   return (
     <Nux prefix="cancellationfee" version={1}>
       <div className="mb5 text-center">
-        In accordance with GIP-44, the cancellation fee for Senior Pool
-        withdrawals has been temporarily changed to 0.
+        In accordance with{" "}
+        <a
+          className="underline"
+          href="https://gov.goldfinch.finance/t/gip-44-temporarily-set-the-withdraw-cancellation-request-fee-to-0/1673"
+        >
+          GIP-44
+        </a>
+        , the cancellation fee for Senior Pool withdrawals has been temporarily
+        changed to 0.
       </div>
     </Nux>
   );
