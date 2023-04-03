0ba250baf3a2945267bb6f78aa405646ef2925c0
Shalini Pyapali <shalu.pyapali@gmail.com>
2023-04-03 16:04:44 -0400

    Modify styling on the account page
    Modifies by:
    1. Adding new naked layout without any of the padding
    2. Add mustard colored background to the appropriate components

--- packages/client2/pages/account/index.page.tsx
+++ packages/client2/pages/account/index.page.tsx
@@ -1,21 +1,42 @@
-import { TabButton, TabGroup, TabList } from "@/components/design-system";
+import {
+  TabButton,
+  TabContent,
+  TabGroup,
+  TabList,
+  TabPanels,
+} from "@/components/design-system";
 import { NextPageWithLayout } from "@/pages/_app.page";
 
 const AccountsPage: NextPageWithLayout = () => {
   return (
-    <div className="display: flex flex-col">
-      <text className="font-serif text-5xl font-bold text-sand-800">
-        Account
-      </text>
+    <div>
+      <div className="bg-mustard-100">
+        <div className="mx-auto max-w-7xl px-5 py-16">
+          <h1 className="font-serif text-5xl font-bold text-sand-800">
+            Account
+          </h1>
+        </div>
+      </div>
       <TabGroup>
-        <TabList>
-          <TabButton>UID and Wallets</TabButton>
-        </TabList>
+        <div className="bg-mustard-100">
+          <div className="mx-auto max-w-7xl px-5">
+            <TabList>
+              <TabButton>UID and Wallets</TabButton>
+            </TabList>
+          </div>
+        </div>
+        <div className="px-5">
+          <div className="mx-auto max-w-7xl pt-0">
+            <TabPanels>
+              <TabContent>Content 1</TabContent>
+            </TabPanels>
+          </div>
+        </div>
       </TabGroup>
     </div>
   );
 };
 
-AccountsPage.layout = "mustard-background";
+AccountsPage.layout = "naked";
 
 export default AccountsPage;
