30a55331169c14ee1278fac32c3cc890511ac4f1
Shalini Pyapali <shalu.pyapali@gmail.com>
2023-04-03 13:18:12 -0400

    Update index.page.tsx
    Added a basic layout

--- packages/client2/pages/account/index.page.tsx
+++ packages/client2/pages/account/index.page.tsx
@@ -1,5 +1,21 @@
+import { TabButton, TabGroup, TabList } from "@/components/design-system";
 import { NextPageWithLayout } from "@/pages/_app.page";
 
-const AccountsPage: NextPageWithLayout = () => <div>My name is Shalini</div>;
+const AccountsPage: NextPageWithLayout = () => {
+  return (
+    <div className="display: flex flex-col">
+      <text className="font-serif text-5xl font-bold text-sand-800">
+        Account
+      </text>
+      <TabGroup>
+        <TabList>
+          <TabButton>UID and Wallets</TabButton>
+        </TabList>
+      </TabGroup>
+    </div>
+  );
+};
+
+AccountsPage.layout = "mustard-background";
 
 export default AccountsPage;
