5a818b66bc1a96235fc94249de5a9d0f41c8631f
Shalini Pyapali <shalu.pyapali@gmail.com>
2023-04-04 21:37:53 -0400

    Fledge our more of call to action component

--- packages/client2/components/design-system/call-to-action/call-to-action.tsx
+++ packages/client2/components/design-system/call-to-action/call-to-action.tsx
@@ -1,20 +1,67 @@
-import { ReactNode } from "react";
+import { MouseEventHandler, ReactNode } from "react";
 
-import { ButtonProps } from "../button";
-import { IconProps } from "../icon";
+import { Button } from "../button";
+import { Icon, IconNameType } from "../icon";
+
+type CallToActionButtonProps = {
+  onClick: MouseEventHandler<HTMLButtonElement> &
+    MouseEventHandler<HTMLAnchorElement>;
+  name: string;
+};
 
 export interface CallToActionProps {
   /**
-   * Optional button that user would click on as a part of the call to action
+   * Button component to optionally pass in
    */
-  buttonProps?: ButtonProps;
+  buttonRight?: CallToActionButtonProps;
   /**
-   * Optional children that render below the call to action button.
+   * Optional children that render below the call to action button
    */
   children?: ReactNode;
   className?: string;
   /**
    * Image appearing to the left of the title
    */
-  icon: IconProps;
+  iconLeft: IconNameType;
+  /**
+   * Color schemes representing the buttons that are being added to the component
+   */
+  colorScheme: "primary" | "flat" | "special";
+  /**
+   * Heading that appears at the top of the component
+   */
+  title: string;
+  /**
+   * Gives a description to this modal
+   */
+  description?: string;
 }
+
+export function CallToAction({
+  buttonRight,
+  children,
+  className,
+  iconLeft,
+  colorScheme,
+  title,
+  description,
+}: CallToActionProps) {
+  return (
+    <div className="max-w-screen rounded bg-twilight-500 p-6 text-white">
+      <div className="row flex justify-between">
+        <div className="row flex">
+          <Icon className="mt-1 mr-1" name={iconLeft} />
+          <h1>{title}</h1>
+        </div>
+        {buttonRight && (
+          <Button
+            name={buttonRight.name}
+            onClick={buttonRight.onClick}
+          ></Button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default CallToAction;
