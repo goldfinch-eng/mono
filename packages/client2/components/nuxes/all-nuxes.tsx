be9ac867ab6f758ed3c5e869d387704a23829e07
Andre Jodat-Danbrani <andre.jodat.danbrani@gmail.com>
2023-04-18 09:43:22 -0400

    decommissioned the fazz nux

--- packages/client2/components/nuxes/all-nuxes.tsx
+++ packages/client2/components/nuxes/all-nuxes.tsx
@@ -1,11 +1,9 @@
-import { CallableLoanNux } from "./callable-loan-nux";
 import { SeniorPoolCancellationFeeNux } from "./senior-pool-cancellation-fee-nux";
 
 // If you want to show a nux when a new user visits, just add it to the return value for this component
 export function AllNuxes() {
   return (
     <>
-      <CallableLoanNux />
       <SeniorPoolCancellationFeeNux />
     </>
   );
