469ba2d8658c227f0614f8d2b973a8b91f6e4e99
bstchow <36556453+bstchow@users.noreply.github.com>
2023-05-03 09:34:17 -0700

    Callable loan deploy changes - manually set gas limit

--- packages/protocol/blockchain_scripts/baseDeploy/deployCallableLoanImplementation.ts
+++ packages/protocol/blockchain_scripts/baseDeploy/deployCallableLoanImplementation.ts
@@ -6,13 +6,16 @@ import {ContractDeployer} from "../deployHelpers"
 const logger = console.log
 
 export async function deployCallableLoanImplementation(deployer: ContractDeployer): Promise<CallableLoan> {
-  logger("About to deploy CallableLoanImplementationRepository...")
+  logger("About to deploy CallableLoan")
   const {gf_deployer} = await getNamedAccounts()
   assertIsString(gf_deployer)
 
   // Deploy Callable Loan
   const callableLoan = (await deployer.deploy("CallableLoan", {
     from: gf_deployer,
+    gasLimit: 7_000_000,
+    maxFeePerGas: "85000000000",
+    maxPriorityFeePerGas: "1500000000",
   })) as CallableLoan
 
   console.log("CallableLoan address: ", callableLoan.address)
