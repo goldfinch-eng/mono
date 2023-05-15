62765cfccc38c47e3550dcd45e220652d9a4e913
Dalton Sweeney <daltyboy11@gmail.com>
2023-05-15 13:47:28 -0600

    various small deletions (#1790)

--- packages/protocol-l2/contracts/interfaces/IEvents.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.19;
-
-/// @notice Common events that can be emmitted by multiple contracts
-interface IEvents {
-  /// @notice Emitted when a safety check fails
-  event SafetyCheckTriggered();
-}
